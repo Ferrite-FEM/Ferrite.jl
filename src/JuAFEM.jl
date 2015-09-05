86390aa4ab220bab66027d4846639e62488441b8
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2015-09-05 12:06:06 +0200

    fixup solve_eq_sys

--- src/JuAFEM.jl
+++ src/JuAFEM.jl
@@ -1,5 +1,7 @@
 module JuAFEM
 
+import Base.LinAlg.chksquare
+
 export spring1e, spring1s
 export plani4e
 
--- src/utilities/solve_eq_sys.jl
+++ src/utilities/solve_eq_sys.jl
@@ -2,30 +2,28 @@
 Solves the equation system Ka = f taking into account the Dirichlet boundary conditions in the matrix bc
 """
 function solve_eq_sys(K::AbstractMatrix, f::Array, bc::Matrix )
-    (nr, nc) = size(K);
+    n = chksquare(K)
     nrf = length(f)
-    if nr != nc
-        throw(DimensionMismatch("Stiffness matrix is not square (#rows=$nr #cols=$nc)"))
-    elseif nr != nrf
-        throw(DimensionMismatch("Mismatch between number of rows in the stiffness matrix and load vector (#rowsK=$nr #rowsf=$nrf)"))
+    if n != nrf
+        throw(DimensionMismatch("Mismatch between number of rows in the stiffness matrix and load vector (#rowsK=$n #rowsf=$nrf)"))
     end
 
-    _K = convert(Matrix{Float64}, K)
-    d_pres = bc[:,1]   # prescribed dofs
-    a_pres = bc[:,2]   # corresponding prescribed dof values
-
-    _d_pres = convert(Array{Int}, d_pres)
-    _a_pres = convert(Array{Float64}, a_pres)
+    if isa(K, SparseMatrixCSC)
+         _K = convert(SparseMatrixCSC{Float64}, K)
+    else
+        _K = convert(Matrix{Float64}, K)
+    end
+    d_pres = convert(Vector{Int}, bc[:,1])   # prescribed dofs
+    a_pres = convert(Vector{Float64}, bc[:,2])   # corresponding prescribed dof values
 
     # Construct array holding all the free dofs
-    d_free = [1:nr]
-    deleteat!(d_free, _d_pres)
+    d_free = setdiff(collect(1:n), d_pres)
 
     # Solve equation system and create full solution vector a
-    a_free = K[d_free, d_free] \ ( f[d_free] - K[d_free, d_pres] * a_pres)
-    a = zeros(nr)
+    a_free = K[d_free, d_free] \ (f[d_free] - K[d_free, d_pres] * a_pres)
+    a = zeros(n)
     a[d_free] = a_free
-    a[d_pres] = _a_pres
+    a[d_pres] = a_pres
 
     # Compute boundary force = reaction force
     f_b = K*a - f;
