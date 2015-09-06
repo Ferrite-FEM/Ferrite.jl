315b6310ec11ac21ce0d4e5ba6515f19392560ec
Jimbo <jim.brouzoulis@chalmers.se>
2015-09-06 12:59:12 +0200

    Added notebook example on springs
    + Changed vector -> array as input to spings1s, since extract_eldisp
    returns an array

--- src/elements/spring.jl
+++ src/elements/spring.jl
@@ -12,7 +12,7 @@ spring1e(k::Number) = spring1e(convert(Float64, k))
 """
 Computes the force *fe* for a spring element
 """
-function spring1s(k::Float64, u::Vector{Float64})
+function spring1s(k::Float64, u::Array{Float64})
     if length(u) != 2
         throw(ArgumentError("displacements for computing the spring force must" *
                             "be a vector of length 2"))
@@ -20,6 +20,6 @@ function spring1s(k::Float64, u::Vector{Float64})
     return k * (u[2] - u[1])
 end
 spring1s{T<:Number, P<:Number}(k::T, u::P) = spring1s(convert(Float64, k),
-                                                convert(Vector{Float64}, u))
+                                                convert(Array{Float64}, u))
 
 
