7c5116ffa119857024071e8b0daa6b13e6c620ef
kirderf <fredrik.ekre@hotmail.com>
2016-03-08 20:56:56 +0100

    restructuring of the types, added more accessor functions

--- src/utilities/fe_mesh.jl
+++ src/utilities/fe_mesh.jl
@@ -1,42 +1,42 @@
 # Typ definitions
 type FEMesh{dim, T <: Real}
     coords::Vector{Vec{dim,T}} # Node coordinates
-    enodes::Matrix{Int} # Element nodes
+    topology::Matrix{Int} # Element nodes
     boundary::Vector{Vector{Int}} # Boundary nodes
 end
 
-function FEMesh()
-    # Set up the mesh here
-    return FEMesh(coords,enodes,boundary)
-end
+# function FEMesh()
+#     # Set up the mesh here
+#     return FEMesh(coords,topology,boundary)
+# end
 
 
 #############
 # Accessors #
 #############
 
-# TODO: inline those?
+# Should these even exist? Atleast I should add the same accessors for the FEDofs aswell so that contains all information needed
 
 """
-    get_element_nodes(fe_m, elindex::Int) -> el_nodes::Vector
+    get_element_nodes(fe_m::FEMesh, elindex::Int) -> el_nodes::Vector
 Gets the nodes for a given element index
 """
-get_element_nodes(fe_m::FEMesh, elindex::Int) = fe_m.enodes[:,elindex]
+@inline get_element_nodes(fe_m::FEMesh, elindex::Int) = fe_m.topology[:,elindex]
 
 """
-    get_element_coords(fe_m, elindex::Int) -> elcoord::Vector{Tensor{1,dim}}
+    get_element_coords(fe_m::FEMesh, elindex::Int) -> elcoord::Vector{Tensor{1,dim}}
 Gets the coordinates of the nodes for a given element index
 """
-get_element_coords(fe_m::FEMesh, elindex::Int) = fe_m.coords[fe_m.enodes[:,elindex]]
+@inline get_element_coords(fe_m::FEMesh, elindex::Int) = fe_m.coords[fe_m.topology[:,elindex]]
 
 """
-    get_node_coords(fe_m, nodeindex::Int) -> nodecoord::Tensor{1,dim}
+    get_node_coords(fe_m::FEMesh, nodeindex::Int) -> nodecoord::Tensor{1,dim}
 Gets the coordinates of the nodes for a given element index
 """
-get_node_coords(fe_m::FEMesh, nodeindex::Int) = fe_m.coords[nodeindex]
+@inline get_node_coords(fe_m::FEMesh, nodeindex::Int) = fe_m.coords[nodeindex]
 
 """
-    get_boundary_nodes(fe_m, boundaryindex::Int) -> boundarynodes::Vector
+    get_boundary_nodes(fe_m::FEMesh, boundaryindex::Int) -> boundarynodes::Vector
 Gets the nodes on a specified boundary
 """
-get_boundary_nodes(fe_m::FEMesh, boundaryindex::Int) = fe_m.boundary[boundaryindex]
+@inline get_boundary_nodes(fe_m::FEMesh, boundaryindex::Int) = fe_m.boundary[boundaryindex]
