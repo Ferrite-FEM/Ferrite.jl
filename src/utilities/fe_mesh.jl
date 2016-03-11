d13913603052e37fb922346efc72a9613f2ee9bf
kirderf <fredrik.ekre@hotmail.com>
2016-03-11 17:25:55 +0100

    finished 1d and 2d mesh generating funcs. started on 3d, not finished with the boundary

--- src/utilities/fe_mesh.jl
+++ src/utilities/fe_mesh.jl
@@ -1,6 +1,5 @@
 # Typ definitions
-type FEBoundary # Tänkte typ parametrisera detta på nåt sätt så att man har FEboundary i 2D, 1D, och 0D
-    dim::Int
+type FEBoundary{T}
     topology::Array{Int}
 end
 
@@ -11,15 +10,14 @@ type FEMesh{dim, T <: Real}
 end
 
 
-function FEMesh(dim)
+function FEMesh() # Input should be shape, vectors with start and end-coordinates, and a vector with number of elements in each dir.
     # Set up the mesh here
-
     # Do some input checks and stuff
 
     # Here I was thinking we could call a set_up_mesh function that is overloaded for 1,2 and 3 dimensions somehow
-    coords,topology,boundary = set_up_mesh(dim)
+    # coords,topology,boundary = set_up_mesh(dim)
 
-    return FEMesh(coords,topology,boundary)
+    # return FEMesh(coords,topology,boundary)
 
 end
 
@@ -58,7 +56,7 @@ Gets the nodes on a specified boundary
 
 # Overloada set_up_mesh för olika dimensioner och former typ?
 # 1D
-function set_up_mesh{T}(xs::Vector{T}, xe::Vector{T}, nel::Vector{Int}) # This is not quite finished yet I realize
+function set_up_mesh_1D_square{T}(xs::Vector{T}, xe::Vector{T}, nel::Vector{Int}) # This is not quite finished yet I realize
     xs_x = xs[1]
     xe_x = xe[1]
     nel_x = nel[1]
@@ -86,7 +84,7 @@ end
 
 
 # 2D
-function set_up_mesh{T}(xs::Vector{T}, xe::Vector{T}, nel::Vector{Int})
+function set_up_mesh_2D_square{T}(xs::Vector{T}, xe::Vector{T}, nel::Vector{Int})
     xs_x = xs[1]; xs_y = xs[2]
     xe_x = xe[1]; xe_y = xe[2]
     nel_x = nel[1]; nel_y = nel[2]
@@ -94,69 +92,93 @@ function set_up_mesh{T}(xs::Vector{T}, xe::Vector{T}, nel::Vector{Int})
     n_nodes_x = nel_x + 1; n_nodes_y = nel_y + 1
     n_nodes = n_nodes_x * n_nodes_y
 
-    coords_x = repmat(collect(linspace(xs_x, xe_x, n_nodes_x)), n_nodes_y)
-    coords_y = repmat(collect(linspace(xs_x, xe_x, n_nodes_y))', n_nodes_x)[:]
-    coords = [coords_x';
-              coords_y']
+    coords_x = linspace(xs_x, xe_x, n_nodes_x)
+    coords_y = linspace(xs_y, xe_y, n_nodes_y)
+
+    coords = T[]
+    for j in 1:n_nodes_y, i in 1:n_nodes_x
+        ccoords = [coords_x[i], coords_y[j]]
+        append!(coords,ccoords)
+    end
+    coords = reshape(coords,(2,n_nodes))
+
     tensor_coords = reinterpret(Vec{2, Float64}, coords, (size(coords,2),))
 
     nodes = reshape(collect(1:n_nodes),(n_nodes_x,n_nodes_y))
 
-
-    cel = 1
-
-    for i = 1:nEly, j = 1:nElx
-        topology[:,cEl] = [(n_nodes_x*(i-1)+j):(n_nodes_x*(i-1)+j+1); flipdim(((n_nodes_x*(i-1)+j):(nxNodes*(i-1)+j+1))+nxNodes,1)]
-        cEl += 1
+    topology = Int[]
+    for j in 1:nel_y, i in 1:nel_x
+        ctopology = [nodes[i:i+1,j]; nodes[i+1:-1:i,j+1]]
+        append!(topology,ctopology)
     end
+    topology = reshape(topology,(4,nel))
 
-    topology = zeros(Int,2,nel_x)
+    # Edges
+    b1D = [FEBoundary{1}(collect(1:n_nodes_x)),
+           FEBoundary{1}(collect(n_nodes_x:n_nodes_x:n_nodes)),
+           FEBoundary{1}(collect((n_nodes_x*(n_nodes_y-1)+1):n_nodes)),
+           FEBoundary{1}(collect(1:n_nodes_x:(n_nodes_x*(n_nodes_y-1)+1)))]
 
-    for i = 1:nel_x
-        topology[:,i] = nodes[i:i+1]
-    end
+    # Corners
+    b0D = [FEBoundary{0}([1]),
+           FEBoundary{0}([n_nodes_x]),
+           FEBoundary{0}([n_nodes_x*(n_nodes_y-1)+1]),
+           FEBoundary{0}([n_nodes])]
 
-    b0 = [FEBoundary(0,[1]), FEBoundary(0,[n_nodes])]
-
-    boundary = Vector[b0]
+    boundary = Vector[b1D,b0D]
 
     return FEMesh(tensor_coords,topology,boundary)
 end
 
-# # 3D
-# function set_up_mesh{T}(xs::Vector{T}, xe::Vector{T}, nel::Vector{Int})
-#     xs_x = xs[1]; xs_y = xs[2]
-#     xe_x = xe[1]; xe_y = xe[2]
-#     nel_x = nel[1]; nel_y = nel[2]
-#     nel = nel_x * nel_y
-#     n_nodes_x = nel_x + 1; n_nodes_y = nel_y + 1
-#     n_nodes = n_nodes_x * n_nodes_y
+# 3D
+function set_up_mesh_3D_square{T}(xs::Vector{T}, xe::Vector{T}, nel::Vector{Int})
+    xs_x = xs[1]; xs_y = xs[2]; xs_z = xs[3]
+    xe_x = xe[1]; xe_y = xe[2]; xe_z = xe[3]
+    nel_x = nel[1]; nel_y = nel[2]; nel_z = nel[3]
+    nel = nel_x * nel_y * nel_z
+    n_nodes_x = nel_x + 1; n_nodes_y = nel_y + 1; n_nodes_z = nel_z + 1
+    n_nodes = n_nodes_x * n_nodes_y * n_nodes_z
 
-#     coords_x = repmat(collect(linspace(xs_x, xe_x, n_nodes_x)), n_nodes_y)
-#     coords_y = repmat(collect(linspace(xs_x, xe_x, n_nodes_y))', n_nodes_x)[:]
-#     coords = [coords_x';
-#               coords_y']
-#     tensor_coords = reinterpret(Vec{2, Float64}, coords, (size(coords,2),))
+    coords_x = linspace(xs_x, xe_x, n_nodes_x)
+    coords_y = linspace(xs_y, xe_y, n_nodes_y)
+    coords_z = linspace(xs_z, xe_z, n_nodes_z)
 
-#     nodes = reshape(collect(1:n_nodes),(n_nodes_x,n_nodes_y))
+    coords = T[]
+    for k in 1:n_nodes_z, j in 1:n_nodes_y, i in 1:n_nodes_x
+        ccoords = [coords_x[i], coords_y[j], coords_z[k]]
+        append!(coords,ccoords)
+    end
+    coords = reshape(coords,(3,n_nodes))
 
+    tensor_coords = reinterpret(Vec{3, Float64}, coords, (size(coords,2),))
 
-#     cel = 1
+    # Set up topology
+    nodes = reshape(collect(1:n_nodes),(n_nodes_x,n_nodes_y,n_nodes_z))
 
-#     for i = 1:nEly, j = 1:nElx
-#         topology[:,cEl] = [(n_nodes_x*(i-1)+j):(n_nodes_x*(i-1)+j+1); flipdim(((n_nodes_x*(i-1)+j):(nxNodes*(i-1)+j+1))+nxNodes,1)]
-#         cEl += 1
-#     end
+    topology = Int[]
+    for k in 1:nel_z, j in 1:nel_y, i in 1:nel_x
+        ctopology = [nodes[i:i+1,j,k]; nodes[i+1:-1:i,j+1,k];
+                     nodes[i:i+1,j,k+1]; nodes[i+1:-1:i,j+1,k+1]]
+        append!(topology,ctopology)
+    end
+    topology = reshape(topology,(8,nel))
 
-#     topology = zeros(Int,2,nel_x)
+    # TODO: Boundaries
+    # Sides
+    b2D = []
+    # Edges
+    b1D = [FEBoundary{1}(collect(1:n_nodes_x)),
+           FEBoundary{1}(collect(n_nodes_x:n_nodes_x:n_nodes)),
+           FEBoundary{1}(collect((n_nodes_x*(n_nodes_y-1)+1):n_nodes)),
+           FEBoundary{1}(collect(1:n_nodes_x:(n_nodes_x*(n_nodes_y-1)+1)))]
 
-#     for i = 1:nel_x
-#         topology[:,i] = nodes[i:i+1]
-#     end
+    # Corners
+    b0D = [FEBoundary{0}([1]),
+           FEBoundary{0}([n_nodes_x]),
+           FEBoundary{0}([n_nodes_x*(n_nodes_y-1)+1]),
+           FEBoundary{0}([n_nodes])]
 
-#     b0 = [FEBoundary(0,[1]), FEBoundary(0,[n_nodes])]
+    boundary = Vector[b1D,b0D]
 
-#     boundary = Vector[b0]
-
-#     return FEMesh(tensor_coords,topology,boundary)
-# end
+    return FEMesh(tensor_coords,topology,boundary)
+end
