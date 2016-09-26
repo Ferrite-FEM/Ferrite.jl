fbdc8627e10a3f5922e136a8f211c6fe8541ecd8
Fredrik Ekre <fredrik.ekre@chalmers.se>
2016-09-26 08:35:29 +0200

    remove BaseTestNext [ci-skip]

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,6 @@
 using JuAFEM
 using FastGaussQuadrature
-
-if VERSION >= v"0.5-"
-    using Base.Test
-else
-    using BaseTestNext
-    const Test = BaseTestNext
-end
+using Base.Test
 
 include("test_fevalues.jl")
 include("test_quadrules.jl")
