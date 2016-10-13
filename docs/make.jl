010000b08780d90e121a3ae756227a5151a02610
Fredrik Ekre <fredrik.ekre@chalmers.se>
2016-10-13 10:41:03 +0200

    remove mkdocs, make html

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,17 @@
 using Documenter, JuAFEM
 
 # Build documentation.
-# ====================
-
 makedocs(
-    # options
-    modules = [JuAFEM],
-    clean   = true,
-    doctest = false
-)
+    format = :html,
+    sitename = "JuAFEM.jl",
+    pages = Any[
+        "Home" => "index.md",
+        "Installation" => "man/installation.md",
+        "man/basic_usage.md","lib/maintypes.md", "lib/utility_functions.md"]
+        )
 
 # Deploy built documentation from Travis.
-# =======================================
-
-# Needs to install an additional dep, mkdocs-material, so provide a custom `deps`.
-custom_deps() = run(`pip install --user pygments mkdocs mkdocs-material`)
-
 deploydocs(
     # options
-    deps = custom_deps,
     repo = "github.com/KristofferC/JuAFEM.jl.git"
 )
