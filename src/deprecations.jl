b2d033719f726aed5d861a1db439ebdf94a41b67
Fredrik Ekre <fredrik.ekre@hotmail.com>
2016-10-15 18:28:41 +0200

    move deprecated func

--- src/deprecations.jl
+++ src/deprecations.jl
@@ -1,3 +1,26 @@
+# PR #70
+function vtk_grid{T}(topology::Matrix{Int}, coord::Matrix{T}, filename::AbstractString)
+    Base.depwarn("vtk_grid(topology::Matrix{Int}, coord::Matrix, filename::AbstractString) is deprecated, use vtk_grid{dim,T}(filename::AbstractString, coords::Vector{Vec{dim,T}}, topology::Matrix{Int}, celltype::VTKCellTypes.VTKCellType) instead", :vtk_grid)
+    nen = size(topology,1)
+    nnodes = size(coord, 2)
+    ndim = size(coord, 1)
+
+    if nen == 3 && ndim == 2
+        cell =  VTKCellTypes.VTK_TRIANGLE
+    elseif nen == 4 && ndim == 2
+        cell = VTKCellTypes.VTK_QUAD
+    elseif nen == 8 && ndim == 3
+        cell = VTKCellTypes.VTK_HEXAHEDRON
+    elseif nen == 4 && ndim == 3
+        cell = VTKCellTypes.VTK_TETRA
+    end
+
+    coords = reinterpret(Vec{ndim,T},coord,(nnodes,))
+
+    vtk = vtk_grid(filename, coords, topology, cell)
+    return vtk
+end
+
 # Issue #66, PR #73
 @deprecate FEValues FECellValues
 
--- test/test_VTK.jl
+++ test/test_VTK.jl
@@ -8,73 +8,69 @@ using SHA
     end
 
     # Set up small mesh
-    n1 = Vec{2}((0.0,0.0))
-    n2 = Vec{2}((1.0,0.0))
-    n3 = Vec{2}((2.0,0.0))
-    n4 = Vec{2}((0.0,1.0))
-    n5 = Vec{2}((1.0,1.0))
-    n6 = Vec{2}((2.0,1.0))
-    n7 = Vec{2}((0.0,2.0))
-    n8 = Vec{2}((1.0,2.0))
+    n1 = Vec{2}((0.0,0.0)); n2 = Vec{2}((1.0,0.0))
+    n3 = Vec{2}((2.0,0.0)); n4 = Vec{2}((0.0,1.0))
+    n5 = Vec{2}((1.0,1.0)); n6 = Vec{2}((2.0,1.0))
+    n7 = Vec{2}((0.0,2.0)); n8 = Vec{2}((1.0,2.0))
     n9 = Vec{2}((2.0,2.0))
-    coords = Vec{2,Float64}[n1,n2,n3,n4,n5,n6,n7,n8,n9]
+    coords_2D = Vec{2,Float64}[n1,n2,n3,n4,n5,n6,n7,n8,n9]
 
-    #####################
-    # Triangle 2-D mesh #
-    #####################
-    celltype = WriteVTK.VTKCellTypes.VTK_TRIANGLE
-    triangle_mesh_edof = [1 2 4
-                          2 5 4
-                          2 6 5
-                          2 3 6
-                          4 8 7
-                          4 5 8
-                          5 6 8
-                          6 9 8]'
-    vtkfile = vtk_grid("triangles",coords,triangle_mesh_edof,celltype)
-    vtk_save(vtkfile)
+    triangle_mesh_edof = [1 2 2 2 4 4 5 6
+                          2 5 6 3 8 5 6 9
+                          4 4 5 6 7 8 8 8]
 
-    sha = bytes2hex(sha1("triangles.vtu"))
-    if OVERWRITE_CHECKSUMS
-        write(csio, sha*" triangles.vtu\n")
-    else
-        # Returns 0:-1 if string is not found
-        cmp = search(checksum_list, sha)
-        @test cmp != 0:-1
-    end
+    quad_mesh_edof = [1 2 4 5
+                      2 3 5 6
+                      5 6 8 9
+                      4 5 7 8]
 
-    # Test old version as well
-    coordmat = reinterpret(Float64,coords,(2,9))
-    vtkfile = vtk_grid(triangle_mesh_edof,coordmat,"triangles_old")
-    vtk_save(vtkfile)
+    n1 = Vec{3}((0.0,0.0,0.0)); n2 = Vec{3}((1.0,0.0,0.0))
+    n3 = Vec{3}((2.0,0.0,0.0)); n4 = Vec{3}((0.0,2.0,0.0))
+    n5 = Vec{3}((1.0,2.0,0.0)); n6 = Vec{3}((2.0,2.0,0.0))
+    n7 = Vec{3}((0.0,0.0,3.0)); n8 = Vec{3}((1.0,0.0,3.0))
+    n9 = Vec{3}((2.0,0.0,3.0)); n10 = Vec{3}((0.0,2.0,3.0))
+    n11 = Vec{3}((1.0,2.0,3.0)); n12 = Vec{3}((2.0,2.0,3.0))
+    coords_3D = Vec{3,Float64}[n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,n11,n12]
 
-    sha = bytes2hex(sha1("triangles_old.vtu"))
-    if OVERWRITE_CHECKSUMS
-        write(csio, sha*" triangles_old.vtu\n")
-    else
-        # Returns 0:-1 if string is not found
-        cmp = search(checksum_list, sha)
-        @test cmp != 0:-1
-    end
+    cube_mesh_edof = [1 2 5 4 7 8 11 10
+                      2 3 6 5 8 9 12 11]'
 
-    #################
-    # Quad 2-D mesh #
-    #################
-    celltype = WriteVTK.VTKCellTypes.VTK_QUAD
-    quad_mesh_edof = [1 2 5 4
-                      2 3 6 5
-                      4 5 8 7
-                      5 6 9 8]'
-    vtkfile = vtk_grid("quads",coords,quad_mesh_edof,celltype)
-    vtk_save(vtkfile)
+    tetra_mesh_edof = [1 2 2 2 4 2 3 3 3 5
+                       2 5 4 7 7 3 6 5 8 8
+                       4 4 7 8 11 5 5 8 9 12
+                       7 11 11 11 10 8 12 12 12 11]
 
-    sha = bytes2hex(sha1("quads.vtu"))
-    if OVERWRITE_CHECKSUMS
-        write(csio, sha*" quads.vtu\n")
-    else
-        # Returns 0:-1 if string is not found
-        cmp = search(checksum_list, sha)
-        @test cmp != 0:-1
+    for (name, coord, edof, ctype) in (("triangles", coords_2D, triangle_mesh_edof, VTKCellTypes.VTK_TRIANGLE),
+                                       ("quads", coords_2D, quad_mesh_edof, VTKCellTypes.VTK_QUAD),
+                                       ("cubes", coords_3D, cube_mesh_edof, VTKCellTypes.VTK_HEXAHEDRON),
+                                       ("tetras", coords_3D, tetra_mesh_edof, VTKCellTypes.VTK_TETRA))
+
+        vtkfile = vtk_grid(name, coord, edof, ctype)
+        vtk_save(vtkfile)
+
+        sha = bytes2hex(sha1(name*".vtu"))
+        if OVERWRITE_CHECKSUMS
+            write(csio, sha*" "*name*".vtu\n")
+        else
+            # Returns 0:-1 if string is not found
+            cmp = search(checksum_list, sha)
+            @test cmp != 0:-1
+        end
+
+        # Test deprecated method, PR #70
+        coordmat = reinterpret(Float64,coord,(size(coord[1],1),length(coord)))
+
+        vtkfile = vtk_grid(edof, coordmat, name*"_old")
+        vtk_save(vtkfile)
+
+        sha = bytes2hex(sha1(name*"_old.vtu"))
+        if OVERWRITE_CHECKSUMS
+            write(csio, sha*" "*name*"_old.vtu\n")
+        else
+            # Returns 0:-1 if string is not found
+            cmp = search(checksum_list, sha)
+            @test cmp != 0:-1
+        end
     end
 
     OVERWRITE_CHECKSUMS && close(csio)
