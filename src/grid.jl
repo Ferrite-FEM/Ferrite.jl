31e3d27c1f41c3c5a72318eaa873fb4a000bca9b
Fredrik Ekre <fredrik.ekre@hotmail.com>
2016-10-19 00:57:57 +0200

    test with CellIndex and BoundaryIndex

--- src/grid.jl
+++ src/grid.jl
@@ -1,5 +1,5 @@
 # Grid types
-export Node, Cell, Grid
+export Node, Cell, CellIndex, Grid
 
 # Cell typealias
 export Line, QuadraticLine,
@@ -7,7 +7,7 @@ export Line, QuadraticLine,
        Tetrahedron, QuadraticTetrahedron, Hexahedron, QuadraticHexahedron
 
 # Grid utilities
-export cells, nodes, cell_coordinates, n_cells, n_nodes
+export cells, nodes, coordinates, n_cells, n_nodes
 
 #########################
 # Main types for meshes #
@@ -24,10 +24,18 @@ Node{dim, T}(x::NTuple{dim, T}) = Node(Vec{dim, T}(x))
 A `Cell` is a sub-domain defined by a collection of `Node`s as vertices.
 """
 immutable Cell{dim, N}
-    id::Int
     nodes::NTuple{N, Int}
 end
-(::Type{Cell{dim}}){dim,N}(id::Int, nodes::NTuple{N}) = Cell{dim,N}(id, nodes)
+(::Type{Cell{dim}}){dim,N}(nodes::NTuple{N}) = Cell{dim,N}(nodes)
+
+immutable CellIndex
+    idx::Int
+end
+
+immutable BoundaryIndex
+    idx::CellIndex
+    side::Int
+end
 
 """
 A `Grid` is a collection of `Cells` and `Node`s which covers the computational domain.
@@ -35,6 +43,7 @@ A `Grid` is a collection of `Cells` and `Node`s which covers the computational d
 immutable Grid{dim, N, T <: Real}
     cells::Vector{Cell{dim, N}}
     nodes::Vector{Node{dim, T}}
+    boundaries::Dict{Int, Vector{Tuple{CellIndex,Int}}}
     # cell_sets # Dict? with symbol/name => Vector of id's
     # node_sets # Dict?
     # interpolation::FunctionSpace
@@ -64,24 +73,39 @@ typealias Hexahedron Cell{3, 8}
 @inline n_cells(grid::Grid) = length(grid.cells)
 @inline n_nodes(grid::Grid) = length(grid.nodes)
 
+# Iterate over cell vector
+Base.start{dim, N}(c::Vector{Cell{dim, N}}) = 1
+Base.next{dim, N}(c::Vector{Cell{dim, N}}, state) = (CellIndex(state), state + 1)
+Base.done{dim, N}(c::Vector{Cell{dim, N}}, state) = state > length(c)
+# Base.eltype{dim, N}(c::Type{Vector{Cell{dim, N}}}) = CellIndex
+
+# Iterate over boundary vector
+Base.start(b::Vector{Tuple{CellIndex,Int}}) = 1
+Base.next(b::Vector{Tuple{CellIndex,Int}}, state) = (BoundaryIndex(b[state]...), state + 1)
+Base.done(b::Vector{Tuple{CellIndex,Int}}, state) = state > length(b)
+# Base.eltype(b::Vector{Tuple{CellIndex,Int}}) = BoundaryIndex
+
 """
 Returns a vector with the coordinates of the vertices of a cell
 
-    cell_coordinates{dim, N}(grid::Grid{dim, N}, cell::Cell{dim, N})
+    coordinates{dim, N}(grid::Grid{dim, N}, cell::CellIndex)
 
 ** Arguments **
 
 * `grid`: a `Grid`
-* `cell`: a `Cell` in the grid
+* `cell`: a `CellIndex` corresponding to a `Cell` in the grid in the grid
 
 ** Results **
 
 * `x`: A `Vector` of `Vec`s, one for each vertex of the cell.
 
 """
-@inline cell_coordinates{dim, N}(grid::Grid{dim, N}, cell::Cell{dim, N}) = cell_coordinates(grid, cell.id)
-
-@inline function cell_coordinates{dim, N, T}(grid::Grid{dim, N, T}, cell_id::Int)
-    nodeidx = grid.cells[cell_id].nodes
+@inline function coordinates{dim, N, T}(grid::Grid{dim, N, T}, cell::CellIndex)
+    nodeidx = grid.cells[cell.idx].nodes
     return [grid.nodes[i].x for i in nodeidx]::Vector{Vec{dim, T}}
 end
+
+@inline coordinates{dim, N, T}(grid::Grid{dim, N, T}, boundary::BoundaryIndex) = coordinates(grid, boundary.idx)
+
+reinit!(fe_cv::FECellValues, grid::Grid, cell::CellIndex) = reinit!(fe_cv, coordinates(grid, cell))
+reinit!(fe_bv::FEBoundaryValues, grid::Grid, boundary::BoundaryIndex) = reinit!(fe_bv, coordinates(grid, boundary), boundary.side)
