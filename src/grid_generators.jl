38ce35368eca73a41f016eab9d20b9ef87da4920
Fredrik Ekre <fredrik.ekre@hotmail.com>
2016-10-26 00:13:31 +0200

    quadratic grids

--- src/grid_generators.jl
+++ src/grid_generators.jl
@@ -29,6 +29,33 @@ function generate_grid{T}(::Type{Line}, nel::NTuple{1, Int}, left::Vec{1, T}=Vec
                          :right => [2])
     return Grid(cells, nodes, cellbounds, cellboundarysets=cellboundsets)
 end
+# QuadraticLine
+function generate_grid{T}(::Type{QuadraticLine}, nel::NTuple{1, Int}, left::Vec{1, T}=Vec{1}((-1.0,)), right::Vec{1, T}=Vec{1}((1.0,)))
+    nel_x = nel[1]
+    n_nodes = 2*nel_x + 1
+
+    # Generate nodes
+    coords_x = collect(linspace(left[1], right[1], n_nodes))
+    nodes = Node{1,T}[]
+    for i in 1:n_nodes
+        push!(nodes, Node((coords_x[i],)))
+    end
+
+    # Generate cells
+    cells = QuadraticLine[]
+    for i in 1:nel_x
+        push!(cells, QuadraticLine((2*i-1, 2*i+1, 2*i)))
+    end
+
+    # Cell boundaries
+    cellbounds = CellBoundary[CellBoundary((1, 1)),
+                              CellBoundary((nel_x, 2))]
+
+    # Cell boundary sets
+    cellboundsets = Dict(:left  => [1],
+                         :right => [2])
+    return Grid(cells, nodes, cellbounds, cellboundarysets=cellboundsets)
+end
 
 # Quadrilateral
 function generate_grid{T}(::Type{Quadrilateral}, nel::NTuple{2, Int}, left::Vec{2, T}=Vec{2}((-1.0,-1.0)), right::Vec{2, T}=Vec{2}((1.0,1.0)))
@@ -49,6 +76,47 @@ function generate_grid{T}(::Type{Quadrilateral}, nel::NTuple{2, Int}, left::Vec{
     cells = Quadrilateral[]
     for j in 1:nel_y, i in 1:nel_x
         push!(cells, Quadrilateral((node_array[i,j], node_array[i+1,j], node_array[i+1,j+1], node_array[i,j+1])))
+    end
+
+    # Cell boundaries
+    cell_array = reshape(collect(1:nel_tot),(nel_x, nel_y))
+    cellbounds = CellBoundary[[CellBoundary((cl, 1)) for cl in cell_array[:,1]];
+                              [CellBoundary((cl, 2)) for cl in cell_array[end,:]];
+                              [CellBoundary((cl, 3)) for cl in cell_array[:,end]];
+                              [CellBoundary((cl, 4)) for cl in cell_array[1,:]]]
+
+    # Cell boundary sets
+    offset = 0
+    cellboundsets = Dict{Symbol, Vector{Int}}()
+    cellboundsets[:bottom] = (1:length(cell_array[:,1]))   + offset; offset += length(cell_array[:,1])
+    cellboundsets[:right]  = (1:length(cell_array[end,:])) + offset; offset += length(cell_array[end,:])
+    cellboundsets[:top]    = (1:length(cell_array[:,end])) + offset; offset += length(cell_array[:,end])
+    cellboundsets[:left]   = (1:length(cell_array[1,:]))   + offset; offset += length(cell_array[1,:])
+
+    return Grid(cells, nodes, cellbounds, cellboundarysets=cellboundsets)
+end
+
+# QuadraticQuadrilateral
+function generate_grid{T}(::Type{QuadraticQuadrilateral}, nel::NTuple{2, Int}, left::Vec{2, T}=Vec{2}((-1.0,-1.0)), right::Vec{2, T}=Vec{2}((1.0,1.0)))
+    nel_x = nel[1]; nel_y = nel[2]; nel_tot = nel_x*nel_y
+    n_nodes_x = 2*nel_x + 1; n_nodes_y = 2*nel_y + 1
+    n_nodes = n_nodes_x * n_nodes_y
+
+    # Generate nodes
+    coords_x = linspace(left[1], right[1], n_nodes_x)
+    coords_y = linspace(left[2], right[2], n_nodes_y)
+    nodes = Node{2,T}[]
+    for j in 1:n_nodes_y, i in 1:n_nodes_x
+        push!(nodes, Node((coords_x[i], coords_y[j])))
+    end
+
+    # Generate cells
+    node_array = reshape(collect(1:n_nodes), (n_nodes_x, n_nodes_y))
+    cells = QuadraticQuadrilateral[]
+    for j in 1:nel_y, i in 1:nel_x
+        push!(cells, QuadraticQuadrilateral((node_array[2*i-1,2*j-1],node_array[2*i+1,2*j-1],node_array[2*i+1,2*j+1],node_array[2*i-1,2*j+1],
+                                             node_array[2*i,2*j-1],node_array[2*i+1,2*j],node_array[2*i,2*j+1],node_array[2*i-1,2*j],
+                                             node_array[2*i,2*j])))
     end
 
     # Cell boundaries
@@ -134,6 +202,48 @@ function generate_grid{T}(::Type{Triangle}, nel::NTuple{2, Int}, left::Vec{2, T}
     for j in 1:nel_y, i in 1:nel_x
         push!(cells, Triangle((node_array[i,j], node_array[i+1,j], node_array[i,j+1]))) # ◺
         push!(cells, Triangle((node_array[i+1,j], node_array[i+1,j+1], node_array[i,j+1]))) # ◹
+    end
+
+    # Cell boundaries
+    cell_array = reshape(collect(1:nel_tot),(2, nel_x, nel_y))
+    cellbounds = CellBoundary[[CellBoundary((cl, 1)) for cl in cell_array[1,:,1]];
+                              [CellBoundary((cl, 1)) for cl in cell_array[2,end,:]];
+                              [CellBoundary((cl, 2)) for cl in cell_array[2,:,end]];
+                              [CellBoundary((cl, 3)) for cl in cell_array[1,1,:]]]
+
+    # Cell boundary sets
+    offset = 0
+    cellboundsets = Dict{Symbol, Vector{Int}}()
+    cellboundsets[:bottom] = (1:length(cell_array[1,:,1]))   + offset; offset += length(cell_array[1,:,1])
+    cellboundsets[:right]  = (1:length(cell_array[2,end,:])) + offset; offset += length(cell_array[2,end,:])
+    cellboundsets[:top]    = (1:length(cell_array[2,:,end])) + offset; offset += length(cell_array[2,:,end])
+    cellboundsets[:left]   = (1:length(cell_array[1,1,:]))   + offset; offset += length(cell_array[1,1,:])
+
+    return Grid(cells, nodes, cellbounds, cellboundarysets=cellboundsets)
+end
+
+# QuadraticTriangle
+function generate_grid{T}(::Type{QuadraticTriangle}, nel::NTuple{2, Int}, left::Vec{2, T}=Vec{2}((-1.0,-1.0)), right::Vec{2, T}=Vec{2}((1.0,1.0)))
+    nel_x = nel[1]; nel_y = nel[2]; nel_tot = 2*nel_x*nel_y
+    n_nodes_x = 2*nel_x + 1; n_nodes_y = 2*nel_y + 1
+    n_nodes = n_nodes_x * n_nodes_y
+
+    # Generate nodes
+    coords_x = linspace(left[1], right[1], n_nodes_x)
+    coords_y = linspace(left[2], right[2], n_nodes_y)
+    nodes = Node{2,T}[]
+    for j in 1:n_nodes_y, i in 1:n_nodes_x
+        push!(nodes, Node((coords_x[i], coords_y[j])))
+    end
+
+    # Generate cells
+    node_array = reshape(collect(1:n_nodes), (n_nodes_x, n_nodes_y))
+    cells = QuadraticTriangle[]
+    for j in 1:nel_y, i in 1:nel_x
+        push!(cells, QuadraticTriangle((node_array[2*i-1,2*j-1], node_array[2*i+1,2*j-1], node_array[2*i-1,2*j+1],
+                                        node_array[2*i,2*j-1], node_array[2*i,2*j], node_array[2*i-1,2*j]))) # ◺
+        push!(cells, QuadraticTriangle((node_array[2*i+1,2*j-1], node_array[2*i+1,2*j+1], node_array[2*i-1,2*j+1],
+                                        node_array[2*i+1,2*j], node_array[2*i,2*j+1], node_array[2*i,2*j]))) # ◹
     end
 
     # Cell boundaries
