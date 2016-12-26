b4c5991991790eaabc99a4d9da1642262bf61f66
Fredrik Ekre <fredrik.ekre@chalmers.se>
2016-12-26 14:53:35 +0100

    try build docs last

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,3 @@
-# Build the docs
-include("../docs/make.jl")
-
 using JuAFEM
 using FastGaussQuadrature
 using Base.Test
@@ -15,3 +12,6 @@ include("test_assemble.jl")
 include("test_grid.jl")
 include("test_VTK.jl")
 include("test_notebooks.jl")
+
+# Build the docs
+include("../docs/make.jl")
