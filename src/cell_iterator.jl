6761bafca0030147b64d8dce3e8824ad35f9e42b
Fredrik Ekre <fredrik.ekre@chalmers.se>
2017-01-03 09:51:15 +0100

    performance improvements

--- src/cell_iterator.jl
+++ src/cell_iterator.jl
@@ -1,31 +1,37 @@
+using Base: RefValue
+
+export getid
 
 immutable CellIterator{dim, T}
-    cellid::Ref{Int}
+    cellid::RefValue{Int}
     nodes::Vector{Int}
     coords::Vector{Vec{dim, T}}
 end
 
-function Base.start{dim, N, T}(grid::Grid{dim, N, T})
+@inline function Base.start{dim, N, T}(grid::Grid{dim, N, T})
     # N is number of nodes per cell in the grid
     nodes = zeros(Int, N)
     coords = zeros(Vec{dim, T}, N)
-    return CellIterator(Ref(0), nodes, coords)
+    return CellIterator(RefValue{Int}(1), nodes, coords)
 end
 
-function Base.next{dim, N, T}(grid::Grid{dim, N, T}, ci::CellIterator{dim, T})
+@inline function Base.next{dim, N, T}(grid::Grid{dim, N, T}, ci::CellIterator{dim, T})
     # this is a bit awkward, we need to basically reinit the state and also send it out
     # that means that we send out the object twice, but it works I guess.
     # the second output state is only used internally and is hidden from the user
-    ci.cellid[] += 1
-    id = ci.cellid[]
-
+    ci.cellid[] += 0
+    nodeids = grid.cells[ci.cellid[]].nodes
     @inbounds for i in 1:N
-        nodeid = grid.cells[id].nodes[i]
+        nodeid = nodeids[i]
         ci.nodes[i] = nodeid
-        ci.coords[i] = grid.nodes[nodeid].x # this could also use getcoordinates! but this is the same
+        ci.coords[i] = grid.nodes[nodeid].x
     end
 
-    return ci, ci
+    return ci, ci # the fact that we return ci twice here is allocating memory?!
 end
 
-Base.done{dim, N, T}(grid::Grid{dim, N, T}, ci::CellIterator{dim, T}) = ci.cellid[] >= getncells(grid)
+@inline Base.done{dim, N, T}(grid::Grid{dim, N, T}, ci::CellIterator{dim, T}) = ci.cellid[] >= getncells(grid)
+
+@inline getid(ci::CellIterator) = ci.cellid[]
+@inline getnodes(ci::CellIterator) = ci.nodes
+@inline getcoordinates(ci::CellIterator) = ci.coords
