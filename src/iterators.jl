c2757cb739eebcaeba2930749d4594ccca2a4c7b
Fredrik Ekre <fredrik.ekre@chalmers.se>
2017-02-16 20:41:39 +0100

    take 3

--- src/iterators.jl
+++ src/iterators.jl
@@ -8,39 +8,43 @@ A `CellIterator` contains information about the cell which can be queried from t
 **Example:**
 
 ```julia
-ci = CellIterator(grid)
-
-for i in 1:getncells(grid)
-    reinit!(grid, ci, i)          # update the CellIterator for cell i
+for cell in CellIterator(grid)
     coords = getcoordinates(cell) # get the coordinates
     nodes = getnodes(cell)        # get the node numbers
 
-    reinit!(cv, ci)               # reinit the FE-base with a CellIterator
+    reinit!(cv, cell)             # reinit! the FE-base with a CellIterator
 end
 ```
 """
-immutable CellIterator{dim, T}
+immutable CellIterator{dim, N, T}
+    grid::Grid{dim, N, T}
     nodes::Vector{Int}
     coords::Vector{Vec{dim, T}}
 end
 
-@inline function CellIterator{dim, N, T}(grid::Grid{dim, N, T})
+function CellIterator{dim, N, T}(grid::Grid{dim, N, T})
     nodes = zeros(Int, N)
     coords = zeros(Vec{dim, T}, N)
-    return CellIterator(nodes, coords)
+    return CellIterator(grid, nodes, coords)
 end
 
-@inline function reinit!{dim, N, T}(ci::CellIterator{dim, T}, grid::Grid{dim, N, T}, i::Int)
-    nodeids = grid.cells[i].nodes
-    @inbounds for i in 1:N
-        nodeid = nodeids[i]
-        ci.nodes[i] = nodeid
-        ci.coords[i] = grid.nodes[nodeid].x
+@inline Base.start(::CellIterator) = 1
+
+@inline function Base.next{dim, N, T}(ci::CellIterator{dim, N, T}, i)
+    nodeids = ci.grid.cells[i].nodes
+    @inbounds for j in 1:N
+        nodeid = nodeids[j]
+        ci.nodes[j] = nodeid
+        ci.coords[j] = ci.grid.nodes[nodeid].x
     end
+    return (ci, i+1)
 end
+
+@inline Base.done(ci::CellIterator, i) = i > getncells(ci.grid)
 
 # utility
 @inline getnodes(ci::CellIterator) = ci.nodes
 @inline getcoordinates(ci::CellIterator) = ci.coords
+@inline reinit!(ci::CellIterator, i::Int) = next(ci, i) # for manual updating
 
-@inline reinit!{dim, T}(cv::CellValues{dim, T}, ci::CellIterator{dim, T}) = reinit!(cv, ci.coords)
+@inline reinit!{dim, N, T}(cv::CellValues{dim, T}, ci::CellIterator{dim, N, T}) = reinit!(cv, ci.coords)
