ee893e65b97c4b0e0f6c60cbe64a6ed00d2d0c94
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2017-03-23 20:55:01 +0100

    test new notebooksä

--- test/test_notebooks.jl
+++ test/test_notebooks.jl
@@ -1,7 +1,23 @@
 # Run the notebook tests in separate modules
 
+
 module TestStiffness
     using NBInclude
     nbinclude("../examples/stiffness_example.ipynb")
+end
+
+module Cantilever
+    using NBInclude
     nbinclude("../examples/cantilever.ipynb")
 end
+
+module HyperElasticity
+    using NBInclude
+    nbinclude("../examples/hyperelasticity.ipynb")
+end
+
+module HeatSquare
+    using NBInclude
+    nbinclude("../examples/heat_square.ipynb")
+end
+
