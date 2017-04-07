bf9ae5d5a727dad8c890dfcf74f9212bf1eb03aa
Fredrik Ekre <fredrik.ekre@chalmers.se>
2017-04-07 11:53:17 +0200

    final commit i hope

--- test/test_notebooks.jl
+++ test/test_notebooks.jl
@@ -3,27 +3,27 @@
 
 module TestStiffness
     using NBInclude
-    # nbinclude("../examples/stiffness_example.ipynb")
+    nbinclude("../examples/stiffness_example.ipynb")
 end
 
 module Cantilever
     using NBInclude
-    # nbinclude("../examples/cantilever.ipynb")
+    nbinclude("../examples/cantilever.ipynb")
 end
 
 module HyperElasticity
     using NBInclude
-    # nbinclude("../examples/hyperelasticity.ipynb")
+    nbinclude("../examples/hyperelasticity.ipynb")
 end
 
 module HeatSquare
     using NBInclude
-    # nbinclude("../examples/heat_square.ipynb")
+    nbinclude("../examples/heat_square.ipynb")
 end
 
 module Helmholtz
     using NBInclude
-    # nbinclude("../examples/helmholtz.ipynb")
+    nbinclude("../examples/helmholtz.ipynb")
 end
 
 module Cook
