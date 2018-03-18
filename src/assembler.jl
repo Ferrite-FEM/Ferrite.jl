f1555b45cb56aa1e3cc014c9d65af7138225a8e9
Fredrik Ekre <fredrik.ekre@chalmers.se>
2018-03-18 14:19:52 +0100

    remove the old style assembler

--- src/assembler.jl
+++ src/assembler.jl
@@ -1,70 +1,3 @@
-struct Assembler{T}
-    I::Vector{Int}
-    J::Vector{Int}
-    V::Vector{T}
-end
-
-function Assembler(N)
-    I = Int[]
-    J = Int[]
-    V = Float64[]
-    sizehint!(I, N)
-    sizehint!(J, N)
-    sizehint!(V, N)
-
-    Assembler(I, J, V)
-end
-
-"""
-    start_assemble([N=0]) -> Assembler
-
-Call before starting an assembly.
-
-Returns an `Assembler` type that is used to hold the intermediate
-data before an assembly is finished.
-"""
-function start_assemble(N::Int=0)
-    return Assembler(N)
-end
-
-"""
-    assemble!(a, Ke, edof)
-
-Assembles the element matrix `Ke` into `a`.
-"""
-function assemble!(a::Assembler{T}, edof::AbstractVector{Int}, Ke::AbstractMatrix{T}) where {T}
-    n_dofs = length(edof)
-    append!(a.V, Ke)
-    @inbounds for j in 1:n_dofs
-        append!(a.I, edof)
-        for i in 1:n_dofs
-            push!(a.J, edof[j])
-        end
-    end
-end
-
-"""
-    end_assemble(a::Assembler) -> K
-
-Finalizes an assembly. Returns a sparse matrix with the
-assembled values.
-"""
-function end_assemble(a::Assembler)
-    return sparse(a.I, a.J, a.V)
-end
-
-"""
-    assemble!(g, ge, edof)
-
-Assembles the element residual `ge` into the global residual vector `g`.
-"""
-@propagate_inbounds function assemble!(g::AbstractVector{T}, edof::AbstractVector{Int}, ge::AbstractVector{T}) where {T}
-    @boundscheck checkbounds(g, edof)
-    @inbounds for i in 1:length(edof)
-        g[edof[i]] += ge[i]
-    end
-end
-
 abstract type AbstractSparseAssembler end
 
 struct AssemblerSparsityPattern{Tv,Ti} <: AbstractSparseAssembler
@@ -82,6 +15,15 @@ end
 
 @inline getsparsemat(a::AssemblerSparsityPattern) = a.K
 @inline getsparsemat(a::AssemblerSymmetricSparsityPattern) = a.K.data
+
+"""
+    start_assemble(K, f=[]) -> Assembler
+
+Call before starting an assembly. Return an `AssemblerSparsityPattern`
+type that caches some values needed when assembling into the global
+stiffness matrix and residual vector.
+"""
+start_assemble
 
 start_assemble(f::Vector, K::Union{SparseMatrixCSC, Symmetric}) = start_assemble(K, f)
 function start_assemble(K::SparseMatrixCSC, f::Vector=Float64[])
--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,6 @@ include("test_interpolations.jl")
 include("test_cellvalues.jl")
 include("test_facevalues.jl")
 include("test_quadrules.jl")
-include("test_assemble.jl")
 include("test_grid_dofhandler_vtk.jl")
 if VERSION.minor == 6
     include("test_notebooks.jl")
