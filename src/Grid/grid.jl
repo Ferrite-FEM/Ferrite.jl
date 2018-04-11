a8d976d4e9d4bba415c3d2dbf062a75f8d814b2c
Mohamed Tarek <mohamed82008@gmail.com>
2018-04-11 16:43:54 +1000

    Fix issue 189 (#190)
    * Fix issue 189
    
    https://github.com/KristofferC/JuAFEM.jl/issues/189
    
    * Update grid.jl

--- src/Grid/grid.jl
+++ src/Grid/grid.jl
@@ -129,7 +129,7 @@ function addcellset!(grid::Grid, name::String, f::Function; all::Bool=true)
         for node_idx in cell.nodes
             node = grid.nodes[node_idx]
             v = f(node.x)
-            all ? (!v && (pass = false; break)) : (v && break)
+            all ? (!v && (pass = false; break)) : (v && (pass = true; break))
         end
         pass && push!(cells, i)
     end
@@ -154,7 +154,7 @@ function addfaceset!(grid::Grid, name::String, f::Function; all::Bool=true)
             pass = all
             for node_idx in face
                 v = f(grid.nodes[node_idx].x)
-                all ? (!v && (pass = false; break)) : (v && break)
+                all ? (!v && (pass = false; break)) : (v && (pass = true; break))
             end
             pass && push!(faceset, (cell_idx, face_idx))
         end
