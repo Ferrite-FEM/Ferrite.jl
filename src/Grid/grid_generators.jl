09a108559e7a57a165ff39092093cc9c0e2dcae9
Harmen Stoppels <harmenstoppels@gmail.com>
2018-05-11 17:38:58 +0300

    Split cubes in 6 tetrahedrons that match on the faces when stacked

--- src/Grid/grid_generators.jl
+++ src/Grid/grid_generators.jl
@@ -315,58 +315,81 @@ function generate_grid(::Type{QuadraticTriangle}, nel::NTuple{2,Int}, LL::Vec{2,
 end
 
 # Tetrahedron
-function generate_grid(::Type{Tetrahedron}, nel::NTuple{3,Int}, left::Vec{3,T}=Vec{3}((-1.0,-1.0,-1.0)), right::Vec{3,T}=Vec{3}((1.0,1.0,1.0))) where {T}
-    nel_x = nel[1]; nel_y = nel[2]; nel_z = nel[3]; nel_tot = 5*nel_x*nel_y*nel_z
-    n_nodes_x = nel_x + 1; n_nodes_y = nel_y + 1; n_nodes_z = nel_z + 1
-    n_nodes = n_nodes_x * n_nodes_y * n_nodes_z
+function generate_grid(::Type{Tetrahedron}, cells_per_dim::NTuple{3,Int}, left::Vec{3,T}=Vec{3}((-1.0,-1.0,-1.0)), right::Vec{3,T}=Vec{3}((1.0,1.0,1.0))) where {T}
+    nodes_per_dim = cells_per_dim .+ 1
+
+    cells_per_cube = 6
+    total_nodes = prod(nodes_per_dim)
+    total_elements = cells_per_cube * prod(cells_per_dim)
+
+    n_nodes_x, n_nodes_y, n_nodes_z = nodes_per_dim
+    n_cells_x, n_cells_y, n_cells_z = cells_per_dim
 
     # Generate nodes
     coords_x = linspace(left[1], right[1], n_nodes_x)
     coords_y = linspace(left[2], right[2], n_nodes_y)
     coords_z = linspace(left[3], right[3], n_nodes_z)
-    nodes = Node{3,T}[]
-    for k in 1:n_nodes_z, j in 1:n_nodes_y, i in 1:n_nodes_x
-        push!(nodes, Node((coords_x[i], coords_y[j], coords_z[k])))
+    numbering = reshape(1:total_nodes, nodes_per_dim)
+
+    # Pre-allocate the nodes & cells
+    nodes = Vector{Node{3,T}}(total_nodes)
+    cells = Vector{Tetrahedron}(total_elements)
+    
+    # Generate nodes
+    node_idx = 1
+    @inbounds for k in 1:n_nodes_z, j in 1:n_nodes_y, i in 1:n_nodes_x
+        nodes[node_idx] = Node((coords_x[i], coords_y[j], coords_z[k]))
+        node_idx += 1
     end
 
-    # Generate cells, case 13 from: http://www.baumanneduard.ch/Splitting%20a%20cube%20in%20tetrahedras2.htm
-    node_array = reshape(collect(1:n_nodes), (n_nodes_x, n_nodes_y, n_nodes_z))
-    cells = Tetrahedron[]
-    for k in 1:nel_z, j in 1:nel_y, i in 1:nel_x
-        tmp = (node_array[i,j,k], node_array[i+1,j,k], node_array[i+1,j+1,k], node_array[i,j+1,k],
-               node_array[i,j,k+1], node_array[i+1,j,k+1], node_array[i+1,j+1,k+1], node_array[i,j+1,k+1])
-        push!(cells, Tetrahedron((tmp[1], tmp[2], tmp[4], tmp[5])))
-        push!(cells, Tetrahedron((tmp[2], tmp[3], tmp[4], tmp[7])))
-        push!(cells, Tetrahedron((tmp[2], tmp[4], tmp[5], tmp[7])))
-        push!(cells, Tetrahedron((tmp[2], tmp[5], tmp[6], tmp[7])))
-        push!(cells, Tetrahedron((tmp[4], tmp[5], tmp[7], tmp[8])))
+    # Generate cells, case 1 from: http://www.baumanneduard.ch/Splitting%20a%20cube%20in%20tetrahedras2.htm
+    # cube = (1, 2, 3, 4, 5, 6, 7, 8)
+    # left = (1, 4, 5, 8), right = (2, 3, 6, 7)
+    # front = (1, 2, 5, 6), back = (3, 4, 7, 8)
+    # bottom = (1, 2, 3, 4), top = (5, 6, 7, 8)
+    cell_idx = 0
+    @inbounds for k in 1:n_cells_z, j in 1:n_cells_y, i in 1:n_cells_x
+        cell = (
+            numbering[i  , j  , k],
+            numbering[i+1, j  , k], 
+            numbering[i+1, j+1, k],
+            numbering[i  , j+1, k],
+            numbering[i  , j  , k+1],
+            numbering[i+1, j  , k+1],
+            numbering[i+1, j+1, k+1],
+            numbering[i  , j+1, k+1]
+        )
+
+        cells[cell_idx + 1] = Tetrahedron((cell[1], cell[2], cell[4], cell[8]))
+        cells[cell_idx + 2] = Tetrahedron((cell[1], cell[5], cell[2], cell[8]))
+        cells[cell_idx + 3] = Tetrahedron((cell[2], cell[3], cell[4], cell[8]))
+        cells[cell_idx + 4] = Tetrahedron((cell[2], cell[7], cell[3], cell[8]))
+        cells[cell_idx + 5] = Tetrahedron((cell[2], cell[5], cell[6], cell[8]))
+        cells[cell_idx + 6] = Tetrahedron((cell[2], cell[6], cell[7], cell[8]))
+
+        cell_idx += cells_per_cube
     end
-    # Cell faces
-    cell_array = reshape(collect(1:nel_tot),(5, nel_x, nel_y, nel_z))
-    boundary = Tuple{Int,Int}[[(cl, 1) for cl in cell_array[1,:,:,1][:]];
-                              [(cl, 1) for cl in cell_array[2,:,:,1][:]];
-                              [(cl, 2) for cl in cell_array[1,:,1,:][:]];
-                              [(cl, 1) for cl in cell_array[4,:,1,:][:]];
-                              [(cl, 2) for cl in cell_array[2,end,:,:][:]];
-                              [(cl, 4) for cl in cell_array[4,end,:,:][:]];
-                              [(cl, 3) for cl in cell_array[2,:,end,:][:]];
-                              [(cl, 4) for cl in cell_array[5,:,end,:][:]];
-                              [(cl, 4) for cl in cell_array[1,1,:,:][:]];
-                              [(cl, 2) for cl in cell_array[5,1,:,:][:]];
-                              [(cl, 3) for cl in cell_array[4,:,:,end][:]];
-                              [(cl, 3) for cl in cell_array[5,:,:,end][:]]]
 
-    boundary_matrix = boundaries_to_sparse(boundary)
+    # Order the cells as c_nxyz[n, x, y, z] such that we can look up boundary cells
+    c_nxyz = reshape(1:total_elements, (cells_per_cube, cells_per_dim...))
 
-    # Cell face sets
-    offset = 0
-    facesets = Dict{String, Set{Tuple{Int,Int}}}()
-    facesets["bottom"] = Set{Tuple{Int,Int}}(boundary[(1:length([cell_array[1,:,:,1][:];   cell_array[2,:,:,1][:]]))   + offset]); offset += length([cell_array[1,:,:,1][:];   cell_array[2,:,:,1][:]])
-    facesets["front"]  = Set{Tuple{Int,Int}}(boundary[(1:length([cell_array[1,:,1,:][:];   cell_array[4,:,1,:][:]]))   + offset]); offset += length([cell_array[1,:,1,:][:];   cell_array[4,:,1,:][:]])
-    facesets["right"]  = Set{Tuple{Int,Int}}(boundary[(1:length([cell_array[2,end,:,:][:]; cell_array[4,end,:,:][:]])) + offset]); offset += length([cell_array[2,end,:,:][:]; cell_array[4,end,:,:][:]])
-    facesets["back"]   = Set{Tuple{Int,Int}}(boundary[(1:length([cell_array[2,:,end,:][:]; cell_array[5,:,end,:][:]])) + offset]); offset += length([cell_array[2,:,end,:][:]; cell_array[5,:,end,:][:]])
-    facesets["left"]   = Set{Tuple{Int,Int}}(boundary[(1:length([cell_array[1,1,:,:][:];   cell_array[5,1,:,:][:]]))   + offset]); offset += length([cell_array[1,1,:,:][:];   cell_array[5,1,:,:][:]])
-    facesets["top"]    = Set{Tuple{Int,Int}}(boundary[(1:length([cell_array[4,:,:,end][:]; cell_array[5,:,:,end][:]])) + offset]); offset += length([cell_array[4,:,:,end][:]; cell_array[5,:,:,end][:]])
+    @views le = [map(x -> (x,4), c_nxyz[1, 1, :, :][:])   ; map(x -> (x,2), c_nxyz[2, 1, :, :][:])]
+    @views ri = [map(x -> (x,1), c_nxyz[4, end, :, :][:]) ; map(x -> (x,1), c_nxyz[6, end, :, :][:])]
+    @views fr = [map(x -> (x,1), c_nxyz[2, :, 1, :][:])   ; map(x -> (x,1), c_nxyz[5, :, 1, :][:])]
+    @views ba = [map(x -> (x,3), c_nxyz[3, :, end, :][:]) ; map(x -> (x,3), c_nxyz[4, :, end, :][:])]
+    @views bo = [map(x -> (x,1), c_nxyz[1, :, :, 1][:])   ; map(x -> (x,1), c_nxyz[3, :, :, 1][:])]
+    @views to = [map(x -> (x,3), c_nxyz[5, :, :, end][:]) ; map(x -> (x,3), c_nxyz[6, :, :, end][:])]
+
+    boundary_matrix = boundaries_to_sparse([le; ri; bo; to; fr; ba])
+    
+    facesets = Dict(
+        "left" => Set(le),
+        "right" => Set(ri),
+        "front" => Set(fr),
+        "back" => Set(ba),
+        "bottom" => Set(bo),
+        "top" => Set(to),
+    )
 
     return Grid(cells, nodes, facesets=facesets, boundary_matrix=boundary_matrix)
 end
