905cb430e1d587400de45f8cff1308df9b7b398e
Jim Brouzoulis <jim.brouzoulis@chalmers.se>
2019-06-22 14:30:58 +0200

    Added missing method `getfieldnames`.

--- src/Dofs/DofHandler.jl
+++ src/Dofs/DofHandler.jl
@@ -50,6 +50,7 @@ ndofs(dh::AbstractDofHandler) = maximum(dh.cell_dofs)
 ndofs_per_cell(dh::AbstractDofHandler, cell::Int=1) = dh.cell_dofs_offset[cell+1] - dh.cell_dofs_offset[cell]
 isclosed(dh::AbstractDofHandler) = dh.closed[]
 nfields(dh::AbstractDofHandler) = length(dh.field_names)
+getfieldnames(dh::AbstractDofHandler) = dh.field_names
 ndim(dh::AbstractDofHandler, field_name::Symbol) = dh.field_dims[find_field(dh, field_name)]
 function find_field(dh::AbstractDofHandler, field_name::Symbol)
     j = findfirst(i->i == field_name, dh.field_names)
