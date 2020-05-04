090f33118a4cb635672f44efad2870a25df0fdca
kimauth <54391668+kimauth@users.noreply.github.com>
2020-05-04 18:53:04 +0200

    Fix CellIterator constructor (#285)
    In this function there is no grid argument, it is stored in the DofHandler.

--- src/iterators.jl
+++ src/iterators.jl
@@ -34,7 +34,7 @@ struct CellIterator{dim,C,T}
     celldofs::Vector{Int}
 
     function CellIterator{dim,C,T}(dh::Union{DofHandler{dim,C,T}, MixedDofHandler{dim,C,T}}, cellset::AbstractVector{Int}, flags::UpdateFlags) where {dim,C,T}
-        isconcretetype(C) || _check_same_celltype(grid, cellset)
+        isconcretetype(C) || _check_same_celltype(dh.grid, cellset)
         N = nnodes_per_cell(dh.grid, first(cellset))
         cell = ScalarWrapper(0)
         nodes = zeros(Int, N)
@@ -84,10 +84,10 @@ Base.eltype(::Type{T})         where {T<:CellIterator} = T
 
 function reinit!(ci::CellIterator{dim,C}, i::Int) where {dim,C}
     ci.current_cellid[] = ci.cellset[i]
-    
+
     ci.flags.nodes  && cellnodes!(ci.nodes, ci.dh, ci.current_cellid[])
     ci.flags.coords && cellcoords!(ci.coords, ci.dh, ci.current_cellid[])
-    
+
     if isdefined(ci, :dh) && ci.flags.celldofs # update celldofs
         celldofs!(ci.celldofs, ci.dh, ci.current_cellid[])
     end
