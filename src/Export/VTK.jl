e1a902a235345911da904234e052fadaba033190
Maximilian Köhler <maximilian.koehler@ruhr-uni-bochum.de>
2020-12-11 20:38:30 +0100

    accidentally removed a VTK_CELL mapping in export.jl, included it again, tests should work now (except documentation tests)

--- src/Export/VTK.jl
+++ src/Export/VTK.jl
@@ -3,6 +3,7 @@ cell_to_vtkcell(::Type{QuadraticLine}) = VTKCellTypes.VTK_QUADRATIC_EDGE
 
 cell_to_vtkcell(::Type{Quadrilateral}) = VTKCellTypes.VTK_QUAD
 cell_to_vtkcell(::Type{C}) where C<:AbstractCell{2,4,4} = VTKCellTypes.VTK_QUAD
+ cell_to_vtkcell(::Type{QuadraticQuadrilateral}) = VTKCellTypes.VTK_BIQUADRATIC_QUAD 
 cell_to_vtkcell(::Type{Triangle}) = VTKCellTypes.VTK_TRIANGLE
 cell_to_vtkcell(::Type{QuadraticTriangle}) = VTKCellTypes.VTK_QUADRATIC_TRIANGLE
 cell_to_vtkcell(::Type{Cell{2,8,4}}) = VTKCellTypes.VTK_QUADRATIC_QUAD
