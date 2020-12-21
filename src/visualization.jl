cac3c19130e7ab3d0d3a73c94c3e0a7c3f2ba68f
Maximilian Köhler <maximilian.koehler@ruhr-uni-bochum.de>
2020-12-21 15:56:09 +0100

    more generic code for future higher order cells and use AbstractDofHandler instead of DofHandler

--- src/visualization.jl
+++ src/visualization.jl
@@ -7,7 +7,7 @@ function postprocess(node_values)
     end 
 end
 
-function dof_to_node(dh::DofHandler, u::Array{T,1}; field::Int=1, process::Function=postprocess) where T
+function dof_to_node(dh::AbstractDofHandler, u::Array{T,1}; field::Int=1, process::Function=postprocess) where T
     fieldnames = JuAFEM.getfieldnames(dh)  
     field_dim = getfielddim(dh, field)
     data = fill(NaN, getnnodes(dh.grid), field_dim) 
@@ -26,22 +26,22 @@ function dof_to_node(dh::DofHandler, u::Array{T,1}; field::Int=1, process::Funct
     return mapslices(process, data, dims=[2])
 end
 
-to_triangle(::Union{Type{Triangle},Type{QuadraticTriangle}}, elements) = elements[:,1:3]
-to_triangle(::Union{Type{Tetrahedron},Type{QuadraticTetrahedron}}, elements) = vcat(elements[:,[1,2,3]], elements[:,[1,2,4]], elements[:,[2,3,4]], elements[:,[1,4,3]])
-to_triangle(::Union{Type{Quadrilateral},Type{QuadraticQuadrilateral}}, elements) = vcat(elements[:,[1,2,3]], elements[:,[3,4,1]])
-to_triangle(::Union{Type{Hexahedron},Type{QuadraticHexahedron}}, elements) = vcat(elements[:,[1,2,3]], elements[:,[3,4,1]], elements[:,[1,5,6]], elements[:,[6,2,1]], elements[:,[2,6,7]], elements[:,[7,3,2]], elements[:,[3,7,8]], elements[:,[8,4,3]], elements[:,[1,4,8]], elements[:,[8,5,1]], elements[:,[5,8,7]], elements[:,[7,6,5]])
+to_triangle(::Type{<:AbstractCell{2,N,3}}, elements) where N = elements[:,1:3]
+to_triangle(::Type{<:AbstractCell{3,N,4}}, elements) where N = vcat(elements[:,[1,2,3]], elements[:,[1,2,4]], elements[:,[2,3,4]], elements[:,[1,4,3]])
+to_triangle(::Type{<:AbstractCell{2,N,4}}, elements) where N = vcat(elements[:,[1,2,3]], elements[:,[3,4,1]])
+to_triangle(::Type{<:AbstractCell{3,N,6}}, elements) where N = vcat(elements[:,[1,2,3]], elements[:,[3,4,1]], elements[:,[1,5,6]], elements[:,[6,2,1]], elements[:,[2,6,7]], elements[:,[7,3,2]], elements[:,[3,7,8]], elements[:,[8,4,3]], elements[:,[1,4,8]], elements[:,[8,5,1]], elements[:,[5,8,7]], elements[:,[7,6,5]])
 
-AbstractPlotting.plottype(::DofHandler{1,C,T}, ::Array{T,1}) where {C,T} = AbstractPlotting.lines
-AbstractPlotting.plottype(::DofHandler{dim,C,T}, ::Array{T,1}) where {dim,C,T} = AbstractPlotting.mesh
+AbstractPlotting.plottype(::Union{DofHandler{1,C,T},MixedDofHandler{1,T}}, ::Array{T,1}) where {C,T} = AbstractPlotting.lines
+AbstractPlotting.plottype(::Union{DofHandler{dim,C,T},MixedDofHandler{dim,T}}, ::Array{T,1}) where {dim,C,T} = AbstractPlotting.mesh
 
-function AbstractPlotting.convert_arguments(::AbstractPlotting.PointBased, dh::DofHandler{1,C,T}, u::Array{T,1}) where {C,T}
+function AbstractPlotting.convert_arguments(::AbstractPlotting.PointBased, dh::Union{DofHandler{1,C,T},MixedDofHandler{1,T}}, u::Array{T,1}) where {C,T}
     nodes = getnodes(dh.grid)
     coords = [node.x[1] for node in nodes]
     solution = dof_to_node(dh, u) 
     return ([AbstractPlotting.Point2f0(coords[i], solution[i]) for i in 1:getnnodes(dh.grid)],)
 end
 
-function AbstractPlotting.mesh(dh::DofHandler, u::Array{T,1}, args...; field::Int=1, process::Function=postprocess, scale_plot=false, shading=false, kwargs...) where T
+function AbstractPlotting.mesh(dh::AbstractDofHandler, u::Array{T,1}, args...; field::Int=1, process::Function=postprocess, scale_plot=false, shading=false, kwargs...) where T
     C = getcelltype(dh.grid)
     nodes = getnodes(dh.grid)
     cells = getcells(dh.grid)
@@ -54,7 +54,7 @@ function AbstractPlotting.mesh(dh::DofHandler, u::Array{T,1}, args...; field::In
     return AbstractPlotting.mesh(coords, triangle_elements, color=solution, args...; scale_plot=scale_plot, shading=shading, kwargs...)
 end
 
-function AbstractPlotting.mesh!(dh::DofHandler, u::Array{T,1}, args...; field::Int=1, process::Function=postprocess, scale_plot=false, shading=false, kwargs...) where T
+function AbstractPlotting.mesh!(dh::AbstractDofHandler, u::Array{T,1}, args...; field::Int=1, process::Function=postprocess, scale_plot=false, shading=false, kwargs...) where T
     C = getcelltype(dh.grid)
     nodes = getnodes(dh.grid)
     cells = getcells(dh.grid)
@@ -67,7 +67,7 @@ function AbstractPlotting.mesh!(dh::DofHandler, u::Array{T,1}, args...; field::I
     return AbstractPlotting.mesh!(coords, triangle_elements, color=solution, args...; scale_plot=scale_plot, shading=shading, kwargs...)
 end
 
-function AbstractPlotting.surface(dh::DofHandler, u::Array{T,1}, args...; field::Int=1, process::Function=postprocess, scale_plot=false, shading=false, kwargs...) where T
+function AbstractPlotting.surface(dh::AbstractDofHandler, u::Array{T,1}, args...; field::Int=1, process::Function=postprocess, scale_plot=false, shading=false, kwargs...) where T
     @assert getdim(dh.grid) == 2 "Only 2D solutions supported!"
     C = getcelltype(dh.grid)
     nodes = getnodes(dh.grid)
@@ -76,13 +76,13 @@ function AbstractPlotting.surface(dh::DofHandler, u::Array{T,1}, args...; field:
     connectivity = getproperty.(cells, :nodes)
     N = length(vertices(cells[1]))
     elements = [element[i] for element in connectivity, i in 1:N]
-    solution = reshape(dof_to_node(dh, u; field=field, process=postprocess), getnnodes(dh.grid))
+    solution = reshape(dof_to_node(dh, u; field=field, process=process), getnnodes(dh.grid))
     points = [AbstractPlotting.Point3f0(coord[1], coord[2], solution[idx]) for (idx, coord) in enumerate(eachrow(coords))]
     triangle_elements = to_triangle(C, elements)  
     return AbstractPlotting.mesh(points, triangle_elements, color=solution, args...; scale_plot=scale_plot, shading=shading, kwargs...)
 end
 
-function AbstractPlotting.surface!(dh::DofHandler, u::Array{T,1}, args...; field::Int=1, process::Function=postprocess, scale_plot=false, shading=false, kwargs...) where T
+function AbstractPlotting.surface!(dh::AbstractDofHandler, u::Array{T,1}, args...; field::Int=1, process::Function=postprocess, scale_plot=false, shading=false, kwargs...) where T
     @assert getdim(dh.grid) == 2 "Only 2D solutions supported!"
     C = getcelltype(dh.grid)
     nodes = getnodes(dh.grid)
@@ -91,13 +91,13 @@ function AbstractPlotting.surface!(dh::DofHandler, u::Array{T,1}, args...; field
     connectivity = getproperty.(cells, :nodes)
     N = length(vertices(cells[1]))
     elements = [element[i] for element in connectivity, i in 1:N]
-    solution = reshape(dof_to_node(dh, u; field=field, process=postprocess), getnnodes(dh.grid))
+    solution = reshape(dof_to_node(dh, u; field=field, process=process), getnnodes(dh.grid))
     points = [AbstractPlotting.Point3f0(coord[1], coord[2], solution[idx]) for (idx, coord) in enumerate(eachrow(coords))]
     triangle_elements = to_triangle(C, elements)  
     return AbstractPlotting.mesh!(points, triangle_elements, color=solution, args...; scale_plot=scale_plot, shading=shading, kwargs...)
 end
 
-function AbstractPlotting.arrows(dh::DofHandler, u::Array{T,1}, args...; field::Int=1, arrowsize=0.08, normalize=true, kwargs...) where T
+function AbstractPlotting.arrows(dh::AbstractDofHandler, u::Array{T,1}, args...; field::Int=1, arrowsize=0.08, normalize=true, kwargs...) where T
     C = getcelltype(dh.grid)
     nodes = getnodes(dh.grid)
     cells = getcells(dh.grid)
@@ -114,7 +114,7 @@ function AbstractPlotting.arrows(dh::DofHandler, u::Array{T,1}, args...; field::
     end
 end
 
-function AbstractPlotting.arrows!(dh::DofHandler, u::Array{T,1}, args...; field::Int=1, arrowsize=0.08, normalize=false, kwargs...) where T
+function AbstractPlotting.arrows!(dh::AbstractDofHandler, u::Array{T,1}, args...; field::Int=1, arrowsize=0.08, normalize=false, kwargs...) where T
     C = getcelltype(dh.grid)
     nodes = getnodes(dh.grid)
     cells = getcells(dh.grid)
@@ -131,7 +131,7 @@ function AbstractPlotting.arrows!(dh::DofHandler, u::Array{T,1}, args...; field:
     end
 end
 
-function warp_by_vector(dh::DofHandler, u::Array{T,1}, args...; field::Int=1, scale=1.0, process::Function=postprocess, scale_plot=false, shading=false, kwargs...) where T
+function warp_by_vector(dh::AbstractDofHandler, u::Array{T,1}, args...; field::Int=1, scale=1.0, process::Function=postprocess, scale_plot=false, shading=false, kwargs...) where T
     C = getcelltype(dh.grid)
     nodes = getnodes(dh.grid)
     cells = getcells(dh.grid)
