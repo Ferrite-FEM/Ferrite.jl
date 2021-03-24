40ede7ef6f8bf6704b440fbe8516f08b4d37a86d
Elias <elias.isak.borjesson@gmail.com>
2021-03-24 15:24:02 +0100

    Change tutorial a bit Add image add todo to dofhandler move code to BcValue constructor

--- src/FEValues/face_values.jl
+++ src/FEValues/face_values.jl
@@ -215,10 +215,10 @@ struct BCValues{T}
     current_face::ScalarWrapper{Int}
 end
 
-BCValues(func_interpol::Interpolation, geom_interpol::Interpolation, _face_edge_vertex::Function = JuAFEM.faces) =
-    BCValues(Float64, func_interpol, geom_interpol, _face_edge_vertex)
+BCValues(func_interpol::Interpolation, geom_interpol::Interpolation, boundary_type::Union{Type{FaceIndex}, Type{EdgeIndex}, Type{VertexIndex}}) =
+    BCValues(Float64, func_interpol, geom_interpol, getgeometryfunction(boundary_type))
 
-function BCValues(::Type{T}, func_interpol::Interpolation{dim,refshape}, geom_interpol::Interpolation{dim,refshape}, _face_edge_vertex::Function = JuAFEM.faces) where {T,dim,refshape}
+function BCValues(::Type{T}, func_interpol::Interpolation{dim,refshape}, geom_interpol::Interpolation{dim,refshape}, _face_edge_vertex::Function) where {T,dim,refshape}
     # set up quadrature rules for each face with dof-positions
     # (determined by func_interpol) as the quadrature points
     interpolation_coords = reference_coordinates(func_interpol)
