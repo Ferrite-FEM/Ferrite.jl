2f8fc9db449d2df07133093dafe89fffa01b973a
kimauth <auth@chalmers.se>
2021-04-06 14:43:00 +0200

    clarify docstring of function_gradient

--- src/FEValues/common_values.jl
+++ src/FEValues/common_values.jl
@@ -142,10 +142,11 @@ for the degrees of freedom. For a scalar valued function, `u` contains scalars.
 For a vector valued function, `u` can be a vector of scalars (for use of `VectorValues`)
 or `u` can be a vector of `Vec`s (for use with ScalarValues).
 
-The gradient of a scalar function is computed as
-``\\mathbf{\\nabla} u(\\mathbf{x}) = \\sum\\limits_{i = 1}^n \\mathbf{\\nabla} N_i (\\mathbf{x}) u_i``
+The gradient of a scalar function or a vector valued function with use of `VectorValues` is computed as
+``\\mathbf{\\nabla} u(\\mathbf{x}) = \\sum\\limits_{i = 1}^n \\mathbf{\\nabla} N_i (\\mathbf{x}) u_i`` or
+``\\mathbf{\\nabla} u(\\mathbf{x}) = \\sum\\limits_{i = 1}^n \\mathbf{\\nabla} \\mathbf{N}_i (\\mathbf{x}) u_i`` respectively,
 where ``u_i`` are the nodal values of the function.
-For a vector valued function the gradient is computed as
+For a vector valued function with use of `ScalarValues` the gradient is computed as
 ``\\mathbf{\\nabla} \\mathbf{u}(\\mathbf{x}) = \\sum\\limits_{i = 1}^n \\mathbf{u}_i \\otimes \\mathbf{\\nabla} N_i (\\mathbf{x})``
 where ``\\mathbf{u}_i`` are the nodal values of ``\\mathbf{u}``.
 """
