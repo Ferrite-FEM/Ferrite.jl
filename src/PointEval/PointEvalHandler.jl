ffa1f8d9e33a45bc840663d57c45f2c041434974
kimauth <auth@chalmers.se>
2021-08-02 19:50:49 +0200

    store local_coords in PointEvalHandler

--- src/PointEval/PointEvalHandler.jl
+++ src/PointEval/PointEvalHandler.jl
@@ -1,81 +1,79 @@
 struct PointEvalHandler{DH<:AbstractDofHandler,dim,T<:Real}
     dh::DH
     cells::Vector{Union{Missing, Int}}
-    cellvalues::Vector{PointScalarValues{dim,T}} # TODO: store local coordinates instead of PointScalarValues (can we toss the PointScalarValues in that case?)
+    local_coords::Vector{Vec{dim,T}} # TODO: store local coordinates instead of PointScalarValues (can we toss the PointScalarValues in that case?)
 end
 
 # TODO write show method for PointEvalHandler
 
-# TODO rewrite this like for MixedDofHandler
-function PointEvalHandler(dh::AbstractDofHandler, func_interpolations::Vector{<:Interpolation{dim}},
-    points::AbstractVector{Vec{dim, T}}, geom_interpolations::Vector{<:Interpolation{dim}}=func_interpolations;
+function PointEvalHandler(dh::AbstractDofHandler, points::AbstractVector{Vec{dim, T}},
+    geom_interpolations::Vector{<:Interpolation{dim}}=get_default_geom_interpolations(dh);
     ) where {dim, T<:Real}
 
-    grid = dh.grid
-    # set up tree structure for finding nearest nodes to points
-    kdtree = KDTree([node.x for node in grid.nodes])
-    nearest_nodes, dists = knn(kdtree, points, 3, true) #TODO 3 is a random value, it shouldn't matter because likely the nearest node is the one we want
+    node_cell_dicts = [_get_node_cell_map(grid) for field_idx in 1:nfields(dh)] # why do we do this per field?
+    cells, local_coords = _get_cellcoords(points, dh.grid, node_cell_dicts, geom_interpolations)
 
-    cells = Vector{Union{Missing, Int}}(undef, length(points))
-    pvs = Vector{PointScalarValues{dim, T}}(undef, length(points))
-    node_cell_dicts = [_get_node_cell_map(grid) for field_idx in 1:nfields(dh)]
-
-    _setup_pointscalarvalues!(cells, pvs, node_cell_dicts, dh, points, nearest_nodes, func_interpolations, geom_interpolations)
-    return PointEvalHandler(dh, cells, pvs)
+    return PointEvalHandler(dh, cells, local_coords)
 end
 
 # function interpolations need to be explicitely given, because we don't know which field we are looking for.
 # Only one field at a time can be interpolated, so one function interpolation per FieldHandler (= per cellset) is sufficient.
 # If several fields should be interpolated with different function interpolations, several PointEvalHandlers need to be constructed.
-function PointEvalHandler(dh::MixedDofHandler{dim, T}, func_interpolations::Vector{<:Interpolation{dim}},
-    points::AbstractVector{Vec{dim, T}}, geom_interpolations::Vector{<:Interpolation{dim}}=func_interpolations;
+function PointEvalHandler(dh::MixedDofHandler{dim, T}, points::AbstractVector{Vec{dim, T}},
+    geom_interpolations::Vector{<:Interpolation{dim}}=get_default_geom_interpolations(dh);
     ) where {dim, T<:Real}
+
     # TODO: test that geom_interpolation is compatible with grid (use this as a default)
-    grid = dh.grid
-    # set up tree structure for finding nearest nodes to points
-    kdtree = KDTree([node.x for node in grid.nodes])
-    nearest_nodes, dists = knn(kdtree, points, 3, true) #TODO 3 is a random value, it shouldn't matter because likely the nearest node is the one we want
 
-    cells = Vector{Union{Missing, Int}}(undef, length(points))
-    pvs = Vector{PointScalarValues{dim, T}}(undef, length(points))
     node_cell_dicts = [_get_node_cell_map(grid, fh.cellset) for fh in dh.fieldhandlers]
+    cells, local_coords = _get_cellcoords(points, dh.grid, node_cell_dicts, geom_interpolations)
 
-    _setup_pointscalarvalues!(cells, pvs, node_cell_dicts, dh, points, nearest_nodes, func_interpolations, geom_interpolations)
-   return PointEvalHandler(dh, cells, pvs)
+   return PointEvalHandler(dh, cells, local_coords)
 end
 
-function _setup_pointscalarvalues!(cells::Vector{Union{Missing,Int}}, pvs::Vector{PointScalarValues{dim,T}},
-     node_cell_dicts::Vector{Dict{Int, Vector{Int}}}, dh, points::AbstractVector{Vec{dim,T}}, nearest_nodes,
-     func_interpolations::Vector{<:Interpolation{dim}}, geom_interpolations::Vector{<:Interpolation{dim}}) where {dim, T<:Real}
+get_default_geom_interpolations(dh::DofHandler{dim}) = [default_interpolation(first(typeof(dh.grid.cells)))]
+
+function get_default_geom_interpolations(dh::MixedDofHandler{dim}) where {dim}
+    ips = Interpolation{dim}[]
+    for fh in dh.fieldhandlers
+        push!(ips, default_interpolation(typeof(grid.cells[first(fh.cellset)])))
+    end
+    return ips
+end
+
+function _get_cellcoords(points::AbstractVector{Vec{dim,T}}, grid::Grid, node_cell_dicts::Vector{Dict{Int, Vector{Int}}}, 
+     geom_interpolations::Vector{<:Interpolation{dim}}) where {dim, T<:Real}
+
+    # set up tree structure for finding nearest nodes to points
+    kdtree = KDTree([node.x for node in grid.nodes])
+    nearest_nodes, _ = knn(kdtree, points, 3, true) #TODO 3 is a random value, it shouldn't matter because likely the nearest node is the one we want
+
+    cells = Vector{Union{Missing, Int}}(undef, length(points))
+    local_coords = Vector{Vec{dim, T}}(undef, length(points))
+
     for point_idx in 1:length(points)
         cell_found = false
-        for field_idx in 1:nfields(dh)
-            node_cell_dict = node_cell_dicts[field_idx]
-            func_interpol = func_interpolations[field_idx]
-            geom_interpol = geom_interpolations[field_idx]
+        for dict_idx in 1:length(node_cell_dicts) # all cells in a node_cell_dict have the same geom_interpolation
+            node_cell_dict = node_cell_dicts[dict_idx]
+            geom_interpol = geom_interpolations[dict_idx]
             # loop over points
             for node in nearest_nodes[point_idx], cell in get(node_cell_dict, node, [0])
                 # if node is not part of the fieldhandler, try the next node
                 cell == 0 ? continue : nothing
 
-                cell_coords = getcoordinates(dh.grid, cell)
-                # TODO: let point_incell return Bool and Coordinate, compute one, put if condition only on bool - toss coordinate if false
-                found_coord, local_coordinate = point_in_cell(geom_interpol, cell_coords, points[point_idx])
-                if found_coord
+                cell_coords = getcoordinates(grid, cell)
+                is_in_cell, local_coord = point_in_cell(geom_interpol, cell_coords, points[point_idx])
+                if is_in_cell
                     cell_found = true
-                    # retrieve RefShape for QuadratureRule
-                    refshape = getrefshape(func_interpol)
-                    point_qr = QuadratureRule{dim, refshape, T}([1], [local_coordinate])
                     cells[point_idx] = cell
-                    # since these are unique for each point to evaluate, we need to create a cellvalue for each point anyway, so we might as well do it all at once
-                    pv = PointScalarValues(point_qr, func_interpol)
-                    pvs[point_idx] = pv
+                    local_coords[point_idx] = local_coord
                     break
                 end
             end
         end
         !cell_found && (cells[point_idx] = missing) #error("No cell found for point $(points[point_idx]), index $point_idx.")
     end
+    return cells, local_coords
 end
 
 # check if point is inside a cell based on physical coordinate
@@ -159,9 +157,17 @@ function _get_node_cell_map(grid::Grid, cellset::Set{Int}=Set{Int64}(1:getncells
     return cell_dict
 end
 
-# values in nodal order
-# can't be used for sub/superparametric approximations
-function get_point_values(ph::PointEvalHandler, nodal_values::Vector{T}) where {T<:Union{Real, AbstractTensor}}
+# some useful code:
+# # values in nodal order
+# # can't be used for sub/superparametric approximations
+# func_interpolations::Vector{<:Interpolation{dim}}
+
+# # retrieve RefShape for QuadratureRule
+# refshape = getrefshape(func_interpol)
+# point_qr = QuadratureRule{dim, refshape, T}([1], [local_coordinate])
+# pv = PointScalarValues(point_qr, func_interpol)
+
+function get_point_values(ph::PointEvalHandler, nodal_values::Vector{T}, func_interpolations = get_default_geom_interpolations(dh)) where {T<:Union{Real, AbstractTensor}}
     # TODO check for sub/superparametric approximations
 
     # if interpolation !== default_interpolation(typeof(ch.dh.grid.cells[first(cellset)]))
@@ -172,6 +178,8 @@ function get_point_values(ph::PointEvalHandler, nodal_values::Vector{T}) where {
     npoints = length(ph.cells)
     vals = Vector{T}(undef, npoints)
     for i in eachindex(ph.cells)
+        # TODO: need the PointScalarValues now. Can either compute them here or supply function for computing all of them and store in a Vector
+        # Anyways: We need the fieldhandler the cells belong to. Should they be stored in the PointEvalHandler?
         node_idxs = getcells(ph.dh.grid, ph.cells[i]).nodes
         vals[i] = function_value(ph.cellvalues[i], 1, [nodal_values[node_idx] for node_idx in node_idxs])
     end
