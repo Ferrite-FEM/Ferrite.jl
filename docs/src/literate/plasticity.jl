dff7b0d55c2c67e6e7d83649b8e6631827420907
Dennis Ogiermann <termi-official@users.noreply.github.com>
2021-09-10 12:19:04 +0200

    Derp.

--- docs/src/literate/plasticity.jl
+++ docs/src/literate/plasticity.jl
@@ -380,8 +380,8 @@ end
 u_max, traction_magnitude = solve();
 
 # Finally we plot the load-displacement curve.
-ENV["PLOTS_TEST"] = "true"  #src
-ENV["GKS_WSTYPE"]=100       #src
+ENV["PLOTS_TEST"] = "true"
+ENV["GKS_WSTYPE"] = 100
 using Plots
 plot(
     vcat(0.0, u_max),                # add the origin as a point
