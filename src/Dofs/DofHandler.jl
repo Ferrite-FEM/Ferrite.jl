b04728dd93301ab4aa83a271ded366c8ff38d631
Maximilian Köhler <maximilian.koehler@ruhr-uni-bochum.de>
2021-10-11 23:24:36 +0200

    added test for subtyping <:AbstractGrid and using it with DofHandler

--- src/Dofs/DofHandler.jl
+++ src/Dofs/DofHandler.jl
@@ -25,7 +25,7 @@ struct DofHandler{dim,T,G<:AbstractGrid{dim}} <: AbstractDofHandler
     ndofs::ScalarWrapper{Int}
 end
 
-function DofHandler(grid::Grid)
+function DofHandler(grid::AbstractGrid)
     isconcretetype(getcelltype(grid)) || error("Grid includes different celltypes. Use MixedDofHandler instead of DofHandler")
     DofHandler(Symbol[], Int[], Interpolation[], BCValues{Float64}[], Int[], Int[], ScalarWrapper(false), grid, Ferrite.ScalarWrapper(-1))
 end
