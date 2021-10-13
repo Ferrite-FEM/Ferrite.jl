c95c5415aa28a5fae18dbe2a0fe2b95229eeb0a2
termi-official <termi-official@users.noreply.github.com>
2021-10-14 00:35:18 +0200

    Reduce benchmark workload.

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -45,30 +45,33 @@ SUITE["dof-management"] = BenchmarkGroup()
 SUITE["dof-management"]["numbering"] = BenchmarkGroup()
 # !!! NOTE close! must wrapped into a custom function, because consecutive calls to close!, since the dofs are already distributed.
 NUMBERING_SUITE = SUITE["dof-management"]["numbering"]
-for spatial_dim ∈ 1:3
+for spatial_dim ∈ [3]# 1:3
     NUMBERING_SUITE["spatial-dim",spatial_dim] = BenchmarkGroup()
     for geo_type ∈ FerriteBenchmarkHelper.geo_types_for_spatial_dim(spatial_dim)
         NUMBERING_SUITE["spatial-dim",spatial_dim][string(geo_type)] = BenchmarkGroup()
 
         ref_type = FerriteBenchmarkHelper.default_refshape(geo_type)
 
-        for grid_size ∈ [3, 6, 9] #multiple grid sized to estimate computational complexity...
+        for grid_size ∈ [2]#[3, 6, 9] #multiple grid sized to estimate computational complexity...
             NUMBERING_SUITE["spatial-dim",spatial_dim][string(geo_type)]["grid-size-",grid_size] = BenchmarkGroup()
             NUMBERING_SUITE["spatial-dim",spatial_dim][string(geo_type)]["grid-size-",grid_size] = BenchmarkGroup()
 
             grid = generate_grid(geo_type, ntuple(x->grid_size, spatial_dim));
 
-            for field_dim ∈ 1:3
+            for field_dim ∈ [3]#1:3
                 NUMBERING_SUITE["spatial-dim",spatial_dim][string(geo_type)]["grid-size-",grid_size]["field-dim-", field_dim] = BenchmarkGroup()
                 NUMBERING_FIELD_DIM_SUITE = NUMBERING_SUITE["spatial-dim",spatial_dim][string(geo_type)]["grid-size-",grid_size]["field-dim-", field_dim]
                 # Lagrange tests
                 for order ∈ 1:2
-                    # higher order Lagrange on hex not working yet...
-                    order > 1 && geo_type ∈ [Hexahedron] && continue
+                    ip = Lagrange{spatial_dim, ref_type, order}()
+
+                    # Skip over elements which are not implemented
+                    ξ_dummy = Vec{spatial_dim}(ntuple(x->0.0, spatial_dim))
+                    !applicable(Ferrite.value, ip, 1, ξ_dummy) && continue
+                    !applicable(Ferrite.value, ip, 1, ξ_dummy) && continue
 
                     NUMBERING_FIELD_DIM_SUITE["Lagrange",order] = BenchmarkGroup()
                     LAGRANGE_SUITE = NUMBERING_FIELD_DIM_SUITE["Lagrange",order]
-                    ip = Lagrange{spatial_dim, ref_type, order}()
                     order2 = max(order-1, 1)
                     ip2 = Lagrange{spatial_dim, ref_type, order2}()
 
@@ -346,7 +349,7 @@ for spatial_dim ∈ [2]
     DIRICHLET_SUITE["global"] = BenchmarkGroup()
 
     geo_type = Quadrilateral
-    grid = generate_grid(geo_type, tuple([spatial_dim, spatial_dim]));
+    grid = generate_grid(geo_type, ntuple(x->2, spatial_dim));
     ref_type = FerriteBenchmarkHelper.default_refshape(geo_type)
     ip_geo = Ferrite.default_interpolation(geo_type)
     order = 2
