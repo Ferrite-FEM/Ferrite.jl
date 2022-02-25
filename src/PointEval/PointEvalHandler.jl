4fa513542de9a914c7c832a1012ed8a10eba3c3a
Fredrik Ekre <ekrefredrik@gmail.com>
2022-02-25 10:12:26 +0100

    PointEval: Mention NaNs in output for points that were not found.

--- src/PointEval/PointEvalHandler.jl
+++ src/PointEval/PointEvalHandler.jl
@@ -162,11 +162,18 @@ function _get_node_cell_map(grid::AbstractGrid)
 end
 
 """
-    get_point_values(ph::PointEvalHandler, proj::L2Projector, dof_values::Vector{T}) where T
     get_point_values(ph::PointEvalHandler, dh::AbstractDofHandler, dof_values::Vector{T}, [fieldname::Symbol]) where T
+    get_point_values(ph::PointEvalHandler, proj::L2Projector, dof_values::Vector{T}) where T
 
-Return a `Vector{T}` (for a 1-dimensional field) or a `Vector{Vec{fielddim, T}}` (for a vector field) with the field values of field `fieldname` in the points of the `PointEvalHandler`. The `fieldname` can be omitted if only one field is stored in `dh`. The field values are computed based on the `dof_values` and interpolated to the local coordinates by the function interpolation of the corresponding `field` stored in the `AbstractDofHandler` or the `L2Projector`.
+Return a `Vector{T}` (for a 1-dimensional field) or a `Vector{Vec{fielddim, T}}` (for a
+vector field) with the field values of field `fieldname` in the points of the
+`PointEvalHandler`. The `fieldname` can be omitted if only one field is stored in `dh`.
+The field values are computed based on the `dof_values` and interpolated to the local
+coordinates by the function interpolation of the corresponding `field` stored in the
+`AbstractDofHandler` or the `L2Projector`.
 
+Points that could not be found in the domain when constructing the `PointEvalHandler` will
+have `NaN`s for the corresponding entries in the output vector.
 """
 get_point_values
 
