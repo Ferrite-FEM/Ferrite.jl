f1f8a80e78e0fd42ab57a7cecf150844723a7844
Maximilian Köhler <maximilian.koehler@ruhr-uni-bochum.de>
2022-04-22 08:54:09 +0200

    typo

--- src/Dofs/DofHandler.jl
+++ src/Dofs/DofHandler.jl
@@ -317,7 +317,7 @@ function cellcoords!(global_coords::Vector{Vec{dim,T}}, grid::AbstractGrid{dim},
     N = length(nodes)
     @assert length(global_coords) == N
     for j in 1:N
-        global_coords[j] = getnodes(grid.nodes,nodes[j]).x
+        global_coords[j] = getnodes(grid,nodes[j]).x
     end
     return global_coords
 end
