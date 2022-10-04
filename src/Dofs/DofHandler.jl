bfaebfdccdb09496b3332aa57e0f9107aae72b91
Maximilian Köhler <maximilian.koehler@ruhr-uni-bochum.de>
2022-10-04 14:58:33 +0200

    remove unneccessary type parametrization

--- src/Dofs/DofHandler.jl
+++ src/Dofs/DofHandler.jl
@@ -302,7 +302,7 @@ function celldofs!(global_dofs::Vector{Int}, dh::DofHandler, i::Int)
     return global_dofs
 end
 
-function cellnodes!(global_nodes::Vector{Int}, grid::AbstractGrid{dim}, i::Int) where {dim,C}
+function cellnodes!(global_nodes::Vector{Int}, grid::AbstractGrid{dim}, i::Int) where {dim}
     nodes = getcells(grid,i).nodes
     N = length(nodes)
     @assert length(global_nodes) == N
@@ -312,7 +312,7 @@ function cellnodes!(global_nodes::Vector{Int}, grid::AbstractGrid{dim}, i::Int) 
     return global_nodes
 end
 
-function cellcoords!(global_coords::Vector{Vec{dim,T}}, grid::AbstractGrid{dim}, i::Int) where {dim,C,T}
+function cellcoords!(global_coords::Vector{Vec{dim,T}}, grid::AbstractGrid{dim}, i::Int) where {dim,T}
     nodes = getcells(grid,i).nodes
     N = length(nodes)
     @assert length(global_coords) == N
