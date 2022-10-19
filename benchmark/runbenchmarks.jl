80ce23ed362da6de4c4211837d34f5490e9b9161
termi-official <termi-official@users.noreply.github.com>
2022-10-19 15:31:16 +0200

    Wrap benchmarks into subproject.

---
+++ benchmark/runbenchmarks.jl
@@ -0,0 +1,12 @@
+using PkgBenchmark
+
+benchmarkpkg(
+    dirname(@__DIR__),
+    BenchmarkConfig(
+        env = Dict(
+            "JULIA_NUM_THREADS" => "1",
+            "OMP_NUM_THREADS" => "1",
+        )
+    ),
+    resultfile = joinpath(@__DIR__, "result.json"),
+)
