269d867c25afe01198bb0670b597766a703f508c
Knut Andreas Meyer <knutam@gmail.com>
2022-10-30 15:46:52 +0100

    Fix tests for julia 1.6

--- test/test_initialvalues.jl
+++ test/test_initialvalues.jl
@@ -152,12 +152,12 @@
 
     @testset "Exceptions" begin
         dh = testdh(Quadrilateral, 1, 1)
-        @test_throws "The field v was not found" apply_analytical!(zeros(ndofs(dh)), dh, :v, x->0.0)
-        @test_throws "length(f(x)) must be" apply_analytical!(zeros(ndofs(dh)), dh, :u, x->0.0)     # Should be f(x)::Vec{2}
+        @test_throws ErrorException apply_analytical!(zeros(ndofs(dh)), dh, :v, x->0.0)
+        @test_throws ErrorException apply_analytical!(zeros(ndofs(dh)), dh, :u, x->0.0)     # Should be f(x)::Vec{2}
 
         mdh = testmdh(2, 1, 1)
-        @test_throws "The field v was not found" apply_analytical!(zeros(ndofs(mdh)), mdh, :v, x->0.0)
-        @test_throws "length(f(x)) must be" apply_analytical!(zeros(ndofs(mdh)), mdh, :u, x->0.0)   # Should be f(x)::Vec{2}
+        @test_throws ErrorException apply_analytical!(zeros(ndofs(mdh)), mdh, :v, x->0.0)
+        @test_throws ErrorException apply_analytical!(zeros(ndofs(mdh)), mdh, :u, x->0.0)   # Should be f(x)::Vec{2}
     end
 
 end
