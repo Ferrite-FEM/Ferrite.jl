450e9c5a39a364277a5dc3189aa6e1e367d3b744
Knut Andreas Meyer <knutam@gmail.com>
2022-12-12 10:07:11 +0100

    Put apply_analytical tests last, before examples

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,6 @@ using LinearAlgebra
 using SparseArrays
 
 include("test_utils.jl")
-include("test_apply_analytical.jl")
 include("test_interpolations.jl")
 include("test_cellvalues.jl")
 include("test_facevalues.jl")
@@ -23,5 +22,6 @@ include("test_l2_projection.jl")
 include("test_pointevaluation.jl")
 # include("test_notebooks.jl")
 include("test_apply_rhs.jl")
+include("test_apply_analytical.jl")
 include("test_examples.jl")
 @test all(x -> isdefined(Ferrite, x), names(Ferrite))  # Test that all exported symbols are defined
