f60c1d7cc344f6ff7693ae3b304ae97906494fb1
termi-official <termi-official@users.noreply.github.com>
2023-01-11 22:23:15 +0100

    Update deprecations.

--- src/deprecations.jl
+++ src/deprecations.jl
@@ -1,4 +1,9 @@
 Base.@deprecate_binding DirichletBoundaryConditions ConstraintHandler
 Base.@deprecate_binding DirichletBoundaryCondition Dirichlet
+
 import Base: push!
 @deprecate push!(dh::AbstractDofHandler, args...) add!(dh, args...)
+
+@deprecate vertices(ip::Interpolation) vertexdof_indices(ip)
+@deprecate faces(ip::Interpolation) facedof_indices(ip)
+@deprecate edges(ip::Interpolation) edgedof_indices(ip)
