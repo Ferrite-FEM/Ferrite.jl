87bf1bbe6d239c8e750a6af83e3869dd2ccf2bb5
Fredrik Ekre <ekrefredrik@gmail.com>
2023-03-10 11:04:38 +0100

    rm debug

--- test/test_examples.jl
+++ test/test_examples.jl
@@ -1,56 +1,56 @@
 # Test the scripts
 
-# module TestHeatEquationExample
+module TestHeatEquationExample
+    mktempdir() do dir
+        cd(dir) do
+            include(joinpath(@__DIR__, "../docs/src/literate/heat_equation.jl"))
+        end
+    end
+end
+
+module TestIncompressibleElasticity
+    mktempdir() do dir
+        cd(dir) do
+            include(joinpath(@__DIR__, "../docs/src/literate/incompressible_elasticity.jl"))
+        end
+    end
+end
+
+module TestHyperElasticity
+    mktempdir() do dir
+        cd(dir) do
+            include(joinpath(@__DIR__, "../docs/src/literate/hyperelasticity.jl"))
+        end
+    end
+end
+
+module TestQuasiIncompressibleHyperElasticity
+    mktempdir() do dir
+        cd(dir) do
+            include(joinpath(@__DIR__, "../docs/src/literate/quasi_incompressible_hyperelasticity.jl"))
+        end
+    end
+end
+
+# module TestNavierStokesDiffeqIntegration
 #     mktempdir() do dir
 #         cd(dir) do
-#             include(joinpath(@__DIR__, "../docs/src/literate/heat_equation.jl"))
+#             include(joinpath(@__DIR__, "../docs/src/literate/ns_vs_diffeq.jl"))
 #         end
 #     end
 # end
 
-# module TestIncompressibleElasticity
-#     mktempdir() do dir
-#         cd(dir) do
-#             include(joinpath(@__DIR__, "../docs/src/literate/incompressible_elasticity.jl"))
-#         end
-#     end
-# end
-
-# module TestHyperElasticity
-#     mktempdir() do dir
-#         cd(dir) do
-#             include(joinpath(@__DIR__, "../docs/src/literate/hyperelasticity.jl"))
-#         end
-#     end
-# end
-
-# module TestQuasiIncompressibleHyperElasticity
-#     mktempdir() do dir
-#         cd(dir) do
-#             include(joinpath(@__DIR__, "../docs/src/literate/quasi_incompressible_hyperelasticity.jl"))
-#         end
-#     end
-# end
-
-# # module TestNavierStokesDiffeqIntegration
-# #     mktempdir() do dir
-# #         cd(dir) do
-# #             include(joinpath(@__DIR__, "../docs/src/literate/ns_vs_diffeq.jl"))
-# #         end
-# #     end
-# # end
-
-# module TestComputationalHomogenization
-#     include(joinpath(@__DIR__, "../docs/download_resources.jl"))
-#     mktempdir() do dir
-#         cd(dir) do
-#             cp(joinpath(@__DIR__, "../docs/src/examples/periodic-rve.msh"),
-#                joinpath(dir, "periodic-rve.msh")
-#             )
-#             include(joinpath(@__DIR__, "../docs/src/literate/computational_homogenization.jl"))
-#         end
-#     end
-# end
+module TestComputationalHomogenization
+    include(joinpath(@__DIR__, "../docs/download_resources.jl"))
+    mktempdir() do dir
+        cd(dir) do
+            cp(joinpath(@__DIR__, "../docs/src/examples/periodic-rve.msh"),
+               joinpath(dir, "periodic-rve.msh")
+            )
+            include(joinpath(@__DIR__, "../docs/src/literate/computational_homogenization.jl"))
+        end
+    end
+end
 
 module TestStokesFlow
 if !Sys.iswindows()
