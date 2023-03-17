e17830b997353a1c36c3396d84b685cf2730527e
Fredrik Ekre <ekrefredrik@gmail.com>
2023-03-17 15:45:16 +0100

    Dont export deprecated names

--- src/deprecations.jl
+++ src/deprecations.jl
@@ -4,6 +4,6 @@ Base.@deprecate_binding DirichletBoundaryCondition Dirichlet
 import Base: push!
 @deprecate push!(dh::AbstractDofHandler, args...) add!(dh, args...)
 
-@deprecate vertices(ip::Interpolation) vertexdof_indices(ip)
-@deprecate faces(ip::Interpolation) facedof_indices(ip)
-@deprecate edges(ip::Interpolation) edgedof_indices(ip)
+@deprecate vertices(ip::Interpolation) vertexdof_indices(ip) false
+@deprecate faces(ip::Interpolation) facedof_indices(ip) false
+@deprecate edges(ip::Interpolation) edgedof_indices(ip) false
