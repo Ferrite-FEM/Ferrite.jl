9cd79a1640c016eff5b0367d48bd53e54313e9df
Knut Andreas Meyer <knutam@gmail.com>
2023-04-19 10:29:57 +0200

    Make efficient structure automatically

--- src/FEValues/MultiCellValues.jl
+++ src/FEValues/MultiCellValues.jl
@@ -26,7 +26,7 @@ function MultiCellValues(named_values::NamedTuple)
     return MultiCellValues{dim,T,RefShape,typeof(tuple_values),typeof(named_values)}(tuple_values, named_values)
 end
 
-# Convenience
+# Convenience constructors (Reason for including this file later)
 create_qr_rule(qr::QuadratureRule, args...) = qr 
 create_qr_rule(qr::Int, ::Interpolation{Dim,RefShape}) where {Dim,RefShape} = QuadratureRule{Dim,RefShape}(qr)
 
@@ -37,12 +37,27 @@ end
 function MultiCellValues(fh::FieldHandler, CT; qr=2)
     # TODO: With new SubDofHandler, CT should not be required anymore. 
     ip_geo = default_interpolation(CT)
-    @assert getdim(ip_geo) > 1 # For 1-dimensional problems we cannot differentiate vector vs scalar values
+
     qr_actual = create_qr_rule(qr, ip_geo)
+
+    # TODO: For 1-dimensional problems we cannot differentiate vector vs scalar values
+    #       This can be solved with the new vectorized interpolations discussin in 
+    #       Then, we also only need ip_fun as key to the dict below
+    @assert getdim(ip_geo) > 1
     ip_funs = [name=>getfieldinterpolation(fh, name) for name in getfieldnames(fh)]
-    val_type = Dict(name=> 1==getfielddim(fh, name) ? CellScalarValues : CellVectorValues for name in getfieldnames(fh))
-    # TODO: Create only the minimum required unique values: I.e. if ip_fun is the same, we can use the same value 
-    return MultiCellValues(NamedTuple(key=> val_type[key](qr_actual, ip_fun, ip_geo) for (key, ip_fun) in ip_funs))
+    values = Dict{}()
+    for (name, ip_fun) in ip_funs
+        dim = getfielddim(fh, name)
+        if !(haskey(values, (ip_fun,dim)))
+            if dim == 1
+                values[(ip_fun,dim)] = CellScalarValues(qr_actual, ip_fun, ip_geo)
+            else
+                values[(ip_fun,dim)] = CellVectorValues(qr_actual, ip_fun, ip_geo)
+            end
+        end
+    end
+
+    return MultiCellValues(NamedTuple(name=>values[(ip_fun, getfielddim(fh, name))] for (name, ip_fun) in ip_funs))
 end
 
 # Not sure if aggressive constprop is required, but is intended so use to ensure?
