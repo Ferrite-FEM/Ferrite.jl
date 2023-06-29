083fd14addad3d5f1c66fb41e59dff27e1306181
termi-official <termi-official@users.noreply.github.com>
2023-06-29 19:59:19 +0200

    Optimize algorithm for topology construction by eliminating allocations.

--- src/Grid/topology.jl
+++ src/Grid/topology.jl
@@ -81,9 +81,11 @@ function Base.show(io::IO, ::MIME"text/plain", topology::ExclusiveTopology)
 end
 
 function ExclusiveTopology(cells::Vector{C}) where C <: AbstractCell
+    # Setup the cell to vertex table
     cell_vertices_table = vertices.(cells) #needs generic interface for <: AbstractCell
     vertex_cell_table = Dict{Int,Set{Int}}()
 
+    # Setup vertex to cell connectivity by flipping the cell to vertex table
     for (cellid, cell_vertices) in enumerate(cell_vertices_table)
        for vertex in cell_vertices
             if haskey(vertex_cell_table, vertex)
@@ -94,57 +96,61 @@ function ExclusiveTopology(cells::Vector{C}) where C <: AbstractCell
         end
     end
 
+    # Setup the remaining neighborhoos
     I_face = Int[]; J_face = Int[]; V_face = EntityNeighborhood{FaceIndex}[]
     I_edge = Int[]; J_edge = Int[]; V_edge = EntityNeighborhood{EdgeIndex}[]
     I_vertex = Int[]; J_vertex = Int[]; V_vertex = EntityNeighborhood{VertexIndex}[]
     cell_neighbor_table = Vector{EntityNeighborhood{CellIndex}}(undef, length(cells))
 
-    for (cellid, cell) in enumerate(cells)
-        cell_neighbors = reduce(union!, [Set{Int}(vertex_cell_table[vertex]) for vertex ∈ vertices(cell) if vertex_cell_table[vertex] != cellid])
-        cell_neighbor_table[cellid] = EntityNeighborhood(CellIndex.(collect(cell_neighbors)))
+    for (cell_id, cell) in enumerate(cells)
+        cell_neighbor_ids = reduce(union!, [Set{Int}(vertex_cell_table[vertex]) for vertex ∈ vertices(cell) if vertex_cell_table[vertex] != cell_id])
+        cell_neighbor_table[cell_id] = EntityNeighborhood(CellIndex.(collect(cell_neighbor_ids)))
 
-        # Add all element connected via face to the table
-        face_neighbors = Set{Int}()
-        for (face_idx,face) ∈ enumerate(faces(cell))
-            neighbor_candidates = Set{Int}(c for c ∈ vertex_cell_table[face[1]] if c != cellid)
-            for face_vertex ∈ face[2:end]
-                intersect!(neighbor_candidates, vertex_cell_table[face_vertex])
-            end
-            union!(face_neighbors, neighbor_candidates)
-        end
+        # Any of the neighbors is now sorted in the respective categories
+        for cell_neighbor_id ∈ cell_neighbor_ids
+            # Skip self
+            cell_id == cell_neighbor_id && continue
+            # Buffer neighbor
+            cell_neighbor = cells[cell_neighbor_id]
+            # TODO handle mixed-dimensional case
+            getdim(cell_neighbor) == getdim(cell) || continue
 
-        # Add all element connected via edge to the table
-        if getdim(cell) > 2
-            edge_neighbors = Set{Int}()
-            for (edge_idx,edge) ∈ enumerate(edges(cell))
-                neighbor_candidates = Set{Int}(c for c ∈ vertex_cell_table[edge[1]] if c != cellid)
-                for edge_vertex ∈ edge[2:end]
-                    edge_neighbor = vertex_cell_table[edge_vertex]
-                    if edge_neighbor != cellid && edge_neighbor ∉ face_neighbors
-                        intersect!(neighbor_candidates, edge_neighbor)
+            num_shared_vertices = 0
+            for vertex ∈ vertices(cell)
+                for vertex_neighbor ∈ vertices(cell_neighbor)
+                    if vertex_neighbor == vertex
+                        num_shared_vertices += 1
+                        continue
                     end
                 end
-                union!(edge_neighbors, neighbor_candidates)
             end
-        end
 
-        # Fill subentity connectivity tables
-        for neighbor_cellid in cell_neighbors
-            getdim(cell) == getdim(cells[neighbor_cellid]) || continue # TODO introduce logic to handle codimensional connectivity
-
-            cell_local_ids = findall(x->x in cell_vertices_table[neighbor_cellid], cell_vertices_table[cellid])::Vector{Int}
-            # vertex neighbor
-            if length(cell_local_ids) == 1
-                neighbor_local_ids = findall(x->x in cell_vertices_table[cellid], cell_vertices_table[neighbor_cellid])::Vector{Int}
-                _vertex_neighbor!(V_vertex, I_vertex, J_vertex, cellid, cell, neighbor_local_ids, neighbor_cellid, cells[neighbor_cellid])
-            # face neighbor
-            elseif neighbor_cellid ∈ face_neighbors
-                neighbor_local_ids = findall(x->x in cell_vertices_table[cellid], cell_vertices_table[neighbor_cellid])::Vector{Int}
-                _face_neighbor!(V_face, I_face, J_face, cellid, cell, neighbor_local_ids, neighbor_cellid, cells[neighbor_cellid])
-            # edge neighbor
-            elseif getdim(cell) > 2 && neighbor_cellid ∈ edge_neighbors
-                neighbor_local_ids = findall(x->x in cell_vertices_table[cellid], cell_vertices_table[neighbor_cellid])::Vector{Int}
-                _edge_neighbor!(V_edge, I_edge, J_edge, cellid, cell, neighbor_local_ids, neighbor_cellid, cells[neighbor_cellid])
+            # Simplest case: Only one vertex is shared => Vertex neighbor
+            if num_shared_vertices == 1
+                for (lvi, vertex) ∈ enumerate(vertices(cell))
+                    for (lvi2, vertex_neighbor) ∈ enumerate(vertices(cell_neighbor))
+                        if vertex_neighbor == vertex
+                            push!(I_vertex, cell_id)
+                            push!(J_vertex, lvi)
+                            push!(V_vertex, EntityNeighborhood(VertexIndex(cell_neighbor_id, lvi2)))
+                            break
+                        end
+                    end
+                end
+            # Shared path
+            elseif num_shared_vertices == 2
+                if getdim(cell) == 2
+                    _add_single_face_neighbor!(I_face, J_face, V_face, cell, cell_id, cell_neighbor, cell_neighbor_id)
+                elseif getdim(cell) == 3
+                    _add_single_edge_neighbor!(I_edge, J_edge, V_edge, cell, cell_id, cell_neighbor, cell_neighbor_id)
+                else
+                    @error "Case not implemented."
+                end
+            # Shared surface
+            elseif num_shared_vertices >= 3
+                _add_single_face_neighbor!(I_face, J_face, V_face, cell, cell_id, cell_neighbor, cell_neighbor_id)
+            else
+                @error "Found connected elements without shared vertex... Mesh broken?"
             end
         end
     end
@@ -172,6 +178,7 @@ function ExclusiveTopology(cells::Vector{C}) where C <: AbstractCell
         end
     end
 
+    # Setup matrices
     vertex_neighbor = sparse(I_vertex,J_vertex,V_vertex, length(cells), max_vertices)
     face_neighbor = sparse(I_face,J_face,V_face, length(cells), max_faces)
     edge_neighbor = sparse(I_edge,J_edge,V_edge, length(cells), max_edges)
@@ -179,33 +186,36 @@ function ExclusiveTopology(cells::Vector{C}) where C <: AbstractCell
     return ExclusiveTopology(vertex_cell_table,cell_neighbor_table,face_neighbor,vertex_neighbor,edge_neighbor)
 end
 
-function _vertex_neighbor!(V_vertex::Vector{EntityNeighborhood{VertexIndex}}, I_vertex::Array{Int}, J_vertex::Array{Int}, cellid::Int, cell::C1, neighbor::Vector{Int}, neighborid::Int, neighbor_cell::C2) where {C1 <: AbstractCell, C2 <: AbstractCell}
-    vertex_neighbor = VertexIndex((neighborid, neighbor[1]))
-    cell_vertex_id = findfirst(x->x==neighbor_cell.nodes[neighbor[1]], cell.nodes)::Int
-    push!(V_vertex,EntityNeighborhood(vertex_neighbor))
-    push!(I_vertex,cellid)
-    push!(J_vertex,cell_vertex_id)
+function _add_single_face_neighbor!(I_face, J_face, V_face, cell, cell_id, cell_neighbor, cell_neighbor_id)
+    for (lfi, face) ∈ enumerate(faces(cell))
+        uniqueface,_ = sortface(face)
+        for (lfi2, face_neighbor) ∈ enumerate(faces(cell_neighbor))
+            uniqueface2,_ = sortface(face_neighbor)
+            if uniqueface == uniqueface2
+                push!(I_face, cell_id)
+                push!(J_face, lfi)
+                push!(V_face, EntityNeighborhood(FaceIndex(cell_neighbor_id, lfi2)))
+                return
+            end
+        end
+    end
 end
 
-function _edge_neighbor!(V_edge::Vector{EntityNeighborhood{EdgeIndex}}, I_edge::Array{Int}, J_edge::Array{Int}, cellid::Int, cell::C1, neighbor::Vector{Int}, neighborid::Int, neighbor_cell::C2) where {C1 <: AbstractCell, C2 <: AbstractCell}
-    neighbor_edge = neighbor_cell.nodes[neighbor]
-    neighbor_edge_id = findfirst(x->issubset(x,neighbor_edge), edges(neighbor_cell))::Int
-    edge_neighbor = EdgeIndex((neighborid, neighbor_edge_id))
-    cell_edge_id = findfirst(x->issubset(x,neighbor_edge),edges(cell))::Int
-    push!(V_edge, EntityNeighborhood(edge_neighbor))
-    push!(I_edge, cellid)
-    push!(J_edge, cell_edge_id)
+function _add_single_edge_neighbor!(I_edge, J_edge, V_edge, cell, cell_id, cell_neighbor, cell_neighbor_id)
+    for (lei, edge) ∈ enumerate(edges(cell))
+        uniqueedge,_ = sortedge(edge)
+        for (lei2, edge_neighbor) ∈ enumerate(edges(cell_neighbor))
+            uniqueedge2,_ = sortedge(edge_neighbor)
+            if uniqueedge == uniqueedge2
+                push!(I_edge, cell_id)
+                push!(J_edge, lei)
+                push!(V_edge, EntityNeighborhood(EdgeIndex(cell_neighbor_id, lei2)))
+                return
+            end
+        end
+    end
 end
 
-function _face_neighbor!(V_face::Vector{EntityNeighborhood{FaceIndex}}, I_face::Array{Int}, J_face::Array{Int}, cellid::Int, cell::C1, neighbor::Vector{Int}, neighborid::Int, neighbor_cell::C2) where {C1 <: AbstractCell, C2 <: AbstractCell}
-    neighbor_face = neighbor_cell.nodes[neighbor]
-    neighbor_face_id = findfirst(x->issubset(x,neighbor_face), faces(neighbor_cell))::Int
-    face_neighbor = FaceIndex((neighborid, neighbor_face_id))
-    cell_face_id = findfirst(x->issubset(x,neighbor_face),faces(cell))::Int
-    push!(V_face, EntityNeighborhood(face_neighbor))
-    push!(I_face, cellid)
-    push!(J_face, cell_face_id)
-end
 
 getcells(neighbor::EntityNeighborhood{T}) where T <: BoundaryIndex = first.(neighbor.neighbor_info)
 getcells(neighbor::EntityNeighborhood{CellIndex}) = getproperty.(neighbor.neighbor_info, :idx)
