d5f71917ecfc8565d11b5e04ad3a1e90b41f793d
Knut Andreas Meyer <knutam@gmail.com>
2023-10-08 13:41:21 +0200

    Move to old files

--- src/FEValues/cell_values.jl
+++ src/FEValues/cell_values.jl
@@ -1,252 +1,80 @@
-"""
-    OldCellValues([::Type{T},] quad_rule::QuadratureRule, func_interpol::Interpolation, [geom_interpol::Interpolation])
 
-A `OldCellValues` object facilitates the process of evaluating values of shape functions, gradients of shape functions,
-values of nodal functions, gradients and divergences of nodal functions etc. in the finite element cell.
+include("GeometryValues.jl")
+include("FunctionValues.jl")
 
-**Arguments:**
-* `T`: an optional argument (default to `Float64`) to determine the type the internal data is stored as.
-* `quad_rule`: an instance of a [`QuadratureRule`](@ref)
-* `func_interpol`: an instance of an [`Interpolation`](@ref) used to interpolate the approximated function
-* `geom_interpol`: an optional instance of a [`Interpolation`](@ref) which is used to interpolate the geometry.
-  By default linear Lagrange interpolation is used. For embedded elements the geometric interpolations should
-  be vectorized to the spatial dimension.
-
-**Common methods:**
-
-* [`reinit!`](@ref)
-* [`getnquadpoints`](@ref)
-* [`getdetJdV`](@ref)
-
-* [`shape_value`](@ref)
-* [`shape_gradient`](@ref)
-* [`shape_symmetric_gradient`](@ref)
-* [`shape_divergence`](@ref)
-
-* [`function_value`](@ref)
-* [`function_gradient`](@ref)
-* [`function_symmetric_gradient`](@ref)
-* [`function_divergence`](@ref)
-* [`spatial_coordinate`](@ref)
-"""
-OldCellValues
-
-struct OldCellValues{IP, N_t, dNdx_t, dNdξ_t, T, dMdξ_t, QR, GIP} <: AbstractCellValues
-    N::Matrix{N_t}
-    dNdx::Matrix{dNdx_t}
-    dNdξ::Matrix{dNdξ_t}
-    detJdV::Vector{T}
-    M::Matrix{T}
-    dMdξ::Matrix{dMdξ_t}
-    qr::QR
-    ip::IP
-    gip::GIP
+function default_geometric_interpolation(::Interpolation{shape}) where {dim, shape <: AbstractRefShape{dim}}
+    return VectorizedInterpolation{dim}(Lagrange{shape, 1}())
 end
 
-# Common initializer code for constructing OldCellValues after the types have been determined
-function OldCellValues{IP, N_t, dNdx_t, dNdξ_t, T, dMdξ_t, QR, GIP}(qr::QR, ip::IP, gip::GIP) where {
-    IP, N_t, dNdx_t, dNdξ_t, T, dMdξ_t, QR, GIP,
-}
-    @assert isconcretetype(IP)     && isconcretetype(N_t) && isconcretetype(dNdx_t) &&
-            isconcretetype(dNdξ_t) && isconcretetype(T)   && isconcretetype(dMdξ_t) &&
-            isconcretetype(QR)     && isconcretetype(GIP)
-    n_qpoints = getnquadpoints(qr)
+struct CellValues{FV, GV, QR, detT<:AbstractVector} <: AbstractCellValues
+    fun_values::FV # FunctionValues
+    geo_values::GV # GeometryValues
+    qr::QR         # QuadratureRule
+    detJdV::detT   # AbstractVector{<:Number}
+end
+function CellValues(::Type{T}, qr::QuadratureRule, ip_fun::Interpolation, ip_geo::VectorizedInterpolation) where T 
+    geo_values = GeometryValues(T, ip_geo.ip, qr, RequiresHessian(ip_fun, ip_geo))
+    fun_values = FunctionValues(T, ip_fun, qr, ip_geo)
+    detJdV = fill(T(NaN), length(getweights(qr)))
+    return CellValues(fun_values, geo_values, qr, detJdV)
+end
 
-    # Field interpolation
-    n_func_basefuncs = getnbasefunctions(ip)
-    N    = fill(zero(N_t)    * T(NaN), n_func_basefuncs, n_qpoints)
-    dNdx = fill(zero(dNdx_t) * T(NaN), n_func_basefuncs, n_qpoints)
-    dNdξ = fill(zero(dNdξ_t) * T(NaN), n_func_basefuncs, n_qpoints)
+CellValues(qr::QuadratureRule, ip::Interpolation, args...) = CellValues(Float64, qr, ip, args...)
+function CellValues(::Type{T}, qr, ip::Interpolation, ip_geo::ScalarInterpolation=default_geometric_interpolation(ip)) where T
+    return CellValues(T, qr, ip, VectorizedInterpolation(ip_geo))
+end
 
-    # Geometry interpolation
-    n_geom_basefuncs = getnbasefunctions(gip)
-    M    = fill(zero(T)      * T(NaN), n_geom_basefuncs, n_qpoints)
-    dMdξ = fill(zero(dMdξ_t) * T(NaN), n_geom_basefuncs, n_qpoints)
+function Base.copy(cv::CellValues)
+    return CellValues(copy(cv.fun_values), copy(cv.geo_values), copy(cv.qr), copy(cv.detJdV))
+end
 
-    for (qp, ξ) in pairs(getpoints(qr))
-        for basefunc in 1:n_func_basefuncs
-            dNdξ[basefunc, qp], N[basefunc, qp] = shape_gradient_and_value(ip, ξ, basefunc)
-        end
-        for basefunc in 1:n_geom_basefuncs
-            dMdξ[basefunc, qp], M[basefunc, qp] = shape_gradient_and_value(gip, ξ, basefunc)
-        end
+# Access geometry values
+for op = (:getngeobasefunctions, :geometric_value)
+    eval(quote
+        @propagate_inbounds $op(cv::CellValues, args...) = $op(cv.geo_values, args...)
+    end)
+end
+getdetJdV(cv::CellValues, q_point::Int) = cv.detJdV[q_point]
+
+# Accessors for function values 
+getnbasefunctions(cv::CellValues) = getnbasefunctions(cv.fun_values)
+get_function_interpolation(cv::CellValues) = get_function_interpolation(cv.fun_values)
+get_geometric_interpolation(cv::CellValues) = get_geometric_interpolation(cv.geo_values)
+shape_value_type(cv::CellValues) = shape_value_type(cv.fun_values)
+shape_gradient_type(cv::CellValues) = shape_gradient_type(cv.fun_values)
+
+for op = (:shape_value, :shape_gradient, :shape_symmetric_gradient)
+    eval(quote
+        @propagate_inbounds $op(cv::CellValues, i::Int, q_point::Int) = $op(cv.fun_values, i, q_point)
+    end)
+end
+# Access quadrature rule values 
+getnquadpoints(cv::CellValues) = getnquadpoints(cv.qr)
+
+function reinit!(cv::CellValues, x::AbstractVector{<:Vec}, cell=nothing)
+    geo_values = cv.geo_values
+    fun_values = cv.fun_values
+    n_geom_basefuncs = getngeobasefunctions(geo_values)
+    if !checkbounds(Bool, x, 1:n_geom_basefuncs) || length(x)!=n_geom_basefuncs
+        throw_incompatible_coord_length(length(x), n_geom_basefuncs)
     end
-
-    detJdV = fill(T(NaN), n_qpoints)
-
-    OldCellValues{IP, N_t, dNdx_t, dNdξ_t, T, dMdξ_t, QR, GIP}(N, dNdx, dNdξ, detJdV, M, dMdξ, qr, ip, gip)
-end
-
-# Common entry point that fills in the numeric type and geometric interpolation
-function OldCellValues(qr::QuadratureRule, ip::Interpolation,
-        gip::Interpolation = default_geometric_interpolation(ip))
-    return OldCellValues(Float64, qr, ip, gip)
-end
-
-# Common entry point that fills in the geometric interpolation
-function OldCellValues(::Type{T}, qr::QuadratureRule, ip::Interpolation) where {T}
-    return OldCellValues(T, qr, ip, default_geometric_interpolation(ip))
-end
-
-# Common entry point that vectorizes an input scalar geometric interpolation
-function OldCellValues(::Type{T}, qr::QuadratureRule, ip::Interpolation, sgip::ScalarInterpolation) where {T}
-    return OldCellValues(T, qr, ip, VectorizedInterpolation(sgip))
-end
-
-# Entrypoint for `ScalarInterpolation`s (rdim == sdim)
-function OldCellValues(::Type{T}, qr::QR, ip::IP, gip::VGIP) where {
-    dim, shape <: AbstractRefShape{dim}, T,
-    QR   <: QuadratureRule{shape},
-    IP   <: ScalarInterpolation{shape},
-    GIP  <: ScalarInterpolation{shape},
-    VGIP <: VectorizedInterpolation{dim, shape, <:Any, GIP},
-}
-    # Function interpolation
-    N_t    = T
-    dNdx_t = dNdξ_t = Vec{dim, T}
-    # Geometry interpolation
-    M_t    = T
-    dMdξ_t = Vec{dim, T}
-    return OldCellValues{IP, N_t, dNdx_t, dNdξ_t, M_t, dMdξ_t, QR, GIP}(qr, ip, gip.ip)
-end
-
-# Entrypoint for `VectorInterpolation`s (vdim == rdim == sdim)
-function OldCellValues(::Type{T}, qr::QR, ip::IP, gip::VGIP) where {
-    dim, shape <: AbstractRefShape{dim}, T,
-    QR  <: QuadratureRule{shape},
-    IP  <: VectorInterpolation{dim, shape},
-    GIP <: ScalarInterpolation{shape},
-    VGIP <: VectorizedInterpolation{dim, shape, <:Any, GIP},
-}
-    # Field interpolation
-    N_t    = Vec{dim, T}
-    dNdx_t = dNdξ_t = Tensor{2, dim, T, Tensors.n_components(Tensor{2,dim})}
-    # Geometry interpolation
-    M_t    = T
-    dMdξ_t = Vec{dim, T}
-    return OldCellValues{IP, N_t, dNdx_t, dNdξ_t, M_t, dMdξ_t, QR, GIP}(qr, ip, gip.ip)
-end
-
-# Entrypoint for `VectorInterpolation`s (vdim != rdim == sdim)
-function OldCellValues(::Type{T}, qr::QR, ip::IP, vgip::VGIP) where {
-    vdim, dim, shape <: AbstractRefShape{dim}, T,
-    QR  <: QuadratureRule{shape},
-    IP  <: VectorInterpolation{vdim, shape},
-    GIP <: ScalarInterpolation{shape},
-    VGIP <: VectorizedInterpolation{dim, shape, <:Any, GIP},
-}
-    # Field interpolation
-    N_t    = SVector{vdim, T}
-    dNdx_t = dNdξ_t = SMatrix{vdim, dim, T, vdim*dim}
-    # Geometry interpolation
-    M_t    = T
-    dMdξ_t = Vec{dim, T}
-    return OldCellValues{IP, N_t, dNdx_t, dNdξ_t, M_t, dMdξ_t, QR, GIP}(qr, ip, vgip.ip)
-end
-
-# reinit! for regular (non-embedded) elements (rdim == sdim)
-function reinit!(cv::OldCellValues{<:Any, N_t, dNdx_t, dNdξ_t}, x::AbstractVector{Vec{dim,T}}) where {
-    dim, T, vdim,
-    N_t    <: Union{Number,   Vec{dim},       SVector{vdim}     },
-    dNdx_t <: Union{Vec{dim}, Tensor{2, dim}, SMatrix{vdim, dim}},
-    dNdξ_t <: Union{Vec{dim}, Tensor{2, dim}, SMatrix{vdim, dim}},
-}
-    n_geom_basefuncs = getngeobasefunctions(cv)
-    n_func_basefuncs = getnbasefunctions(cv)
-    length(x) == n_geom_basefuncs || throw_incompatible_coord_length(length(x), n_geom_basefuncs)
-
-    @inbounds for (i, w) in pairs(getweights(cv.qr))
-        fecv_J = zero(Tensor{2,dim,T})
-        for j in 1:n_geom_basefuncs
-            fecv_J += x[j] ⊗ cv.dMdξ[j, i]
-        end
-        detJ = det(fecv_J)
+    @inbounds for (q_point, w) in enumerate(getweights(cv.qr))
+        mapping = calculate_mapping(geo_values, q_point, x)
+        detJ = calculate_detJ(getjacobian(mapping))
         detJ > 0.0 || throw_detJ_not_pos(detJ)
-        cv.detJdV[i] = detJ * w
-        Jinv = inv(fecv_J)
-        for j in 1:n_func_basefuncs
-            # cv.dNdx[j, i] = cv.dNdξ[j, i] ⋅ Jinv
-            cv.dNdx[j, i] = dothelper(cv.dNdξ[j, i], Jinv)
-        end
-    end
-end
-
-# Entrypoint for embedded `ScalarInterpolation`s (rdim < sdim)
-function OldCellValues(::Type{T}, qr::QR, ip::IP, gip::VGIP) where {
-    sdim, rdim, shape <: AbstractRefShape{rdim}, T,
-    QR  <: QuadratureRule{shape},
-    IP  <: ScalarInterpolation{shape},
-    GIP <: ScalarInterpolation{shape},
-    VGIP <: VectorizedInterpolation{sdim, shape, <:Any, GIP},
-}
-    @assert sdim > rdim
-    # Function interpolation
-    N_t    = T
-    dNdx_t = SVector{sdim, T}
-    dNdξ_t = SVector{rdim, T}
-    # Geometry interpolation
-    M_t    = T
-    dMdξ_t = Vec{rdim, T}
-    return OldCellValues{IP, N_t, dNdx_t, dNdξ_t, M_t, dMdξ_t, QR, GIP}(qr, ip, gip.ip)
-end
-
-# Entrypoint for embedded `VectorInterpolation`s (rdim < sdim)
-function OldCellValues(::Type{T}, qr::QR, ip::IP, gip::VGIP) where {
-    sdim, vdim, rdim, shape <: AbstractRefShape{rdim}, T,
-    QR  <: QuadratureRule{shape},
-    IP  <: VectorInterpolation{vdim, shape},
-    GIP <: ScalarInterpolation{shape},
-    VGIP <: VectorizedInterpolation{sdim, shape, <:Any, GIP},
-}
-    @assert sdim > rdim
-    # Function interpolation
-    N_t    = SVector{vdim, T}
-    dNdx_t = SMatrix{vdim, sdim, T, vdim*sdim}
-    dNdξ_t = SMatrix{vdim, rdim, T, vdim*rdim}
-    # Geometry interpolation
-    M_t    = T
-    dMdξ_t = Vec{rdim, T}
-    return OldCellValues{IP, N_t, dNdx_t, dNdξ_t, M_t, dMdξ_t, QR, GIP}(qr, ip, gip.ip)
-end
-
-# reinit! for embedded elements, rdim < sdim
-function reinit!(cv::OldCellValues{<:Any, N_t, dNdx_t, dNdξ_t}, x::AbstractVector{Vec{sdim,T}}) where {
-    rdim, sdim, vdim, T,
-    N_t    <: Union{Number,           SVector{vdim}},
-    dNdx_t <: Union{SVector{sdim, T}, SMatrix{vdim, sdim, T}},
-    dNdξ_t <: Union{SVector{rdim, T}, SMatrix{vdim, rdim, T}},
-}
-    @assert sdim > rdim "This reinit only works for embedded elements. Maybe you swapped the reference and spatial dimensions?"
-    n_geom_basefuncs = getngeobasefunctions(cv)
-    n_func_basefuncs = getnbasefunctions(cv)
-    length(x) == n_geom_basefuncs || throw_incompatible_coord_length(length(x), n_geom_basefuncs)
-
-    @inbounds for (i, w) in pairs(getweights(cv.qr))
-        fecv_J = zero(MMatrix{sdim, rdim, T}) # TODO replace with MixedTensor (see https://github.com/Ferrite-FEM/Tensors.jl/pull/188)
-        for j in 1:n_geom_basefuncs
-            #fecv_J += x[j] ⊗ cv.dMdξ[j, i] # TODO via Tensors.jl
-            for k in 1:sdim, l in 1:rdim
-                fecv_J[k, l] += x[j][k] * cv.dMdξ[j, i][l]
-            end
-        end
-        fecv_J = SMatrix(fecv_J)
-        detJ = embedding_det(fecv_J)
-        detJ > 0.0 || throw_detJ_not_pos(detJ)
-        cv.detJdV[i] = detJ * w
-        # Compute "left inverse" of J
-        Jinv = pinv(fecv_J)
-        for j in 1:n_func_basefuncs
-            #cv.dNdx[j, i] = cv.dNdξ[j, i] ⋅ Jinv # TODO via Tensors.jl
-            cv.dNdx[j, i] = dothelper(cv.dNdξ[j, i], Jinv)
-        end
+        @inbounds cv.detJdV[q_point] = detJ*w
+        apply_mapping!(fun_values, q_point, mapping, cell)
     end
     return nothing
 end
 
-function Base.show(io::IO, m::MIME"text/plain", cv::OldCellValues)
-    println(io, "CellValues with")
-    println(io, "- Quadrature rule with ", getnquadpoints(cv), " points")
-    print(io, "- Function interpolation: "); show(io, m, cv.ip)
-    println(io)
-    print(io, "- Geometric interpolation: "); show(io, m, cv.gip)
+function Base.show(io::IO, d::MIME"text/plain", cv::CellValues)
+    rdim = getdim(cv.geo_values.ip)
+    vdim = isa(shape_value(cv, 1, 1), Vec) ? length(shape_value(cv, 1, 1)) : 0
+    sdim = length(shape_gradient(cv, 1, 1)) ÷ length(shape_value(cv, 1, 1))
+    vstr = vdim==0 ? "scalar" : "vdim=$vdim"
+    print(io, "CellValues(", vstr, ", rdim=$rdim, and sdim=$sdim): ")
+    print(io, getnquadpoints(cv), " quadrature points")
+    print(io, "\n Function interpolation: "); show(io, d, cv.fun_values.ip)
+    print(io, "\nGeometric interpolation: "); show(io, d, cv.geo_values.ip^sdim)
 end
--- src/FEValues/face_values.jl
+++ src/FEValues/face_values.jl
@@ -1,191 +1,117 @@
-"""
-    FaceValues([::Type{T}], quad_rule::FaceQuadratureRule, func_interpol::Interpolation, [geom_interpol::Interpolation])
-
-A `FaceValues` object facilitates the process of evaluating values of shape functions, gradients of shape functions,
-values of nodal functions, gradients and divergences of nodal functions etc. on the faces of finite elements.
-
-**Arguments:**
-
-* `T`: an optional argument to determine the type the internal data is stored as.
-* `quad_rule`: an instance of a [`FaceQuadratureRule`](@ref)
-* `func_interpol`: an instance of an [`Interpolation`](@ref) used to interpolate the approximated function
-* `geom_interpol`: an optional instance of an [`Interpolation`](@ref) which is used to interpolate the geometry.
-  By default linear Lagrange interpolation is used.
-
-**Common methods:**
-
-* [`reinit!`](@ref)
-* [`getnquadpoints`](@ref)
-* [`getdetJdV`](@ref)
-
-* [`shape_value`](@ref)
-* [`shape_gradient`](@ref)
-* [`shape_symmetric_gradient`](@ref)
-* [`shape_divergence`](@ref)
-
-* [`function_value`](@ref)
-* [`function_gradient`](@ref)
-* [`function_symmetric_gradient`](@ref)
-* [`function_divergence`](@ref)
-* [`spatial_coordinate`](@ref)
-"""
-FaceValues
-
-struct OldFaceValues{IP, N_t, dNdx_t, dNdξ_t, T, dMdξ_t, QR, Normal_t, GIP} <: AbstractFaceValues
-    N::Array{N_t, 3}
-    dNdx::Array{dNdx_t, 3}
-    dNdξ::Array{dNdξ_t, 3}
-    detJdV::Matrix{T}
-    normals::Vector{Normal_t}
-    M::Array{T, 3}
-    dMdξ::Array{dMdξ_t, 3}
-    qr::QR
+struct FaceValues{FV, GV, QR, detT, nT, V_FV<:AbstractVector{FV}, V_GV<:AbstractVector{GV}} <: AbstractFaceValues
+    fun_values::V_FV # AbstractVector{FunctionValues}
+    geo_values::V_GV # AbstractVector{GeometryValues}
+    qr::QR           # FaceQuadratureRule
+    detJdV::detT     # AbstractVector{<:Number}
+    normals::nT      # AbstractVector{<:Vec}
     current_face::ScalarWrapper{Int}
-    func_interp::IP
-    geo_interp::GIP
 end
 
-# Common initializer code for constructing OldFaceValues after the types have been determined
-function OldFaceValues{IP, N_t, dNdx_t, dNdξ_t, T, dMdξ_t, QR, Normal_t, GIP}(qr::QR, ip::IP, gip::GIP) where {
-    IP, N_t, dNdx_t, dNdξ_t, T, dMdξ_t, QR, Normal_t, GIP,
-}
-    @assert isconcretetype(IP)     && isconcretetype(N_t)      && isconcretetype(dNdx_t) &&
-            isconcretetype(dNdξ_t) && isconcretetype(T)        && isconcretetype(dMdξ_t) &&
-            isconcretetype(QR)     && isconcretetype(Normal_t) && isconcretetype(GIP)
-    # Quadrature
-    max_n_qpoints = maximum(getnquadpoints, qr.face_rules; init = 0)
-    n_faces = length(qr.face_rules)
-
-    # Normals
-    normals = zeros(Normal_t, max_n_qpoints)
-
-    # Field interpolation
-    n_func_basefuncs = getnbasefunctions(ip)
-    N    = fill(zero(N_t)    * T(NaN), n_func_basefuncs, max_n_qpoints, n_faces)
-    dNdx = fill(zero(dNdx_t) * T(NaN), n_func_basefuncs, max_n_qpoints, n_faces)
-    dNdξ = fill(zero(dNdξ_t) * T(NaN), n_func_basefuncs, max_n_qpoints, n_faces)
-
-    # Geometry interpolation
-    n_geom_basefuncs = getnbasefunctions(gip)
-    M    = fill(zero(T)      * T(NaN), n_geom_basefuncs, max_n_qpoints, n_faces)
-    dMdξ = fill(zero(dMdξ_t) * T(NaN), n_geom_basefuncs, max_n_qpoints, n_faces)
-
-    for face in 1:n_faces, (qp, ξ) in pairs(getpoints(qr, face))
-        for basefunc in 1:n_func_basefuncs
-            dNdξ[basefunc, qp, face], N[basefunc, qp, face] = shape_gradient_and_value(ip, ξ, basefunc)
-        end
-        for basefunc in 1:n_geom_basefuncs
-            dMdξ[basefunc, qp, face], M[basefunc, qp, face] = shape_gradient_and_value(gip, ξ, basefunc)
-        end
-    end
-
-    detJdV = fill(T(NaN), max_n_qpoints, n_faces)
-
-    OldFaceValues{IP, N_t, dNdx_t, dNdξ_t, T, dMdξ_t, QR, Normal_t, GIP}(N, dNdx, dNdξ, detJdV, normals, M, dMdξ, qr, ScalarWrapper(0), ip, gip)
+function FaceValues(::Type{T}, fqr::FaceQuadratureRule, ip_fun::Interpolation, ip_geo::VectorizedInterpolation{sdim}=default_geometric_interpolation(ip_fun)) where {T,sdim} 
+    geo_values = [GeometryValues(T, ip_geo.ip, qr, RequiresHessian(ip_fun, ip_geo)) for qr in fqr.face_rules]
+    fun_values = [FunctionValues(T, ip_fun, qr, ip_geo) for qr in fqr.face_rules]
+    max_nquadpoints = maximum(qr->length(getweights(qr)), fqr.face_rules)
+    detJdV = fill(T(NaN), max_nquadpoints)
+    normals = fill(zero(Vec{sdim,T})*T(NaN), max_nquadpoints)
+    return FaceValues(fun_values, geo_values, fqr, detJdV, normals, ScalarWrapper(1))
 end
 
-# Common entry point that fills in the numeric type and geometric interpolation
-function OldFaceValues(qr::FaceQuadratureRule, ip::Interpolation,
-        gip::Interpolation = default_geometric_interpolation(ip))
-    return OldFaceValues(Float64, qr, ip, gip)
+FaceValues(qr::FaceQuadratureRule, ip::Interpolation, args...) = FaceValues(Float64, qr, ip, args...)
+function FaceValues(::Type{T}, qr::FaceQuadratureRule, ip::Interpolation, ip_geo::ScalarInterpolation) where T
+    return FaceValues(T, qr, ip, VectorizedInterpolation(ip_geo))
 end
 
-# Common entry point that fills in the geometric interpolation
-function OldFaceValues(::Type{T}, qr::FaceQuadratureRule, ip::Interpolation) where {T}
-    return OldFaceValues(T, qr, ip, default_geometric_interpolation(ip))
+function Base.copy(fv::FaceValues)
+    fun_values = map(copy, fv.fun_values)
+    geo_values = map(copy, fv.geo_values)
+    return FaceValues(fun_values, geo_values, copy(fv.qr), copy(fv.detJdV), copy(fv.normals), copy(fv.current_face))
 end
 
-# Common entry point that vectorizes an input scalar geometric interpolation
-function OldFaceValues(::Type{T}, qr::FaceQuadratureRule, ip::Interpolation, sgip::ScalarInterpolation) where {T}
-    return OldFaceValues(T, qr, ip, VectorizedInterpolation(sgip))
+getngeobasefunctions(fv::FaceValues) = getngeobasefunctions(get_geo_values(fv))
+getnbasefunctions(fv::FaceValues) = getnbasefunctions(get_fun_values(fv))
+getnquadpoints(fv::FaceValues) = getnquadpoints(fv.qr, getcurrentface(fv))
+getdetJdV(fv::FaceValues, q_point) = fv.detJdV[q_point]
+
+shape_value_type(fv::FaceValues) = shape_value_type(get_fun_values(fv))
+shape_gradient_type(fv::FaceValues) = shape_gradient_type(get_fun_values(fv))
+get_function_interpolation(fv::FaceValues) = get_function_interpolation(get_fun_values(fv))
+get_geometric_interpolation(fv::FaceValues) = get_geometric_interpolation(get_geo_values(fv))
+
+get_geo_values(fv::FaceValues) = @inbounds fv.geo_values[getcurrentface(fv)]
+for op = (:getngeobasefunctions, :geometric_value)
+    eval(quote
+        @propagate_inbounds $op(fv::FaceValues, args...) = $op(get_geo_values(fv), args...)
+    end)
 end
 
-# Entrypoint for `ScalarInterpolation`s (rdim == sdim)
-function OldFaceValues(::Type{T}, qr::QR, ip::IP, gip::VGIP) where {
-    dim, shape <: AbstractRefShape{dim}, T,
-    QR  <: FaceQuadratureRule{shape},
-    IP  <: ScalarInterpolation{shape},
-    GIP <: ScalarInterpolation{shape},
-    VGIP <: VectorizedInterpolation{dim, shape, <:Any, GIP},
-}
-    # Normals
-    Normal_t = Vec{dim, T}
-    # Function interpolation
-    N_t = T
-    dNdx_t = dNdξ_t = Vec{dim, T}
-    # Geometry interpolation
-    M_t    = T
-    dMdξ_t = Vec{dim, T}
-    return OldFaceValues{IP, N_t, dNdx_t, dNdξ_t, M_t, dMdξ_t, QR, Normal_t, GIP}(qr, ip, gip.ip)
+get_fun_values(fv::FaceValues) = @inbounds fv.fun_values[getcurrentface(fv)]
+for op = (:shape_value, :shape_gradient, :shape_symmetric_gradient, :shape_curl)
+    eval(quote
+        @propagate_inbounds $op(fv::FaceValues, i::Int, q_point::Int) = $op(get_fun_values(fv), i, q_point)
+    end)
 end
 
-# Entrypoint for `VectorInterpolation`s (vdim == rdim == sdim)
-function OldFaceValues(::Type{T}, qr::QR, ip::IP, gip::VGIP) where {
-    dim, shape <: AbstractRefShape{dim}, T,
-    QR  <: FaceQuadratureRule{shape},
-    IP  <: VectorInterpolation{dim, shape},
-    GIP <: ScalarInterpolation{shape},
-    VGIP <: VectorizedInterpolation{dim, shape, <:Any, GIP},
-}
-    # Normals
-    Normal_t = Vec{dim, T}
-    # Function interpolation
-    N_t    = Vec{dim, T}
-    dNdx_t = dNdξ_t = Tensor{2, dim, T, Tensors.n_components(Tensor{2,dim})}
-    # Geometry interpolation
-    M_t    = T
-    dMdξ_t = Vec{dim, T}
-    return OldFaceValues{IP, N_t, dNdx_t, dNdξ_t, M_t, dMdξ_t, QR, Normal_t, GIP}(qr, ip, gip.ip)
-end
+"""
+    getcurrentface(fv::FaceValues)
 
-function reinit!(fv::OldFaceValues{<:Any, N_t, dNdx_t}, x::AbstractVector{Vec{dim,T}}, face::Int) where {
-    dim, T,
-    N_t    <: Union{Number,   Vec{dim}},
-    dNdx_t <: Union{Vec{dim}, Tensor{2,dim}}
-}
-    n_geom_basefuncs = getngeobasefunctions(fv)
-    n_func_basefuncs = getnbasefunctions(fv)
-    length(x) == n_geom_basefuncs || throw_incompatible_coord_length(length(x), n_geom_basefuncs)
-    @boundscheck checkface(fv, face)
+Return the current active face of the `FaceValues` object (from last `reinit!`).
 
-    fv.current_face[] = face
-    cb = getcurrentface(fv)
+"""
+getcurrentface(fv::FaceValues) = fv.current_face[]
 
-    @inbounds for (i, w) in pairs(getweights(fv.qr, cb))
-        fefv_J = zero(Tensor{2,dim})
-        for j in 1:n_geom_basefuncs
-            fefv_J += x[j] ⊗ fv.dMdξ[j, i, cb]
-        end
-        weight_norm = weighted_normal(fefv_J, fv, cb)
-        fv.normals[i] = weight_norm / norm(weight_norm)
-        detJ = norm(weight_norm)
+"""
+    getnormal(fv::FaceValues, qp::Int)
 
-        detJ > 0.0 || throw_detJ_not_pos(detJ)
-        fv.detJdV[i, cb] = detJ * w
-        Jinv = inv(fefv_J)
-        for j in 1:n_func_basefuncs
-            fv.dNdx[j, i, cb] = fv.dNdξ[j, i, cb] ⋅ Jinv
-        end
-    end
+Return the normal at the quadrature point `qp` for the active face of the
+`FaceValues` object(from last `reinit!`).
+"""
+getnormal(fv::FaceValues, qp::Int) = fv.normals[qp]
+
+nfaces(fv::FaceValues) = length(fv.geo_values)
+
+function checkface(fv::FaceValues, face::Int)
+    0 < face <= nfaces(fv) || error("Face index out of range.")
     return nothing
 end
 
-"""
-    getcurrentface(fv::OldFaceValues)
+function reinit!(fv::FaceValues, x::AbstractVector{Vec{dim,T}}, face_nr::Int, cell=nothing) where {dim, T}
+    @boundscheck checkface(fv, face_nr)
+    n_geom_basefuncs = getngeobasefunctions(fv)
+    length(x) == n_geom_basefuncs || throw_incompatible_coord_length(length(x), n_geom_basefuncs)
+    
+    fv.current_face[] = face_nr
 
-Return the current active face of the `OldFaceValues` object (from last `reinit!`).
+    geo_values = get_geo_values(fv)
+    fun_values = get_fun_values(fv)
+    @inbounds for (q_point, w) in pairs(getweights(fv.qr, face_nr))
+        mapping = calculate_mapping(geo_values, q_point, x)
+        J = getjacobian(mapping)
+        weight_norm = weighted_normal(J, getrefshape(geo_values.ip), face_nr)
+        detJ = norm(weight_norm)
+        detJ > 0.0 || throw_detJ_not_pos(detJ)
+        @inbounds fv.detJdV[q_point] = detJ*w
+        @inbounds fv.normals[q_point] = weight_norm / norm(weight_norm)       
+        apply_mapping!(fun_values, q_point, mapping, cell)
+    end
+end
 
-"""
-getcurrentface(fv::OldFaceValues) = fv.current_face[]
+function Base.show(io::IO, d::MIME"text/plain", fv::FaceValues)
+    ip_geo = get_geometric_interpolation(fv)
+    rdim = getdim(ip_geo)
+    vdim = isa(shape_value(fv, 1, 1), Vec) ? length(shape_value(fv, 1, 1)) : 0
+    sdim = length(shape_gradient(fv, 1, 1)) ÷ length(shape_value(fv, 1, 1))
+    vstr = vdim==0 ? "scalar" : "vdim=$vdim"
+    print(io, "FaceValues(", vstr, ", rdim=$rdim, sdim=$sdim): ")
+    nqp = getnquadpoints.(fv.qr.face_rules)
+    if all(n==first(nqp) for n in nqp)
+        println(io, first(nqp), " quadrature points per face")
+    else
+        println(io, tuple(nqp...), " quadrature points on each face")
+    end
+    print(io, " Function interpolation: "); show(io, d, get_function_interpolation(fv))
+    print(io, "\nGeometric interpolation: "); show(io, d, ip_geo^sdim)
+end
 
-"""
-    getnormal(fv::OldFaceValues, qp::Int)
-
-Return the normal at the quadrature point `qp` for the active face of the
-`OldFaceValues` object(from last `reinit!`).
-"""
-getnormal(fv::OldFaceValues, qp::Int) = fv.normals[qp]
-#= Moved to FaceValues.jl
 """
     BCValues(func_interpol::Interpolation, geom_interpol::Interpolation, boundary_type::Union{Type{<:BoundaryIndex}})
 
@@ -242,25 +168,4 @@ function spatial_coordinate(bcv::BCValues, q_point::Int, xh::AbstractVector{Vec{
         x += bcv.M[i,q_point,face] * xh[i] # geometric_value(fe_v, q_point, i) * xh[i]
     end
     return x
-end
-
-nfaces(fv::FaceValues) = size(fv.N, 3)
-
-function checkface(fv::FaceValues, face::Int)
-    0 < face <= nfaces(fv) || error("Face index out of range.")
-    return nothing
-end
-=#
-
-function Base.show(io::IO, m::MIME"text/plain", fv::OldFaceValues)
-    println(io, "FaceValues with")
-    nqp = getnquadpoints.(fv.qr.face_rules)
-    if all(n==first(nqp) for n in nqp)
-        println(io, "- Quadrature rule with ", first(nqp), " points per face")
-    else
-        println(io, "- Quadrature rule with ", tuple(nqp...), " points on each face")
-    end
-    print(io, "- Function interpolation: "); show(io, m, fv.func_interp)
-    println(io)
-    print(io, "- Geometric interpolation: "); show(io, m, fv.geo_interp)
 end
