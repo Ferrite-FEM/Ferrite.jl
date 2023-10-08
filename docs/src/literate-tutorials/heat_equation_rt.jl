3aad963b0726571052c10053e91415bdd207aa0a
Knut Andreas Meyer <knutam@gmail.com>
2023-10-08 18:39:24 +0200

    Generalize example to try with 2nd order Lagrange

--- docs/src/literate-tutorials/heat_equation_rt.jl
+++ docs/src/literate-tutorials/heat_equation_rt.jl
@@ -45,6 +45,7 @@ using Ferrite, SparseArrays
 # We start by generating a simple grid with 20x20 quadrilateral elements
 # using `generate_grid`. The generator defaults to the unit square,
 # so we don't need to specify the corners of the domain.
+#grid = generate_grid(QuadraticTriangle, (20, 20));
 grid = generate_grid(Triangle, (20, 20));
 
 # ### Trial and test functions
@@ -55,8 +56,8 @@ grid = generate_grid(Triangle, (20, 20));
 # based on the two-dimensional reference quadrilateral. We also define a quadrature rule based on
 # the same reference element. We combine the interpolation and the quadrature rule
 # to a `CellValues` object.
-ip_geo = Lagrange{RefTriangle, 1}()
-ipu = Lagrange{RefTriangle, 1}()
+ip_geo = Ferrite.default_interpolation(getcelltype(grid))
+ipu = Lagrange{RefTriangle, 1}() # Why does it "explode" for 2nd order ipu?
 ipq = RaviartThomas{2,RefTriangle,1}()
 qr = QuadratureRule{RefTriangle}(2)
 cellvalues = (u=CellValues(qr, ipu, ip_geo), q=CellValues(qr, ipq, ip_geo)) 
@@ -150,12 +151,12 @@ function assemble_element!(Ke::Matrix, fe::Vector, cv::NamedTuple, dr::NamedTupl
             δu  = shape_value(cvu, q_point, iu)
             ∇δu = shape_gradient(cvu, q_point, iu)
             ## Add contribution to fe
-            fe[Iu] += δu * dΩ
+            fe[Iu] -= δu * dΩ
             ## Loop over trial shape functions
             for (jq, Jq) in pairs(drq)
                 q = shape_value(cvq, q_point, jq)
                 ## Add contribution to Ke
-                Ke[Iu, Jq] -= (∇δu ⋅ q) * dΩ
+                Ke[Iu, Jq] += (∇δu ⋅ q) * dΩ
             end
         end
         for (iq, Iq) in pairs(drq)
@@ -192,6 +193,7 @@ end
 #     connection between the weak form and the Ferrite implementation.
 
 function assemble_global(cellvalues, K::SparseMatrixCSC, dh::DofHandler)
+    grid = dh.grid
     ## Allocate the element stiffness matrix and element force vector
     dofranges = (u = dof_range(dh, :u), q = dof_range(dh, :q))
     ncelldofs = ndofs_per_cell(dh)
@@ -201,12 +203,12 @@ function assemble_global(cellvalues, K::SparseMatrixCSC, dh::DofHandler)
     f = zeros(ndofs(dh))
     ## Create an assembler
     assembler = start_assemble(K, f)
-    x = copy(getcoordinates(dh.grid, 1))
+    x = copy(getcoordinates(grid, 1))
     dofs = copy(celldofs(dh, 1))
     ## Loop over all cels
-    for cellnr in 1:getncells(dh.grid)
+    for cellnr in 1:getncells(grid)
         ## Reinitialize cellvalues for this cell
-        cell = getcells(dh.grid, cellnr)
+        cell = getcells(grid, cellnr)
         getcoordinates!(x, grid, cell)
         celldofs!(dofs, dh, cellnr)
         reinit!(cellvalues[:u], x, cell)
@@ -251,9 +253,11 @@ end
 
 # ## Postprocess the total flux
 function calculate_flux(dh, dΩ, ip, a)
-    qr = FaceQuadratureRule{RefTriangle}(4)
-    fv = FaceValues(qr, ip, Lagrange{RefTriangle,1}())
     grid = dh.grid
+    qr = FaceQuadratureRule{RefTriangle}(4)
+    ip_geo = Ferrite.default_interpolation(getcelltype(grid))
+    fv = FaceValues(qr, ip, ip_geo)
+    
     dofrange = dof_range(dh, :q)
     flux = 0.0
     dofs = celldofs(dh, 1)
@@ -276,9 +280,10 @@ function calculate_flux(dh, dΩ, ip, a)
 end
 
 function calculate_flux_lag(dh, dΩ, ip, a)
-    qr = FaceQuadratureRule{RefTriangle}(4)
-    fv = FaceValues(qr, ip, Lagrange{RefTriangle,1}())
     grid = dh.grid
+    qr = FaceQuadratureRule{RefTriangle}(4)
+    ip_geo = Ferrite.default_interpolation(getcelltype(grid))
+    fv = FaceValues(qr, ip, ip_geo)
     dofrange = dof_range(dh, :u)
     flux = 0.0
     dofs = celldofs(dh, 1)
@@ -303,6 +308,25 @@ end
 flux = calculate_flux(dh, ∂Ω, ipq, u)
 flux_lag = calculate_flux_lag(dh, ∂Ω, ipu, u)
 @show flux, flux_lag
+
+
+function get_Ke(dh, cellvalues; cellnr=1)
+    dofranges = (u = dof_range(dh, :u), q = dof_range(dh, :q))
+    ncelldofs = ndofs_per_cell(dh)
+    Ke = zeros(ncelldofs, ncelldofs)
+    fe = zeros(ncelldofs)
+    x = getcoordinates(grid, cellnr)
+    cell = getcells(grid, cellnr)
+    reinit!(cellvalues[:u], x, cell)
+    reinit!(cellvalues[:q], x, cell)
+
+    ## Reset to 0
+    fill!(Ke, 0)
+    fill!(fe, 0)
+    ## Compute element contribution
+    assemble_element!(Ke, fe, cellvalues, dofranges)
+    return Ke
+end
 #md # ## [Plain program](@id heat_equation-plain-program)
 #md #
 #md # Here follows a version of the program without any comments.
