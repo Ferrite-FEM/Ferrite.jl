19f8b9764daef87d0a347a89126847bf0d69f2fb
termi-official <termi-official@users.noreply.github.com>
2023-10-13 00:09:24 +0200

    Fix CI

--- test/test_interpolations.jl
+++ test/test_interpolations.jl
@@ -51,7 +51,7 @@
         coords = Ferrite.reference_coordinates(interpolation)
         f = (x) -> [shape_value(interpolation, Tensor{1, ref_dim}(x), i) for i in 1:n_basefuncs]
         # @testset let x = sample_random_point(ref_shape) # not compatible with Julia 1.6
-        @testset "Random point test"
+        @testset "Random point test" begin
             x = sample_random_point(ref_shape)
             # Check gradient evaluation
             @test vec(ForwardDiff.jacobian(f, Array(x))') ≈
