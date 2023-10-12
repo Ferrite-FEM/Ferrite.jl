b1fd0e891bdc16178745be2fb1c1c29381e87484
termi-official <termi-official@users.noreply.github.com>
2023-10-13 00:28:19 +0200

    Test context info.

--- test/test_cellvalues.jl
+++ test/test_cellvalues.jl
@@ -1,5 +1,4 @@
-@testset "CellValues" begin
-for (scalar_interpol, quad_rule) in  (
+@testset "CellValues" for (scalar_interpol, quad_rule) in  (
                                     (Lagrange{RefLine, 1}(), QuadratureRule{RefLine}(2)),
                                     (Lagrange{RefLine, 2}(), QuadratureRule{RefLine}(2)),
                                     (Lagrange{RefQuadrilateral, 1}(), QuadratureRule{RefQuadrilateral}(2)),
