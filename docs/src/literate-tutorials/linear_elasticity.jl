82ad1641a3bcf59ad7bb4b7d14a1a8d6dd2bb253
Kim Louisa Auth <auth@chalmers.se>
2023-11-01 12:00:25 +0100

    remove internal force vector computation

--- docs/src/literate-tutorials/linear_elasticity.jl
+++ docs/src/literate-tutorials/linear_elasticity.jl
@@ -63,11 +63,13 @@
 # =
 # 0 \,.
 # ```
-
+#
+# Finally, we choose to operate on a 2-dimensional problem under plain strain conditions.
 # First we load Ferrite, and some other packages we need.
 using Ferrite, FerriteGmsh, SparseArrays
 # Like for the Heat Equation example, we will use a unit square - but here we'll load the grid of the Ferrite logo! This is done by loading [`logo.geo`](logo.geo) with [`FerriteGmsh.jl`](https://github.com/Ferrite-FEM/FerriteGmsh.jl) here.
 grid = togrid("logo.geo");
+#md nothing # hide
 # By default the grid lacks the facesets for the boundaries, so we add them by Ferrite here.
 # Note that approximate comparison to 0.0 doesn't work well, so we use a tolerance instead.
 addfaceset!(grid, "top", x->x[2] ≈ 1.0)
@@ -99,53 +101,43 @@ qr = QuadratureRule{RefTriangle}(1) # 1 quadrature point
 cellvalues = CellValues(qr, ip);
 
 # ### Degrees of freedom
-# For distributing degrees of freedom, we define a `DofHandler`. The `DofHandler` knows that `u` has two degrees of freedom per node because we vectorized the interpolation above.
+# For distributing degrees of freedom, we define a `DofHandler`. The `DofHandler` knows that
+# `u` has two degrees of freedom per node because we vectorized the interpolation above.
 dh = DofHandler(grid)
 add!(dh, :u, ip)
 close!(dh);
 
 # ### Boundary conditions
-# Now, we add boundary conditions. We simply support the bottom and the left side and prescribe a displacement upwards on the top edge.
-# The last argument to `Dirichlet` determines which components of the field should be constrained.
+# Now, we add boundary conditions. We simply support the bottom and the left side and
+# prescribe a displacement upwards on the top edge.
+# The last argument to `Dirichlet` determines which components of the field should be
+# constrained.
 ch = ConstraintHandler(dh)
 add!(ch, Dirichlet(:u, getfaceset(grid, "bottom"), (x, t) -> 0.0, 2))
 add!(ch, Dirichlet(:u, getfaceset(grid, "left"), (x, t) -> 0.0, 1))
 add!(ch, Dirichlet(:u, getfaceset(grid, "top"), (x, t) -> 0.1, 2))
 close!(ch);
 
-# ### Material routine
-# For the sake of structuring the program, we introduce a material routine here.
-# We define a data structure that holds the material constants, here it stores the
-# shear modulus $G$ and the bulk modulus $K$. The material routine then computes the stress
-# $\boldsymbol{\sigma}$ and its tangent $\frac{\partial\boldsymbol{\sigma}}{\partial\boldsymbol{\varepsilon}}$
-# based on the input strain $\boldsymbol{\varepsilon}$. Here, we use automatic differentiation
-# for computing the stress tangent. You can read more about automatic differentiation on tensor
-# operations in the [`Tensors.jl` docs](https://ferrite-fem.github.io/Tensors.jl/stable/man/automatic_differentiation/).
-struct Elasticity
-    G::Float64
-    K::Float64
-end
-
-function material_routine(material::Elasticity, ε::SymmetricTensor{2})
-    (; G, K) = material
-    stress(ε) = 2G * dev(ε) + K * tr(ε) * one(ε)
-    ∂σ∂ε, σ = gradient(stress, ε, :all)
-    return σ, ∂σ∂ε
-end
-
+# ### Material behavior
+# Next, we need to define the material behavior. Since we use linear elasticity here,
+# we have a linear problem and only need to assemble the stiffness matrix, but not the 
+# residual vector. Consequently we only need the tangent of the stress $\boldsymbol{\sigma}$
+# with respect to the small strain tensor $\boldsymbol{\varepsilon}$ for our element routine.
+#
+# We also operate on a 2-dimensional problem under plain strain conditions here, keep in
+# mind that the plane stress stiffness tensor is defined differently.
 E = 200e3 # Young's modulus [MPa]
 ν = 0.3 # Poisson's ratio [-]
-material = Elasticity(E/2(1+ν), E/3(1-2ν));
+
+λ = E*ν / ((1 + ν) * (1 - 2ν)) # 1st Lamé parameter
+μ = E / (2(1 + ν)) # 2nd Lamé parameter
+I = one(SymmetricTensor{2, dim}) # 2nd order unit tensor
+II = one(SymmetricTensor{4, dim}) # 4th order symmetric unit tensor
+∂σ∂ε = 2μ * II + λ * (I ⊗ I) # elastic stiffness tensor
 
 # ### Element routine
-# The residual vector and stiffness matrix follow from the weak form such that
+# The stiffness matrix follows from the weak form such that
 # ```math
-# \left(\underline{R}\right)_i
-# =
-# \int_\Omega
-#   \boldsymbol{\sigma} : \boldsymbol{\nabla} \boldsymbol{N}_i
-# \, \mathrm{d}V
-# \,, \qquad
 # \left(\underline{\underline{K}}\right)_{ij}
 # =
 # \int_\Omega
@@ -158,22 +150,19 @@ material = Elasticity(E/2(1+ν), E/3(1-2ν));
 #   \boldsymbol{\nabla} \boldsymbol{N}_i
 # \, \mathrm{d}V
 # ```
-# The element routine computes the local residual vector `re` and stiffness matrix `ke`
-# for a single element. `ke` and `re` are pre-allocated and reused for all elements.
-function assemble_cell!(ke, fe, cellvalues, material, ue)
+# The element routine computes the local  stiffness matrix `ke`
+# for a single element. `ke` is pre-allocated and reused for all elements.
+#
+# Note that the elastic stiffness tensor is constant. Thus is needs to be computed and once
+# and can then be used for all integration points.
+function assemble_cell!(ke, cellvalues, ∂σ∂ε)
     fill!(ke, 0.0)
-    fill!(fe, 0.0)
 
     n_basefuncs = getnbasefunctions(cellvalues)
     for q_point in 1:getnquadpoints(cellvalues)
-        ## For each integration point, compute strain, stress and material stiffness
-        ε = function_symmetric_gradient(cellvalues, q_point, ue)
-        σ, ∂σ∂ε = material_routine(material, ε)
-
         dΩ = getdetJdV(cellvalues, q_point)
         for i in 1:n_basefuncs
             ∇Nᵢ = shape_gradient(cellvalues, q_point, i)# shape_symmetric_gradient(cellvalues, q_point, i)
-            fe[i] += σ ⊡ ∇Nᵢ * dΩ # add internal force to residual
             for j in 1:n_basefuncs
                 ∇ˢʸᵐNⱼ = shape_symmetric_gradient(cellvalues, q_point, j)
                 ke[i, j] += (∂σ∂ε ⊡ ∇ˢʸᵐNⱼ) ⊡ ∇Nᵢ * dΩ
@@ -185,38 +174,35 @@ end
 
 # #### Global assembly
 # We define the function `assemble_global` to loop over the elements and do the global
-# assembly. The function takes the sparse matrix `K`, a guess of the displacement vector
-# (this would be more relevant for non-linear problems), our DofHandler, our `cellvalues` and
-# the material definition that we want to use for the stress computation
-# as input arguments and fills the assembled global stiffness matrix, and the assembled
-# global force vector. 
-function assemble_global!(K, f, a, dh, cellvalues, material)
-    ## Allocate the element stiffness matrix and element force vector
+# assembly. The function takes the preallocated sparse matrix `K`, our DofHandler `dh`, our
+# `cellvalues` and the elastic stiffness tensor `∂σ∂ε` as input arguments and computes the
+# global stiffness matrix `K`.
+function assemble_global!(K, dh, cellvalues, ∂σ∂ε)
+    ## Allocate the element stiffness matrix
     n_basefuncs = getnbasefunctions(cellvalues)
     ke = zeros(n_basefuncs, n_basefuncs)
-    fe = zeros(n_basefuncs)
     ## Create an assembler
-    assembler = start_assemble(K, f)
+    assembler = start_assemble(K)
     ## Loop over all cells
     for cell in CellIterator(dh)
         reinit!(cellvalues, cell) # update spatial derivatives based on element coordinates
-        @views ue = a[celldofs(cell)]
         ## Compute element contribution
-        assemble_cell!(ke, fe, cellvalues, material, ue)
+        assemble_cell!(ke, cellvalues, ∂σ∂ε)
         ## Assemble ke and fe into K and f
-        assemble!(assembler, celldofs(cell), ke, fe)
+        assemble!(assembler, celldofs(cell), ke)
     end
-    return K, f
+    return K
 end
 #md nothing # hide
 
 # ### Solution of the system
-# The last step is to solve the system. First we call `assemble_global`
-# to obtain the global stiffness matrix `K` and force vector `f`.
+# The last step is to solve the system. First we allocate the global stiffness matrix `K`
+# and assemble it.
 K = create_sparsity_pattern(dh)
-f = zeros(ndofs(dh))
-a = zeros(ndofs(dh))
-assemble_global!(K, f, a, dh, cellvalues, material);
+assemble_global!(K, dh, cellvalues, ∂σ∂ε);
+# Then we allocate the external force vector. Since we don't apply any external forces,
+# it is a zero vector in this case.
+f = zeros(ndofs(dh));
 
 # To account for the Dirichlet boundary conditions we use the `apply!` function.
 # This modifies elements in `K` and `f` respectively, such that
@@ -229,14 +215,14 @@ u = K \ f;
 # to a VTK-file, which can be viewed in e.g. [ParaView](https://www.paraview.org/).
 vtk_grid("linear_elasticity", dh) do vtk
     vtk_point_data(vtk, dh, u)
-    vtk_cellset(vtk, grid)
+    vtk_cellset(vtk, grid) # export cellsets of grains for logo-coloring
 end
 #md nothing # hide
 
-#md # ## [Plain program](@id heat_equation-plain-program)
+#md # ## [Plain program](@id linear_elasticity-plain-program)
 #md #
 #md # Here follows a version of the program without any comments.
-#md # The file is also available here: [`heat_equation.jl`](heat_equation.jl).
+#md # The file is also available here: [`linear_elasticity.jl`](linear_elasticity).
 #md #
 #md # ```julia
 #md # @__CODE__
