ad353da63b69982aecc6aa0efeccc106313c8a86
Knut Andreas Meyer <knutam@gmail.com>
2024-04-26 09:01:48 +0200

    Don't import push

--- src/deprecations.jl
+++ src/deprecations.jl
@@ -1,8 +1,7 @@
 Base.@deprecate_binding DirichletBoundaryConditions ConstraintHandler
 Base.@deprecate_binding DirichletBoundaryCondition Dirichlet
 
-import Base: push!
-@deprecate push!(dh::AbstractDofHandler, args...) add!(dh, args...)
+@deprecate Base.push!(dh::AbstractDofHandler, args...) add!(dh, args...)
 
 @deprecate vertices(ip::Interpolation) vertexdof_indices(ip) false
 @deprecate faces(ip::Interpolation) facedof_indices(ip) false
