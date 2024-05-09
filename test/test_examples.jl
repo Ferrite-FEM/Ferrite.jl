17bb6ec40b14a5d66231ac17980a08d732956812
Knut Andreas Meyer <knutam@gmail.com>
2024-05-09 13:55:20 +0200

    revert test_examples to master version

--- test/test_examples.jl
+++ test/test_examples.jl
@@ -1,27 +1,5 @@
 # Test the scripts
 
-module TestStokesFlow
-if !Sys.iswindows()
-    mktempdir() do dir
-        cd(dir) do
-            include(joinpath(@__DIR__, "../docs/src/literate-tutorials/stokes-flow.jl"))
-        end
-    end
-end
-
-module TestComputationalHomogenization
-    include(joinpath(@__DIR__, "../docs/download_resources.jl"))
-    mktempdir() do dir
-        cd(dir) do
-            cp(joinpath(@__DIR__, "../docs/src/tutorials/periodic-rve.msh"),
-               joinpath(dir, "periodic-rve.msh")
-            )
-            include(joinpath(@__DIR__, "../docs/src/literate-tutorials/computational_homogenization.jl"))
-        end
-    end
-end
-
-
 module TestHeatEquationExample
     mktempdir() do dir
         cd(dir) do
@@ -62,6 +40,24 @@ end
 #     end
 # end
 
+module TestComputationalHomogenization
+    include(joinpath(@__DIR__, "../docs/download_resources.jl"))
+    mktempdir() do dir
+        cd(dir) do
+            cp(joinpath(@__DIR__, "../docs/src/tutorials/periodic-rve.msh"),
+               joinpath(dir, "periodic-rve.msh")
+            )
+            include(joinpath(@__DIR__, "../docs/src/literate-tutorials/computational_homogenization.jl"))
+        end
+    end
+end
 
-
+module TestStokesFlow
+if !Sys.iswindows()
+    mktempdir() do dir
+        cd(dir) do
+            include(joinpath(@__DIR__, "../docs/src/literate-tutorials/stokes-flow.jl"))
+        end
+    end
+end
 end
