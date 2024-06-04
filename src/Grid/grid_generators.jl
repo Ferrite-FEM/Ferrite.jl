14805f36448eb77449da50739434e2dc64b3046e
Abdelrahman Fathy (Tee) <abdelrahmanfathe95@gmail.com>
2024-06-04 23:40:53 +0200

    add descriptive comments and add additional test to check entries type for grids

--- src/Grid/grid_generators.jl
+++ src/Grid/grid_generators.jl
@@ -69,6 +69,11 @@ end
 
 function _generate_2d_nodes!(nodes, nx, ny, LL, LR, UR, UL)
       for i in 0:ny-1
+
+        # This float division will be by default Float64, 
+        # so we need to convert it to the same type as element type of LL
+        # e.g. LL = Vec{2,Float16} -> eltype(LL) = Float16
+        # ratio_bounds =  (i / (ny-1)) # old code
         ratio_bounds = convert(eltype(LL), (i / (ny-1)))
 
         x0 = LL[1] * (1 - ratio_bounds) + ratio_bounds * UL[1]
@@ -78,6 +83,10 @@ function _generate_2d_nodes!(nodes, nx, ny, LL, LR, UR, UL)
         y1 = LR[2] * (1 - ratio_bounds) + ratio_bounds * UR[2]
 
         for j in 0:nx-1
+            # This float division will be by default Float64, 
+            # so we need to convert it to the same type as element type of LL
+            # e.g. LL = Vec{2,Float16} -> eltype(LL) = Float16
+            # ratio = j / (nx-1) # old code
             ratio = convert(eltype(LL), j / (nx-1))
             x = x0 * (1 - ratio) + ratio * x1
             y = y0 * (1 - ratio) + ratio * y1
@@ -284,6 +293,9 @@ function generate_grid(::Type{Pyramid}, nel::NTuple{3,Int}, left::Vec{3,T}=Vec{3
 
     #Center node in each "voxel"
     for k in 1:nel_z, j in 1:nel_y, i in 1:nel_x
+        # midx = 0.5(coords_x[i+1] + coords_x[i]) # old code, this will be always Float64 regardless of the element type of left
+        # midy = 0.5(coords_y[j+1] + coords_y[j]) # old code, this will be always Float64 regardless of the element type of left
+        # midz = 0.5(coords_z[k+1] + coords_z[k]) # old code, this will be always Float64 regardless of the element type of left
         midx = convert(eltype(left),0.5(coords_x[i+1] + coords_x[i]))
         midy = convert(eltype(left),0.5(coords_y[j+1] + coords_y[j]))
         midz = convert(eltype(left),0.5(coords_z[k+1] + coords_z[k]))
--- test/test_grid_generators.jl
+++ test/test_grid_generators.jl
@@ -2,55 +2,75 @@ using Test
 using Ferrite
 using Tensors
 
-
-
 # Helper function to test grid generation for a given floating-point type
 function test_generate_grid(T::Type)
+    # Helper function to check the type of nodes and cells in the grid
+    function check_grid_types(grid::Grid{dim, C, T}) where {dim, C <: Ferrite.AbstractCell, T}
+        # Check nodes
+        for node in grid.nodes
+            @test typeof(get_node_coordinate(node)) == Vec{dim, T}
+        end
+        
+        # Check cells
+        @test all(typeof(cell) == C for cell in grid.cells)
+    end
+    
     # Testing Line grid
     nel = (10,)
     left = Vec{1,T}((-1.0,))
     right = Vec{1,T}((1.0,))
     grid = generate_grid(Line, nel, left, right)
-    @test isa(grid, Grid)
+    @test isa(grid, Grid{1, Line, T})
+    check_grid_types(grid)
     
     # Testing QuadraticLine grid
     grid = generate_grid(QuadraticLine, nel, left, right)
-    @test isa(grid, Grid)
+    @test isa(grid, Grid{1, QuadraticLine, T})
+    check_grid_types(grid)
 
     # Testing 2D grids
     nel = (10, 10)
     left_2d = Vec{2,T}((-1.0, -1.0))
     right_2d = Vec{2,T}((1.0, 1.0))
     grid = generate_grid(Quadrilateral, nel, left_2d, right_2d)
-    @test isa(grid, Grid)
+    @test isa(grid, Grid{2, Quadrilateral, T})
+    check_grid_types(grid)
 
     grid = generate_grid(QuadraticQuadrilateral, nel, left_2d, right_2d)
-    @test isa(grid, Grid)
+    @test isa(grid, Grid{2, QuadraticQuadrilateral, T})
+    check_grid_types(grid)
 
     grid = generate_grid(Triangle, nel, left_2d, right_2d)
-    @test isa(grid, Grid)
+    @test isa(grid, Grid{2, Triangle, T})
+    check_grid_types(grid)
 
     grid = generate_grid(QuadraticTriangle, nel, left_2d, right_2d)
-    @test isa(grid, Grid)
+    @test isa(grid, Grid{2, QuadraticTriangle, T})
+    check_grid_types(grid)
 
     # Testing 3D grids
     nel = (10, 10, 10)
     left_3d = Vec{3,T}((-1.0, -1.0, -1.0))
     right_3d = Vec{3,T}((1.0, 1.0, 1.0))
     grid = generate_grid(Hexahedron, nel, left_3d, right_3d)
-    @test isa(grid, Grid)
+    @test isa(grid, Grid{3, Hexahedron, T})
+    check_grid_types(grid)
 
     grid = generate_grid(Wedge, nel, left_3d, right_3d)
-    @test isa(grid, Grid)
+    @test isa(grid, Grid{3, Wedge, T})
+    check_grid_types(grid)
 
     grid = generate_grid(Pyramid, nel, left_3d, right_3d)
-    @test isa(grid, Grid)
+    @test isa(grid, Grid{3, Pyramid, T})
+    check_grid_types(grid)
 
     grid = generate_grid(Tetrahedron, nel, left_3d, right_3d)
-    @test isa(grid, Grid)
+    @test isa(grid, Grid{3, Tetrahedron, T})
+    check_grid_types(grid)
 
     grid = generate_grid(SerendipityQuadraticHexahedron, nel, left_3d, right_3d)
-    @test isa(grid, Grid)
+    @test isa(grid, Grid{3, SerendipityQuadraticHexahedron, T})
+    check_grid_types(grid)
 end
 
 # Run tests for different floating-point types
