1d2d2c8956512cf848b9cc584385234b8ba085ac
Knut Andreas Meyer <knutam@gmail.com>
2024-06-06 21:57:03 +0200

    Fix topopt example 2

--- docs/src/literate-gallery/topology_optimization.jl
+++ docs/src/literate-gallery/topology_optimization.jl
@@ -202,9 +202,9 @@ function cache_neighborhood(dh, topology)
         for j in 1:_nfacets
             nbg_cellid = getneighborhood(topology, dh.grid, FacetIndex(i,j))
             if(!isempty(nbg_cellid))
-                nbg[j] = first(nbg_cellid) # assuming only one face neighbor per cell
+                nbg[j] = first(nbg_cellid)[1] # assuming only one face neighbor per cell
             else # boundary face
-                nbg[j] = first(getneighborhood(topology, dh.grid, FacetIndex(i,opp[j])))
+                nbg[j] = first(getneighborhood(topology, dh.grid, FacetIndex(i,opp[j])))[1]
             end
         end
 
