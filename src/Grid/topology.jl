c428fbb4bed6c8c5e69f9eb0a958809997d289d9
Knut Andreas Meyer <knutam@gmail.com>
2024-06-21 15:54:51 +0200

    Clarify experimental status in docstring

--- src/Grid/topology.jl
+++ src/Grid/topology.jl
@@ -19,10 +19,9 @@ abstract type AbstractTopology end
 """
     ExclusiveTopology(grid::AbstractGrid)
 
-`ExclusiveTopology` saves topological (connectivity/neighborhood) data of the grid. The constructor works with an `AbstractCell`
-vector for all cells that dispatch `vertices`, `edges`, and `faces`.
-The struct saves the highest dimensional neighborhood, i.e. if something is connected by a face and an
- edge only the face neighborhood is saved. The lower dimensional neighborhood is recomputed, if needed.
+The **experimental feature** `ExclusiveTopology` saves topological (connectivity/neighborhood) data of the grid.
+Only the highest dimensional neighborhood is saved. I.e., if something is connected by a face and an
+edge, only the face neighborhood is saved. The lower dimensional neighborhood is recomputed when calling getneighborhood if needed.
 
 # Fields
 - `vertex_to_cell::AbstractArray{AbstractVector{Int}, 1}`:           global vertex id to all cells containing the vertex
@@ -37,7 +36,7 @@ The struct saves the highest dimensional neighborhood, i.e. if something is conn
 !!! warning "Limitations"
     Non-conforming grids will silently not work.
     Grids with embedded cells (different reference dimension compared
-    to the spatial dimension) is not supported, and will error on construction.
+    to the spatial dimension) are not supported, and will error on construction.
 
 """
 mutable struct ExclusiveTopology <: AbstractTopology
