18128aee5b45ef777fa022e269c4d2d511c11ebd
termi-official <termi-official@users.noreply.github.com>
2024-07-15 14:19:08 +0200

    ws

--- src/Quadrature/quadrature.jl
+++ src/Quadrature/quadrature.jl
@@ -180,7 +180,7 @@ Create a `FacetQuadratureRule` used for integration of the faces of the refshape
 type [`AbstractRefShape`](@ref)). `order` is the order of the quadrature rule.
 If no symbol is provided, the default `quad_rule_type` for each facet's reference shape is used (see [QuadratureRule](@ref)).
 For non-default `quad_rule_type`s on cells with mixed facet types (e.g. `RefPrism` and `RefPyramid`), the
-`face_rules` must be provided explicitly. 
+`face_rules` must be provided explicitly.
 
 `FacetQuadratureRule` is used as one of the components to create [`FacetValues`](@ref).
 """
