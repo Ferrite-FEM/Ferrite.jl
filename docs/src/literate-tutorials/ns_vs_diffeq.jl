Ferrite-FEM/Ferrite.jl

commit 9f083f2019e981b5135c344f345d3773e61af11a

Remove VTKFileCollection (#1015)
Restores pre #692 functionality for collection files by removing VTKFileCollection.

docs/src/literate-tutorials/ns_vs_diffeq.jl
@@ -119,7 +119,7 @@
 # The full program, without comments, can be found in the next [section](@ref ns_vs_diffeq-plain-program).
 #
 # First we load Ferrite and some other packages we need
-using Ferrite, SparseArrays, BlockArrays, LinearAlgebra, UnPack, LinearSolve
+using Ferrite, SparseArrays, BlockArrays, LinearAlgebra, UnPack, LinearSolve, WriteVTK
 # Since we do not need the complete DifferentialEquations suite, we just load the required ODE infrastructure, which can also handle
 # DAEs in mass matrix form.
 using OrdinaryDiffEq
@@ -576,10 +576,13 @@ integrator = init(
 # !!! note "Export of solution"
 #     Exporting interpolated solutions of problems containing mass matrices is currently broken.
 #     Thus, the `intervals` iterator is used. Note that `solve` holds all solutions in the memory.
-pvd = VTKFileCollection("vortex-street", grid);
+pvd = paraview_collection("vortex-street")
+step = 0
 for (u,t) in intervals(integrator)
-    addstep!(pvd, t) do io
-        write_solution(io, dh, u)
+    step += 1
+    VTKGridFile("vortex-street-$step", dh) do vtk
+        write_solution(vtk, dh, u)
+        pvd[t] = vtk
     end
 end
 close(pvd);

docs/Manifest.toml
@@ -2,7 +2,7 @@
 
 julia_version = "1.10.4"
 manifest_format = "2.0"
-project_hash = "8722b6a67abf1b81e2c7808ddfcfb1d737cd0040"
+project_hash = "2283fea1dfcfa0f2c82cdecbc5750875c26d6009"
 
 [[deps.ADTypes]]
 git-tree-sha1 = "3a6511b6e54550bcbc986c560921a8cd7761fcd8"
@@ -1987,9 +1987,9 @@ version = "1.31.0+0"
 
 [[deps.WriteVTK]]
 deps = ["Base64", "CodecZlib", "FillArrays", "LightXML", "TranscodingStreams", "VTKBase"]
-git-tree-sha1 = "48b9e8e9c83865e99e57f027d4edfa94e0acddae"
+git-tree-sha1 = "46664bb833f24e4fe561192e3753c9168c3b71b2"
 uuid = "64499a7a-5c06-52f2-abe2-ccb03c286192"
-version = "1.19.1"
+version = "1.19.2"
 
 [[deps.XML2_jll]]
 deps = ["Artifacts", "JLLWrappers", "Libdl", "Libiconv_jll", "Zlib_jll"]

docs/Project.toml
@@ -20,3 +20,4 @@ StaticArrays = "90137ffa-7385-5640-81b9-e52037218182"
 Tensors = "48a634ad-e948-5137-8d70-aa71f2a747f4"
 TimerOutputs = "a759f4b9-e2f1-59dc-863e-4aeb61b1ea8f"
 UnPack = "3a884ed6-31ef-47d7-9d2a-63182c4928ed"
+WriteVTK = "64499a7a-5c06-52f2-abe2-ccb03c286192"

docs/src/literate-gallery/quasi_incompressible_hyperelasticity.jl
@@ -72,7 +72,7 @@
 # ## Implementation
 # We now get to the actual code. First, we import the respective packages
 
-using Ferrite, Tensors, ProgressMeter
+using Ferrite, Tensors, ProgressMeter, WriteVTK
 using BlockArrays, SparseArrays, LinearAlgebra
 
 # and the corresponding `struct` to store our material properties.
@@ -305,8 +305,8 @@ function solve(interpolation_u, interpolation_p)
     Δt = 0.1;
     NEWTON_TOL = 1e-8
 
-    pvd = VTKFileCollection("hyperelasticity_incomp_mixed", grid);
-    for t ∈ 0.0:Δt:Tf
+    pvd = paraview_collection("hyperelasticity_incomp_mixed");
+    for (step, t) ∈ enumerate(0.0:Δt:Tf)
         ## Perform Newton iterations
         Ferrite.update!(dbc, t)
         apply!(w, dbc)
@@ -334,8 +334,9 @@ function solve(interpolation_u, interpolation_p)
         end;
 
         ## Save the solution fields
-        addstep!(pvd, t) do io
-            write_solution(io, dh, w)
+        VTKGridFile("hyperelasticity_incomp_mixed_$step", grid) do vtk
+            write_solution(vtk, dh, w)
+            pvd[t] = vtk
         end
     end;
     close(pvd);

docs/src/literate-tutorials/porous_media.jl
@@ -96,7 +96,7 @@
 #md # the final [section](@ref porous-media-plain-program)
 #
 # Required packages
-using Ferrite, FerriteMeshParser, Tensors
+using Ferrite, FerriteMeshParser, Tensors, WriteVTK
 
 # ### Elasticity
 # We start by defining the elastic material type, containing the elastic stiffness,
@@ -335,7 +335,8 @@ function solve(dh, ch, domains; Δt=0.025, t_total=1.0)
     r = zeros(ndofs(dh))
     a = zeros(ndofs(dh))
     a_old = copy(a)
-    pvd = VTKFileCollection("porous_media.pvd", dh);
+    pvd = paraview_collection("porous_media")
+    step = 0
     for t in 0:Δt:t_total
         if t>0
             update!(ch, t)
@@ -347,8 +348,10 @@ function solve(dh, ch, domains; Δt=0.025, t_total=1.0)
             a .+= Δa
             copyto!(a_old, a)
         end
-        addstep!(pvd, t) do io
-            write_solution(io, dh, a)
+        step += 1
+        VTKGridFile("porous_media_$step", dh) do vtk
+            write_solution(vtk, dh, a)
+            pvd[t] = vtk
         end
     end
     close(pvd);

docs/src/literate-tutorials/reactive_surface.jl
@@ -77,7 +77,7 @@ nothing                    #hide
 # First we load Ferrite, and some other packages we need
 
 using Ferrite, FerriteGmsh
-using BlockArrays, SparseArrays, LinearAlgebra
+using BlockArrays, SparseArrays, LinearAlgebra, WriteVTK
 
 # ### Assembly routines
 # Before we head into the assembly, we define a helper struct to control the dispatches.
@@ -276,9 +276,10 @@ function gray_scott_on_sphere(material::GrayScottMaterial, Δt::Real, T::Real, r
     setup_initial_conditions!(uₜ₋₁, cellvalues, dh)
 
     ## And prepare output for visualization.
-    pvd = VTKFileCollection("reactive-surface.pvd", dh);
-    addstep!(pvd, 0.0) do io
-        write_solution(io, dh, uₜ₋₁)
+    pvd = paraview_collection("reactive-surface")
+    VTKGridFile("reactive-surface-0", dh) do vtk
+        write_solution(vtk, dh, uₜ₋₁)
+        pvd[0.0] = vtk
     end
 
     ## This is now the main solve loop.
@@ -302,8 +303,9 @@ function gray_scott_on_sphere(material::GrayScottMaterial, Δt::Real, T::Real, r
         ## The solution is then stored every 10th step to vtk files for
         ## later visualization purposes.
         if (iₜ % 10) == 0
-            addstep!(pvd, t) do io
-                write_solution(io, dh, uₜ₋₁)
+            VTKGridFile("reactive-surface-$(iₜ)", dh) do vtk
+                write_solution(vtk, dh, uₜ₋₁)
+                pvd[t] = vtk
             end
         end
 

docs/src/literate-tutorials/transient_heat_equation.jl
@@ -58,7 +58,7 @@
 #md # The full program, without comments, can be found in the next [section](@ref heat_equation-plain-program).
 #
 # First we load Ferrite, and some other packages we need.
-using Ferrite, SparseArrays
+using Ferrite, SparseArrays, WriteVTK
 # We create the same grid as in the heat equation example.
 grid = generate_grid(Quadrilateral, (100, 100));
 
@@ -189,15 +189,15 @@ apply_analytical!(uₙ, dh, :u, x -> (x[1]^2 - 1) * (x[2]^2 - 1) * max_temp);
 # Here, we apply **once** the boundary conditions to the system matrix `A`.
 apply!(A, ch);
 
-# To store the solution, we initialize a `VTKFileCollection` (.pvd) file.
-pvd = VTKFileCollection("transient-heat", grid);
-t = 0
-addstep!(pvd, t) do io
-    write_solution(io, dh, uₙ)
+# To store the solution, we initialize a paraview collection (.pvd) file,
+pvd = paraview_collection("transient-heat")
+VTKGridFile("transient-heat-0", dh) do vtk
+    write_solution(vtk, dh, uₙ)
+    pvd[0.0] = vtk
 end
 
 # At this point everything is set up and we can finally approach the time loop.
-for t in Δt:Δt:T
+for (step, t) in enumerate(Δt:Δt:T)
     #First of all, we need to update the Dirichlet boundary condition values.
     update!(ch, t)
 
@@ -209,8 +209,9 @@ for t in Δt:Δt:T
     #Finally, we can solve the time step and save the solution afterwards.
     u = A \ b
 
-    addstep!(pvd, t) do io
-        write_solution(io, dh, u)
+    VTKGridFile("transient-heat-$step", dh) do vtk
+        write_solution(vtk, dh, u)
+        pvd[t] = vtk
     end
     #At the end of the time loop, we set the previous solution to the current one and go to the next time step.
     uₙ .= u

src/Export/VTK.jl
@@ -54,80 +54,11 @@ function Base.show(io::IO, ::MIME"text/plain", vtk::VTKGridFile)
     print(io, "VTKGridFile for the $open_str file \"$(filename)\".")
 end
 
-"""
-    VTKFileCollection(name::String, grid::AbstractGrid; kwargs...)
-    VTKFileCollection(name::String, dh::DofHandler; kwargs...)
-
-Create a paraview data file (.pvd) that can be used to
-save multiple vtk file along with a time stamp. The keyword arguments
-are forwarded to `WriteVTK.paraview_collection`.
-
-See [`addstep!`](@ref) for examples for how to use `VTKFileCollection`.
-```
-"""
-mutable struct VTKFileCollection{P<:WriteVTK.CollectionFile,G_DH}
-    pvd::P
-    grid_or_dh::G_DH
-    name::String
-    step::Int
+function WriteVTK.collection_add_timestep(pvd::WriteVTK.CollectionFile, datfile::VTKGridFile, time::Real)
+    WriteVTK.collection_add_timestep(pvd, datfile.vtk, time)
 end
-function VTKFileCollection(name::String, grid_or_dh::Union{AbstractGrid,AbstractDofHandler}; kwargs...)
-    pvd = WriteVTK.paraview_collection(name; kwargs...)
-    basename = string(first(split(pvd.path, ".pvd")))
-    return VTKFileCollection(pvd, grid_or_dh, basename, 0)
-end
-Base.close(pvd::VTKFileCollection) = WriteVTK.vtk_save(pvd.pvd)
-
-"""
-    addstep!(f::Function, pvd::VTKFileCollection, t::Real, [grid_or_dh]; kwargs...)
-
-Add a step at time `t` by writing a `VTKGridFile` to `pvd`.
-The keyword arguments are forwarded to `WriteVTK.vtk_grid`.
-If required, a new grid can be used by supplying the grid or dofhandler as the last argument.
-Should be used in a do-block, e.g.
-```julia
-filename = "myoutput"
-pvd = VTKFileCollection(filename, grid)
-for (n, t) in pairs(timevector)
-    # Calculate, e.g., the solution `u` and the stress `σeff`
-    addstep!(pvd, t) do io
-        write_cell_data(io, σeff, "Effective stress")
-        write_solution(io, dh, u)
-    end
-end
-close(pvd)
-```
-"""
-function addstep!(f::Function, pvd::VTKFileCollection, t, grid=pvd.grid_or_dh; kwargs...)
-    pvd.step += 1
-    VTKGridFile(string(pvd.name, "_", pvd.step), grid; kwargs...) do vtk
-        f(vtk)
-        pvd.pvd[t] = vtk.vtk # Add to collection
-    end
-end
-
-"""
-    addstep!(pvd::VTKFileCollection, vtk::VTKGridFile, t)
-
-As an alternative to using the `addstep!(pvd, t) do` block, it is
-also possible to add a specific `vtk` at time `t` to `pvd`.
-Note that this will close the `vtk`. Example
-```julia
-filename = "myoutput"
-pvd = VTKFileCollection(filename, grid)
-for (n, t) in pairs(timevector)
-    # Calculate, e.g., the solution `u` and the stress `σeff`
-    vtk = VTKGridFile(string(filename, "_", n), dh)
-    write_cell_data(vtk, σeff, "Effective stress")
-    write_solution(vtk, dh, u)
-    addstep!(pvd, vtk, t)
-end
-close(pvd)
-```
-"""
-function addstep!(pvd::VTKFileCollection, vtk::VTKGridFile, t)
-    pvd.step += 1
-    pvd.pvd[t] = vtk.vtk
+function Base.setindex!(pvd::WriteVTK.CollectionFile, datfile::VTKGridFile, time::Real)
+    WriteVTK.collection_add_timestep(pvd, datfile.vtk, time)
 end
 
 cell_to_vtkcell(::Type{Line}) = VTKCellTypes.VTK_LINE

commit b6976d3e22f7cc14e8e97f53b6de1d094a3c9889

Fix scope error in ns_vs_diffeq tutorial (#1045)
Fix error introduced in #1015

docs/src/literate-tutorials/ns_vs_diffeq.jl
@@ -577,9 +577,7 @@ integrator = init(
 #     Exporting interpolated solutions of problems containing mass matrices is currently broken.
 #     Thus, the `intervals` iterator is used. Note that `solve` holds all solutions in the memory.
 pvd = paraview_collection("vortex-street")
-step = 0
-for (u,t) in intervals(integrator)
-    step += 1
+for (step, (u,t)) in enumerate(intervals(integrator))
     VTKGridFile("vortex-street-$step", dh) do vtk
         write_solution(vtk, dh, u)
         pvd[t] = vtk