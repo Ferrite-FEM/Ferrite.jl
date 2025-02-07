1d08cf1bedc4f07671316a41806660fcefa4e461
Jared Frazier <72281389+jfdev001@users.noreply.github.com>
2025-02-07 15:24:06 +0100

    Update src/interpolations.jl
    Co-authored-by: Knut Andreas Meyer <knutam@gmail.com>

--- src/interpolations.jl
+++ src/interpolations.jl
@@ -1323,7 +1323,7 @@ end
 #######################################
 # Lagrange-Bubble RefTriangle order 1 #
 #######################################
-# Taken from https://web.archive.org/web/20240519081038/https://defelement.com/elements/bubble-enriched-lagrange.html 
+# Taken from https://web.archive.org/web/20240519081038/https://defelement.com/elements/bubble-enriched-lagrange.html
 getnbasefunctions(::BubbleEnrichedLagrange{RefTriangle, 1}) = 4
 adjust_dofs_during_distribution(::BubbleEnrichedLagrange{RefTriangle, 1}) = false
 
