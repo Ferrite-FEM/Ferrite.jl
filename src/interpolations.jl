f1d1d0deef7bdaf019bd63ce9e8d959b6ebc8c4d
Knut Andreas Meyer <knutam@gmail.com>
2025-05-01 12:12:49 +0200

    Update CHANGELOG and set version to 1.1 (#1191)
    * Update CHANGELOG and bump to 1.1
    
    * Update manifest to reflect new Ferrite version
    
    * List implemented interpolations
    
    ---------
    
    Co-authored-by: Fredrik Ekre <ekrefredrik@gmail.com>

--- src/interpolations.jl
+++ src/interpolations.jl
@@ -9,31 +9,45 @@ a function between nodes.
 
 The following interpolations are implemented:
 
-* `Lagrange{RefLine,1}`
-* `Lagrange{RefLine,2}`
-* `Lagrange{RefQuadrilateral,1}`
-* `Lagrange{RefQuadrilateral,2}`
-* `Lagrange{RefQuadrilateral,3}`
-* `Lagrange{RefTriangle,1}`
-* `Lagrange{RefTriangle,2}`
-* `Lagrange{RefTriangle,3}`
-* `Lagrange{RefTriangle,4}`
-* `Lagrange{RefTriangle,5}`
-* `BubbleEnrichedLagrange{RefTriangle,1}`
+* `Lagrange{RefLine, 1}`
+* `Lagrange{RefLine, 2}`
+* `Lagrange{RefQuadrilateral, 1}`
+* `Lagrange{RefQuadrilateral, 2}`
+* `Lagrange{RefQuadrilateral, 3}`
+* `Lagrange{RefTriangle, 1}`
+* `Lagrange{RefTriangle, 2}`
+* `Lagrange{RefTriangle, 3}`
+* `Lagrange{RefTriangle, 4}`
+* `Lagrange{RefTriangle, 5}`
+* `BubbleEnrichedLagrange{RefTriangle, 1}`
 * `CrouzeixRaviart{RefTriangle, 1}`
 * `CrouzeixRaviart{RefTetrahedron, 1}`
 * `RannacherTurek{RefQuadrilateral, 1}`
 * `RannacherTurek{RefHexahedron, 1}`
-* `Lagrange{RefHexahedron,1}`
-* `Lagrange{RefHexahedron,2}`
-* `Lagrange{RefTetrahedron,1}`
-* `Lagrange{RefTetrahedron,2}`
-* `Lagrange{RefPrism,1}`
-* `Lagrange{RefPrism,2}`
-* `Lagrange{RefPyramid,1}`
-* `Lagrange{RefPyramid,2}`
-* `Serendipity{RefQuadrilateral,2}`
-* `Serendipity{RefHexahedron,2}`
+* `Lagrange{RefHexahedron, 1}`
+* `Lagrange{RefHexahedron, 2}`
+* `Lagrange{RefTetrahedron, 1}`
+* `Lagrange{RefTetrahedron, 2}`
+* `Lagrange{RefPrism, 1}`
+* `Lagrange{RefPrism, 2}`
+* `Lagrange{RefPyramid, 1}`
+* `Lagrange{RefPyramid, 2}`
+* `Serendipity{RefQuadrilateral, 2}`
+* `Serendipity{RefHexahedron, 2}`
+* `Nedelec{RefTriangle, 1}`
+* `Nedelec{RefTriangle, 2}`
+* `Nedelec{RefQuadrilateral, 1}`
+* `Nedelec{RefTetrahedron, 1}`
+* `Nedelec{RefHexahedron, 1}`
+* `RaviartThomas{RefTriangle, 1}`
+* `RaviartThomas{RefTriangle, 2}`
+* `RaviartThomas{RefQuadrilateral, 1}`
+* `RaviartThomas{RefTetrahedron, 1}`
+* `RaviartThomas{RefHexahedron, 1}`
+* `BrezziDouglasMarini{RefTriangle, 1}`
+
+Additionally, `DiscontinuousLagrange` is implemented for the same reference shapes
+and orders as `Lagrange`, as well as for `order = 0` on any reference shape.
 
 # Examples
 ```jldoctest
