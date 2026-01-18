e3608d9ad8619879ee138d48e527726b7c2d9cc9
Elias <elias.isak.borjesson@gmail.com>
2026-01-19 00:00:21 +0100

    Implement argyris element

--- test/test_cellvalues.jl
+++ test/test_cellvalues.jl
@@ -540,4 +540,45 @@
         end
     end
 
+    @testset "FEValues+Argyris" begin
+
+        grid = generate_grid(Triangle, (3,3))
+        #Jiggle mesh amd rotate some elements
+        transform_coordinates!(grid, x-> x + rand(Vec{2})*0.05) 
+        shifts = ([1,2,3], [3,1,2], [2,3,1])
+        for i in 1:getncells(grid)
+            cell = grid.cells[i]
+            nodes = cell.nodes[shifts[rand(1:length(shifts))]]
+            grid.cells[i] = Triangle((nodes))
+        end
+
+        for vdim in 1:2
+            ip_geo = Ferrite.geometric_interpolation(getcelltype(grid))
+            ip = vdim==1 ? Argyris{RefTriangle, 5}() : Argyris{RefTriangle, 5}()^vdim
+            dh = DofHandler(grid)
+            add!(dh, :u, ip)
+            close!(dh)
+
+            #Loop over all interfaces and chack that jump in gradient and value is zero (C1 continuity)
+            fqr = FacetQuadratureRule{RefTriangle}(3)
+            fv1 = FacetValues(fqr, ip, ip_geo^2; update_hessians=true)
+            fv2 = FacetValues(deepcopy(fqr), ip, ip_geo^2; update_hessians=true)
+            iv = InterfaceValues(fv1, fv2)
+            a = rand(Float64, Ferrite.ndofs(dh)) #Generate random solution vector
+            value_type = typeof(shape_value(iv, 1, 1; here=true))
+            gradient_type = typeof(shape_gradient(iv, 1, 1; here=true))
+            for interface_data in InterfaceIterator(dh)
+                dofs = interfacedofs(interface_data)
+                ue = a[dofs]
+                reinit!(iv, interface_data)
+                for iqp in 1:getnquadpoints(iv)
+                    @test isapprox(function_value_jump(iv, iqp, ue), zero(value_type), atol=1e-15)
+                    @test isapprox(function_gradient_jump(iv, iqp, ue), zero(gradient_type), atol=1e-13)
+                end
+            end
+        end
+
+    end
+
 end # of testset
+
