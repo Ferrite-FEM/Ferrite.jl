003aa805bbbfcfa2c6f765edc3157dc66e7f6375
Knut Andreas <knutam@gmail.com>
2026-01-28 13:34:17 +0100

    Add symbol access test to benchmark

--- benchmark/benchmark-cellvalues.jl
+++ benchmark/benchmark-cellvalues.jl
@@ -1,5 +1,30 @@
 using Ferrite, BenchmarkTools
 
+function element_routine(cv::CellValues)
+    s = 0.0
+    for q_point in 1:getnquadpoints(cv)
+        for i in 1:getnbasefunctions(cv)
+            for j in 1:getnbasefunctions(cv)
+                s += norm(shape_value(cv, q_point, i)) * norm(shape_gradient(cv, q_point, j))
+            end
+        end
+    end
+    return s
+end
+
+function element_routine(cv::CellMultiValues)
+    s = 0.0
+    for q_point in 1:getnquadpoints(cv)
+        for i in 1:getnbasefunctions(cv[:s1])
+            for j in 1:getnbasefunctions(cv[:s1])
+                s += norm(shape_value(cv[:s1], q_point, i)) * norm(shape_gradient(cv[:s1], q_point, j))
+            end
+        end
+    end
+    return s
+end
+
+
 CT = QuadraticHexahedron
 ip_geo = geometric_interpolation(CT)
 x = Ferrite.reference_coordinates(ip_geo)
@@ -30,4 +55,11 @@ for key in (
     cv = getproperty(Main, key)
     print(key, ": ")
     @btime reinit!($cv, $x)
+end
+
+# Check that the indexing inside the element routine doesn't affect the performance
+for key in (:cv_s1, :cmv_s1_v1_s2_v2)
+    cv = getproperty(Main, key)
+    reinit!(cv, x)
+    @btime element_routine($cv)
 end
