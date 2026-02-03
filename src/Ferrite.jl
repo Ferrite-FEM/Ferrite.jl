fb4312d360a87e60b76289e623c5253afb6e94d4
Elias <elias.isak.borjesson@gmail.com>
2026-02-03 19:53:32 +0100

    Fix implicit import

--- src/Ferrite.jl
+++ src/Ferrite.jl
@@ -22,7 +22,7 @@ using WriteVTK:
     WriteVTK, VTKCellTypes
 using Tensors:
     Tensors, AbstractTensor, SecondOrderTensor, SymmetricTensor, Tensor, Vec, gradient,
-    rotation_tensor, symmetric, tovoigt!, hessian, otimesu, otimesl
+    rotation_tensor, symmetric, tovoigt!, hessian, otimesu, otimesl, rotate
 using ForwardDiff:
     ForwardDiff
 
