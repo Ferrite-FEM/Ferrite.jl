42012ddf5338ee34e05c3e4127d0e8c284c039c4
yolhan83 <yolhan@laposte.net>
2026-02-09 19:36:36 +0100

    simplify state signature and inline in InterfaceIterator

--- src/iterators.jl
+++ src/iterators.jl
@@ -363,23 +363,21 @@ function InterfaceIterator(
 end
 
 # Iterator interface
-function Base.iterate(ii::InterfaceIterator{<:Any, <:Grid{sdim}}, state...) where {sdim}
-    neighborhood = get_facet_facet_neighborhood(ii.topology, ii.grid) # TODO: This could be moved to InterfaceIterator constructor (potentially type-instable for non-union or mixed grids)
-    while true
-        it = iterate(facetskeleton(ii.topology, ii.grid), state...)
-        it === nothing && return nothing
-        facet_a, state = it
-        if isempty(neighborhood[facet_a[1], facet_a[2]])
-            continue
-        end
-        neighbors = neighborhood[facet_a[1], facet_a[2]]
-        length(neighbors) > 1 && error("multiple neighboring faces not supported yet")
-        facet_b = neighbors[1]
-        reinit!(ii.cache, facet_a, facet_b)
-        return (ii.cache, state)
+@inline function Base.iterate(ii::InterfaceIterator{<:Any,<:Grid{sdim}}, i::Int) where {sdim}
+    neighbors = Ferrite.get_facet_facet_neighborhood(ii.topology, ii.grid)# TODO: This could be moved to InterfaceIterator constructor (potentially type-instable for non-union or mixed grids)
+    skel::Vector{Ferrite.FacetIndex} = facetskeleton(ii.topology, ii.grid)
+    while i <= length(skel)
+        fa = skel[i]; i += 1
+        nbhs = neighbors[fa[1], fa[2]]
+        isempty(nbhs) && continue
+        length(nbhs) > 1 && error("multiple neighboring faces not supported yet")
+        Ferrite.reinit!(ii.cache, fa, nbhs[1])
+        return (ii.cache, i)
     end
-    return
+    return nothing
 end
+@inline Base.iterate(ii::InterfaceIterator{<:Any,<:Grid{sdim}}) where {sdim} = Base.iterate(ii, 1)
+
 
 
 # Iterator interface for CellIterator/FacetIterator
