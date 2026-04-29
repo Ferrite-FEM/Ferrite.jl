2e629ae96026452b8e3fe187fe941d2ab3757f2f
Henrik Jakob <jakob@ibb.uni-stuttgart.de>
2026-04-29 09:51:39 +0000

    feedback from comment

--- src/FEValues/common_values.jl
+++ src/FEValues/common_values.jl
@@ -161,6 +161,8 @@ end
 curl_from_gradient(∇v::SecondOrderTensor{3}) = Vec{3}((∇v[3, 2] - ∇v[2, 3], ∇v[1, 3] - ∇v[3, 1], ∇v[2, 1] - ∇v[1, 2]))
 curl_from_gradient(∇v::SecondOrderTensor{2}) = Vec{1}((∇v[2, 1] - ∇v[1, 2],)) # Alternatively define as Vec{3}((0, 0, v))
 
+@propagate_inbounds shape_symmetric_gradient(cv::AbstractValues, q_point::Int, base_func::Int) = symmetric(shape_gradient(cv, q_point, base_func))
+
 """
     function_value(fe_v::AbstractValues, q_point::Int, u::AbstractVector, [dof_range])
 
--- test/test_pointevaluation.jl
+++ test/test_pointevaluation.jl
@@ -515,6 +515,8 @@ end
     @test function_symmetric_gradient(pvv, uv) ≈ function_symmetric_gradient(cvv, 1, uv)
 
     @test shape_gradient(pvv, 1, 1) ≈ shape_gradient(cvv, 1, 1)
+    @test shape_divergence(pvv, 1, 1) ≈ shape_divergence(cvv, 1, 1)
+    @test shape_curl(pvv, 1, 1) ≈ shape_curl(cvv, 1, 1)
     @test shape_symmetric_gradient(pvv, 1, 1) ≈ shape_symmetric_gradient(cvv, 1, 1)
 
     reinit!(pvv, x, ξ₂)
@@ -523,5 +525,7 @@ end
     @test function_symmetric_gradient(pvv, uv) ≈ function_symmetric_gradient(cvv, 2, uv)
 
     @test shape_gradient(pvv, 1, 1) ≈ shape_gradient(cvv, 2, 1)
+    @test shape_divergence(pvv, 1, 1) ≈ shape_divergence(cvv, 2, 1)
+    @test shape_curl(pvv, 1, 1) ≈ shape_curl(cvv, 2, 1)
     @test shape_symmetric_gradient(pvv, 1, 1) ≈ shape_symmetric_gradient(cvv, 2, 1)
 end
