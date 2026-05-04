b88ca974407e1035d5f99c27af511ad89fce1982
Phil Kreutzheide <kreutzheidephil@gmail.com>
2026-05-04 12:46:00 +0200

    Runic and spell check

--- src/Dofs/untangling_affine_constraints.jl
+++ src/Dofs/untangling_affine_constraints.jl
@@ -1,6 +1,6 @@
-function _assign_new_index!(d::Dict{Int,Int}, k::Int)
-    if !haskey(d, k)
-        d[k] = maximum(values(d); init=0) + 1
+function _assign_new_index!(d::Dict{Int, Int}, k::Int)
+    return if !haskey(d, k)
+        d[k] = maximum(values(d); init = 0) + 1
     end
 end
 
@@ -15,7 +15,7 @@ of freedom via `A * a_c = C * a_f + g`. Three mappings are also returned.
 
     * `affine_equation_ordering` which maps `constraint (eq.) => row of A`
 
-    * `dofcoeffs_to_remove` which maps `constraint (eq.) => postion of dof coefficient to remove`
+    * `dofcoeffs_to_remove` which maps `constraint (eq.) => position of dof coefficient to remove`
 
 !!! note
     In this case, the system `A * a_c = C * a_f + g` only contains the affine 
@@ -23,17 +23,17 @@ of freedom via `A * a_c = C * a_f + g`. Three mappings are also returned.
     `ConstraintHandler` has been closed.
 
 """
-function _create_lhs_affine_constraint_matrix(ch::ConstraintHandler{DH,T}) where {DH,T}
-    
+function _create_lhs_affine_constraint_matrix(ch::ConstraintHandler{DH, T}) where {DH, T}
+
     # maps the constrained dofs to a position in `a_c`
-    affine_cdof_ordering = Dict{Int,Int}()
+    affine_cdof_ordering = Dict{Int, Int}()
     # maps the constraint equation to a row in `A * a_c = C * a_f + g`
-    affine_equation_ordering = Dict{Int,Int}()
+    affine_equation_ordering = Dict{Int, Int}()
     # collect the position of the dof coefficients that need to be removed for ch.dofcoefficients
-    dofcoeffs_to_remove = Dict{Int,Vector{Int}}()
+    dofcoeffs_to_remove = Dict{Int, Vector{Int}}()
 
     I = Int[]; J = Int[]; V = T[]
-    dofmapping⁻¹ = Dict{Int,Int}(v => k for (k, v) in ch.dofmapping)
+    dofmapping⁻¹ = Dict{Int, Int}(v => k for (k, v) in ch.dofmapping)
 
     for (eq, coeffs) in enumerate(ch.dofcoefficients)
         coeffs === nothing && continue # this constraint corresponds to a Dirichlet constraint
@@ -42,10 +42,10 @@ function _create_lhs_affine_constraint_matrix(ch::ConstraintHandler{DH,T}) where
             nested_eq = get(ch.dofmapping, d, 0)
             dof_position_counter += 1
             nested_eq == 0 && continue # skip as d is not in the prescribed dofs and therefore not nested
-            
+
             nested_coeffs = ch.dofcoefficients[nested_eq]
             if !(nested_coeffs === nothing || isempty(nested_coeffs)) # nothing means Dirichlet, empty means Dirichlet but through AffineConstraint
-                
+
                 # add the dof to the affine_cdof_ordering
                 _assign_new_index!(affine_cdof_ordering, d)
                 # add the equation to affine_equation_ordering
@@ -55,9 +55,9 @@ function _create_lhs_affine_constraint_matrix(ch::ConstraintHandler{DH,T}) where
                 _assign_new_index!(affine_cdof_ordering, dofmapping⁻¹[eq])
                 # add the equation pertaining to the master dof
                 _assign_new_index!(affine_equation_ordering, eq)
-                
+
                 i = affine_equation_ordering[eq]
-                j = affine_cdof_ordering[d]                
+                j = affine_cdof_ordering[d]
                 push!(I, i)
                 push!(J, j)
                 push!(V, -c)
@@ -82,11 +82,11 @@ function _create_lhs_affine_constraint_matrix(ch::ConstraintHandler{DH,T}) where
             push!(V, 1)
         end
     end
-    
+
     m = length(affine_equation_ordering)
     n = length(affine_cdof_ordering)
     @assert(m == n)
-    
+
     A = SparseArrays.sparse(I, J, V, m, n)
 
     return A, affine_equation_ordering, affine_cdof_ordering, dofcoeffs_to_remove
@@ -105,14 +105,14 @@ The rows are indexed using `affine_equation_ordering` from `_create_lhs_affine_c
     constraints that are nested. Therefore this function is not designed to be used after the
     `ConstraintHandler` has been closed.
 """
-function _create_rhs_affine_constraint_matrices(ch::ConstraintHandler{DH,T}, affine_equation_ordering::Dict{Int,Int}) where {DH,T}
+function _create_rhs_affine_constraint_matrices(ch::ConstraintHandler{DH, T}, affine_equation_ordering::Dict{Int, Int}) where {DH, T}
 
     n_nested_constraints = length(affine_equation_ordering)
     I = Int[]; J = Int[]; V = T[]
     g = Vector{T}(undef, n_nested_constraints) # inhomogeneities
 
     # maps the free dofs to a position in `a_f`
-    affine_fdof_ordering = Dict{Int,Int}()
+    affine_fdof_ordering = Dict{Int, Int}()
 
     for (eq, coeffs) in enumerate(ch.dofcoefficients)
         (isnothing(coeffs) || !haskey(affine_equation_ordering, eq)) && continue
@@ -139,16 +139,16 @@ function _create_rhs_affine_constraint_matrices(ch::ConstraintHandler{DH,T}, aff
     return C, g, affine_fdof_ordering
 end
 
-function _update_dof_coefficents!(dc::Vector{Union{Nothing,DofCoefficients{T}}}, C::AbstractMatrix, affine_equation_ordering::Dict{Int,Int}, affine_fdof_ordering::Dict{Int,Int}) where {T}
-    
+function _update_dof_coefficients!(dc::Vector{Union{Nothing, DofCoefficients{T}}}, C::AbstractMatrix, affine_equation_ordering::Dict{Int, Int}, affine_fdof_ordering::Dict{Int, Int}) where {T}
+
     affine_fdof_mapping⁻¹ = Dict(v => k for (k, v) in affine_fdof_ordering) # Bijections.jl could avoid this but not really worth it
 
     for (eq, i) in pairs(affine_equation_ordering)
-        coeffs = Pair{Int,T}[]
+        coeffs = Pair{Int, T}[]
         for j in axes(C, 2)
-            C[i,j] == zero(T) && continue # skip zero entry
+            C[i, j] == zero(T) && continue # skip zero entry
             d = affine_fdof_mapping⁻¹[j]
-            push!(coeffs, (d => C[i,j]))
+            push!(coeffs, (d => C[i, j]))
         end
         # overwrite the old coefficients
         dc[eq] = coeffs
