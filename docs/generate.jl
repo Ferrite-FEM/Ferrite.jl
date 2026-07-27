59c07a0a8c82a3c305b1b48e86090194ed5a9258
termi-official <termi-official@users.noreply.github.com>
2026-07-27 20:59:43 +0200

    Add comment on generator why some files are not executed in the docs build

--- docs/generate.jl
+++ docs/generate.jl
@@ -1,7 +1,9 @@
 # generate examples
 import Literate
 
-# Skip execution of the following files
+# Skip execution of the following files.
+# These are tested directly in the CI, as they need additional dependencies
+# and/or special machines (e.g. GPUs) to run on.
 DONT_EXECUTE = Set(["gpu_heat_howto_literate.jl"])
 
 # Tutorials
