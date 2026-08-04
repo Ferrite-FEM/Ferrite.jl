9839744b75eecdb7b47de201b9290c7cbff46125
termi-official <termi-official@users.noreply.github.com>
2026-08-04 18:07:07 +0200

    Fix CSR ext

--- ext/FerriteSparseMatrixCSR.jl
+++ ext/FerriteSparseMatrixCSR.jl
@@ -34,7 +34,7 @@ end
                 # Match: add the value (if non-zero) and advance the pointers
                 val = Ke[Kerow, colpermutation[ci]]
                 if !iszero(val)
-                    Ferrite._addindex!(K.nzval, C, val, atomic)
+                    Ferrite.addindex!(K.nzval, C, val, atomic)
                 end
                 ci += 1
                 Ci += 1
