b9a869d2a3b03ab32020431c609525ae01700d55
Phil Kreutzheide <kreutzheidephil@gmail.com>
2026-08-10 09:59:32 +0200

    runic will be the death of me lol

--- src/Dofs/untangling_affine_constraints.jl
+++ src/Dofs/untangling_affine_constraints.jl
@@ -21,11 +21,13 @@ function untangle_constraints!(ch::ConstraintHandler)
         LinearAlgebra.lu(A; check = true)
     catch e
         if e isa LinearAlgebra.SingularException
-            throw(ArgumentError(
-                "the affine constraints are tangled and untangling them results in " *
-                    "ill defined constraints. A possibility to avoid this is to guarantee that " *
-                    "the constraints are not tangled before calling close!"
-            ))
+            throw(
+                ArgumentError(
+                    "the affine constraints are tangled and untangling them results in " *
+                        "ill defined constraints. A possibility to avoid this is to guarantee that " *
+                        "the constraints are not tangled before calling close!"
+                )
+            )
         else
             rethrow(e)
         end
