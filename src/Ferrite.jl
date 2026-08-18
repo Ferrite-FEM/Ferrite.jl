28c575aa5cf9b0a2844ddf11d176089992c164c3
Fredrik Ekre <ekrefredrik@gmail.com>
2026-08-18 13:16:17 +0200

    Add reference shape figures and improve reference cell documentation (#689)
    - Add figures from DefElement (CC BY 4.0), adjusted to Ferrite.jl
      numbering and Julia logo colors, for all reference cells to the
      devdocs page, together with vertex coordinates and edge/face
      identifiers for each reference cell.
    - Move the reference shape definitions from Ferrite.jl, and their
      extended docstrings from docs.jl, into the new file refshapes.jl.
    
    All entity numbering (markdown and figure labels) has been verified
    programmatically against the canonical definitions on master
    (reference_coordinates, reference_edges, reference_faces).

--- src/Ferrite.jl
+++ src/Ferrite.jl
@@ -30,33 +30,12 @@ using .CollectionsOfViews:
     insert_sorted_at_index!, ConstructionBuffer
 
 include("exports.jl")
-
-
-"""
-    AbstractRefShape{refdim}
-
-Supertype for all reference shapes, with reference dimension `refdim`. Reference shapes are
-used to define grid cells, shape functions, and quadrature rules. Currently existing
-reference shapes are: [`RefLine`](@ref), [`RefTriangle`](@ref), [`RefQuadrilateral`](@ref),
-[`RefTetrahedron`](@ref), [`RefHexahedron`](@ref), [`RefPrism`](@ref), [`RefPyramid`](@ref).
-"""
-abstract type AbstractRefShape{refdim} end
-
-# See src/docs.jl for detailed documentation
-struct RefHypercube{refdim} <: AbstractRefShape{refdim} end
-struct RefSimplex{refdim} <: AbstractRefShape{refdim} end
-const RefLine = RefHypercube{1}
-const RefQuadrilateral = RefHypercube{2}
-const RefHexahedron = RefHypercube{3}
-const RefTriangle = RefSimplex{2}
-const RefTetrahedron = RefSimplex{3}
-struct RefPrism <: AbstractRefShape{3} end
-struct RefPyramid <: AbstractRefShape{3} end
+include("refshapes.jl")
 
 """
     Ferrite.getrefdim(RefShape::Type{<:AbstractRefShape})
 
-Get the dimension of the reference shape
+Get the dimension of the reference shape.
 """
 getrefdim(::Type{<:AbstractRefShape}) # To get correct doc filtering
 getrefdim(::Type{<:AbstractRefShape{rdim}}) where {rdim} = rdim
@@ -175,7 +154,6 @@ include("PointEvalHandler.jl")
 # Other
 include("soa_utils.jl")
 include("deprecations.jl")
-include("docs.jl")
 
 # Adaptivity
 include("Adaptivity/AMR.jl")
