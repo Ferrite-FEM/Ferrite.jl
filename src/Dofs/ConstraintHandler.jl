c7694d375fa54b891121108d7e475bb12d24aa7e
Phil Kreutzheide <kreutzheidephil@gmail.com>
2026-09-14 16:18:13 +0200

    @warn -> @info

--- src/Dofs/ConstraintHandler.jl
+++ src/Dofs/ConstraintHandler.jl
@@ -337,7 +337,7 @@ function close!(ch::ConstraintHandler)
     #   `add_prescribed_dof`.)
 
     if istangled(ch) # untangle affine constraints
-        @debug @warn "untangling tangled and cyclic affine constraints"
+        @debug @info "untangling tangled and cyclic affine constraints"
         _untangle_affine_constraints!(ch)
     end
 
