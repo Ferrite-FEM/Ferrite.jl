c5f40db6340f14928d4ad691ef1963a5661b9363
Phil Kreutzheide <kreutzheidephil@gmail.com>
2026-09-14 16:14:57 +0200

    docs + naming

--- src/Dofs/untangling_affine_constraints.jl
+++ src/Dofs/untangling_affine_constraints.jl
@@ -1,21 +1,47 @@
 """
-    untangle_constraints!(ch::ConstraintHandler)
+    _untangle_affine_constraints!(ch::ConstraintHandler)
 
-Untangle the constraints in `ch`. This function will error if the constraints are not tangled.
+Untangle the affine constraints in `ch`. This is best illustrated using an example. The following system has
+tangled constraints as `u2` appears as a master and a slave dof. 
+
+    u1 = u2 + u5
+    u2 = u3 + 4*u10 + 4.0
+    u9 = 3*u2 - 2.0
+
+To untangle this the following linear system is assembled, here `a_c` and `a_f` are the vectors of the individual dofs `u_i`.
+
+    A * a_c = C * a_f + g.
+
+Concretely, for the above example we get
+
+    | 1  -1  ⋅ | |u1|   |1  ⋅||u5|   | 1.0|
+    | ⋅   1  ⋅ | |u2| = |⋅  1||u3| + | 4.0|
+    | ⋅  -3  1 | |u9|   |⋅  ⋅|       |-2.0|.
+    
+Solving this system we find the new master/slave dofs and their coefficients
+
+    |u1|   |1  1||u5|   | 5.0|
+    |u2| = |0  1||u3| + | 4.0|
+    |u9|   |0  3|       |10.0|
+
+which are then used to update the `ConstraintHandler` accordingly. A couple of things to note here:
+
+    * If a Dirichlet-type dof appears in a tangled fashion, i.e. `u3 = f3(t)` in the system above, this is not included in `A` or `C`
+    as Ferrite allows tangled dofs if they of Dirichlet-type.
+
+    * The case that a Dirichlet dof is also a master dof i.e., `u9 = f9(t)` in the above system, does not occur as Ferrite overwrites master
+    dofs in `add!` to ensure no two constraints share a master dof.
+
+!!! warning
+    As the system `A * a_c = C * a_f + g` only contains the affine
+    constraints that are tangled. Therefore, this function is not designed to be called when the
+    constraints are not tangled.
+
 """
-function untangle_constraints!(ch::ConstraintHandler)
-    # Incase user calls this? Maybe not relevant if not exported?
-    @assert istangled(ch) "the constraints are not tangled"
-    A, affine_equation_ordering, _, dofcoeffs_to_remove = _create_lhs_affine_constraint_matrix(ch)
+function _untangle_affine_constraints!(ch::ConstraintHandler)
 
-    new_dofcoefficients = deepcopy(ch.dofcoefficients)
-    # update new_dofcoefficients so that they can be used to construct `C`
-    for (k, v) in dofcoeffs_to_remove
-        deleteat!(new_dofcoefficients[k], v)
-    end
+    A, affine_equation_ordering, new_dofcoefficients = _create_lhs_affine_constraint_matrix(ch)
     C, g, affine_fdof_ordering = _create_rhs_affine_constraint_matrices(ch, new_dofcoefficients, affine_equation_ordering)
-
-    # TODO: maybe add possibility to warn user (if user chooses through kwarg) if `A` is ill conditioned
 
     luA = try
         LinearAlgebra.lu(A; check = true)
@@ -23,8 +49,8 @@ function untangle_constraints!(ch::ConstraintHandler)
         if e isa LinearAlgebra.SingularException
             throw(
                 ArgumentError(
-                    "the affine constraints are tangled and untangling them results in " *
-                        "ill defined constraints. A possibility to avoid this is to guarantee that " *
+                    "the affine constraints are tangled and untangling them fails. " *
+                        "This can be due to e.g. redudant constraints. A possibility to avoid this is to guarantee that " *
                         "the constraints are not tangled before calling close!"
                 )
             )
@@ -33,12 +59,10 @@ function untangle_constraints!(ch::ConstraintHandler)
         end
     end
 
-    luAC = _sparse_column_wise_solve(luA, C)
-    _update_dof_coefficients!(new_dofcoefficients, luAC, affine_equation_ordering, affine_fdof_ordering)
+    A⁻¹C = _sparse_column_wise_solve(luA, C)
+    _update_dof_coefficients!(new_dofcoefficients, A⁻¹C, affine_equation_ordering, affine_fdof_ordering)
 
-    # TODO: making affine constraint inhomogeneities time dependent requires (?) saving
-    # `A⁻¹` or `luA` as it will be needed in update!
-    copy!(g, LinearAlgebra.ldiv(luA, g))
+    ldiv!(luA, g)
 
     # we need to update ch.affine_inhomogeneities NOT ch.inhomogeneities
     # as ch.inhomogeneities will be computed in update!
@@ -56,20 +80,10 @@ end
 """
     _create_lhs_affine_constraint_matrix(ch::ConstraintHandler{DH, T}) where {DH, T}
 
-Create and returns the constraint matrix, `A`, that described the affine
-tangled constraints in `ch`. The matrix `A` relates constrained affine dofs, `a_c`, and free, `a_f`, degrees
-of freedom via `A * a_c = C * a_f + g`. Three mappings are also returned.
-
-    * `affine_cdof_ordering` which maps `constrained dof => column of A`
-
-    * `affine_equation_ordering` which maps `constraint (eq.) => row of A`
-
-    * `dofcoeffs_to_remove` which maps `constraint (eq.) => position of dof coefficient to remove`
-
-!!! note
-    In this case, the system `A * a_c = C * a_f + g` only contains the affine
-    constraints that are tangled. Therefore this function is not designed to be used after the
-    `ConstraintHandler` has been closed.
+Create and returns the left-hand side constraint matrix `A` from the system `A * a_c = C * a_f + g`. As `A` only contains the 
+tangled affine constraints its structure is built from the ground up. This means there is a mapping required to associate
+each row in `A` to its original position. To do this `affine_equation_ordering` returned. Finally, `new_dofcoefficients` are returned
+which have the entries that are now in `A` removed.
 
 """
 function _create_lhs_affine_constraint_matrix(ch::ConstraintHandler{DH, Tv, Ti}) where {DH, Tv, Ti}
@@ -81,20 +95,21 @@ function _create_lhs_affine_constraint_matrix(ch::ConstraintHandler{DH, Tv, Ti})
     # collect the position of the dof coefficients that need to be removed for ch.dofcoefficients
     dofcoeffs_to_remove = Dict{Int, Vector{Int}}()
 
-    I = Int[]; J = Int[]; V = Tv[]
+    I = Ti[]; J = Ti[]; V = Tv[]
     dofmapping⁻¹ = Dict{Int, Int}(v => k for (k, v) in ch.dofmapping)
 
     for (eq, coeffs) in enumerate(ch.dofcoefficients)
         coeffs === nothing && continue # this constraint corresponds to a Dirichlet constraint
         dof_position_counter = 0
         for (d, c) in coeffs
+            
             tangled_eq = get(ch.dofmapping, d, 0)
             dof_position_counter += 1
             tangled_eq == 0 && continue # skip as d is not in the prescribed dofs and therefore not tangled
 
             tangled_coeffs = ch.dofcoefficients[tangled_eq]
             if !(tangled_coeffs === nothing || isempty(tangled_coeffs)) # nothing means Dirichlet, empty means Dirichlet but through AffineConstraint
-
+                
                 # add the dof to the affine_cdof_ordering
                 _assign_new_index!(affine_cdof_ordering, d)
                 # add the equation to affine_equation_ordering
@@ -134,30 +149,30 @@ function _create_lhs_affine_constraint_matrix(ch::ConstraintHandler{DH, Tv, Ti})
 
     m = length(affine_equation_ordering)
     n = length(affine_cdof_ordering)
-    @assert(m == n)
+    @assert m == n "The matrix A has dimensions m = $m != n = $n"
 
     A = SparseArrays.sparse(I, J, V, m, n)
 
-    return A, affine_equation_ordering, affine_cdof_ordering, dofcoeffs_to_remove
+    # finally remove the entries that have been moved into A so that new_dofcoefficients can be used to construct `C`
+    new_dofcoefficients = deepcopy(ch.dofcoefficients)
+    for (k, v) in dofcoeffs_to_remove
+        deleteat!(new_dofcoefficients[k], v)
+    end
+
+    return A, affine_equation_ordering, new_dofcoefficients
 end
 
 """
     _create_rhs_affine_constraint_matrices(ch::ConstraintHandler{DH, Tv, Ti}, new_dofcoefficients, affine_equation_ordering::Dict{Int, Int}) where {DH, Tv, Ti}
 
-Create and return the constraint matrix, `C`, and the inhomogeneities, `g`, from the tangled affine
-constraints in `ch`. The constraint matrix relates constrained affine dofs, `a_c`, and free, `a_f`, degrees of freedom via
-`A * a_c = C * a_f + g`. A mapping `affine_fdof_ordering` is also returned to index `C`, i.e. it maps "free" dof => column of C.
-The rows are indexed using `affine_equation_ordering` from `_create_lhs_affine_constraint_matrix`.
+Create and returns the right-hand side constraint matrix `C` and its inhomogenties `g` from the system `A * a_c = C * a_f + g`. Returned are `C`,
+`g` and `affine_fdof_ordering` which maps the "free" dofs to the columns of `C`.
 
-!!! note
-    In this case, the system `A * a_c = C * a_f + g` only contains the affine
-    constraints that are tangled. Therefore this function is not designed to be used after the
-    `ConstraintHandler` has been closed.
 """
 function _create_rhs_affine_constraint_matrices(ch::ConstraintHandler{DH, Tv, Ti}, new_dofcoefficients::Vector{Union{Nothing, DofCoefficients{Tv, Ti}}}, affine_equation_ordering::Dict{Int, Int}) where {DH, Tv, Ti}
 
     n_tangled_constraints = length(affine_equation_ordering)
-    I = Int[]; J = Int[]; V = Tv[]
+    I = Ti[]; J = Ti[]; V = Tv[]
     g = Vector{Tv}(undef, n_tangled_constraints) # inhomogeneities
 
     # maps the free dofs to a position in `a_f`
@@ -189,25 +204,25 @@ function _create_rhs_affine_constraint_matrices(ch::ConstraintHandler{DH, Tv, Ti
 end
 
 """
-    _update_dof_coefficients!(dc::Vector{Union{Nothing, DofCoefficients{Tv, Ti}}}, C::AbstractMatrix, affine_equation_ordering::Dict{Int, Int}, affine_fdof_ordering::Dict{Int, Int}) where {Tv, Ti}
+    _update_dof_coefficients!(dc::Vector{Union{Nothing, DofCoefficients{Tv, Ti}}}, A⁻¹C::AbstractMatrix, affine_equation_ordering::Dict{Int, Int}, affine_fdof_ordering::Dict{Int, Int}) where {Tv, Ti}
 
-Update the dof coefficients `dc` using the constraint matrix `C` and the mappings `affine_equation_ordering` and `affine_fdof_ordering`.
+Update the dof coefficients `dc` using the constraint matrix `A⁻¹C` and the mappings `affine_equation_ordering` and `affine_fdof_ordering`.
 """
-function _update_dof_coefficients!(dc::Vector{Union{Nothing, DofCoefficients{Tv, Ti}}}, C::AbstractMatrix, affine_equation_ordering::Dict{Int, Int}, affine_fdof_ordering::Dict{Int, Int}) where {Tv, Ti}
-    affine_fdof_mapping⁻¹ = Dict(v => k for (k, v) in affine_fdof_ordering) # Bijections.jl could avoid this but not really worth it
+function _update_dof_coefficients!(dc::Vector{Union{Nothing, DofCoefficients{Tv, Ti}}}, A⁻¹C::SparseMatrixCSC, affine_equation_ordering::Dict{Int, Int}, affine_fdof_ordering::Dict{Int, Int}) where {Tv, Ti}
+    
+    affine_fdof_mapping⁻¹ = Dict(v => k for (k, v) in affine_fdof_ordering) # Bijections.jl could avoid this but probably not worth it
     affine_equation_ordering⁻¹ = Dict(v => k for (k, v) in affine_equation_ordering)
 
     for (k, _) in affine_equation_ordering
         dc[k] = DofCoefficients{Tv, Ti}()
     end
 
-    SparseArrays.dropzeros!(C) # make the following loop over C shorter
-    for j in axes(C, 2)
-        for nz_i in nzrange(C, j)
-            i = C.rowval[nz_i]
+    for j in axes(A⁻¹C, 2)
+        for nz_i in nzrange(A⁻¹C, j)
+            i = A⁻¹C.rowval[nz_i]
             dof = affine_fdof_mapping⁻¹[j]
             coeffs = dc[affine_equation_ordering⁻¹[i]]
-            push!(coeffs, (dof => C.nzval[nz_i]))
+            push!(coeffs, (dof => A⁻¹C.nzval[nz_i]))
         end
     end
     return dc
@@ -216,7 +231,12 @@ end
 """
     istangled(ch::ConstraintHandler)
 
-Check if the constraint handler has any tangled dofs.
+Check if the constraint handler has any tangled dofs. An example of a tangled dof is
+
+    u1 = u2 + u5
+    u2 = u3 + 4*u10 + 4.0.
+
+Here, `u2` is a tangled dof as it appears on the left- and right-hand side of the constraints.
 """
 function istangled(ch::ConstraintHandler)
     for coeffs in ch.dofcoefficients
@@ -238,23 +258,24 @@ function _assign_new_index!(d::Dict{Int, Int}, key::Int)
 end
 
 """
-    _sparse_column_wise_solve(luA::SparseArrays.UMFPACK.UmfpackLU{T, Int}, C::SparseMatrixCSC{T, Int}) where {T}
+    _sparse_column_wise_solve(A::SparseArrays.UMFPACK.UmfpackLU{T, TiA}, C::SparseMatrixCSC{T, TiC}) where {T, TiA, TiC}
 
 Perform a column wise solve of `AX = C` where `X` is expected to be a sparse matrix, this avoids the dense construction of `X`.
+The LU decomposition of `A` should be passed
 """
-function _sparse_column_wise_solve(luA::SparseArrays.UMFPACK.UmfpackLU{T, Int}, C::SparseMatrixCSC{T, Int}) where {T}
+function _sparse_column_wise_solve(A::SparseArrays.UMFPACK.UmfpackLU{T, TiA}, C::SparseMatrixCSC{T, TiC}) where {T, TiA, TiC}
     (m, n) = size(C)
-    I = Int[]; J = Int[]; V = T[]
+    I = TiC[]; J = TiC[]; V = T[]
     sh = SparseArrays.nnz(C)
     sizehint!(I, sh); sizehint!(J, sh); sizehint!(V, sh)
-    sol = zeros(T, m)
-    rhs = SparseArrays.SparseVector{T, Int}(undef, m) # zeros(T, n)
+    lhs = zeros(T, m)
+    rhs = zeros(T, m)
     for j in axes(C, 2)
         iszero(C[:, j]) && continue
         copy!(rhs, C[:, j])
-        ldiv!(sol, luA, rhs)
-        for (i, v) in pairs(sol)
-            v == T(0) && continue
+        ldiv!(lhs, A, rhs)
+        for (i, v) in pairs(lhs)
+            v == zero(T) && continue
             push!(I, i)
             push!(J, j)
             push!(V, v)
